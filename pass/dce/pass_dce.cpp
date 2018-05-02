744a0d4bfb789fa350a6811f739a1a65e06ef409
rafaeltp <rafaeltp@soe.ucsc.edu>
2018-05-02 14:28:04 -0700

    changed dce pass to not rely on backward, the intended behavior is different than the one from backward traversal

--- pass/dce/pass_dce.cpp
+++ pass/dce/pass_dce.cpp
@@ -15,14 +15,35 @@ Pass_dce::Pass_dce()
 void Pass_dce::transform(LGraph *g) {
 
   bm::bvector<> output_used;
+  std::set<Index_ID> pending;
 
-  for(auto idx:g->backward()) {
+  for(auto idx:g->fast()) {
+    if(g->is_graph_output(idx)) {
+      pending.insert(idx);
+    }
+  }
+
+  while(!pending.empty()) {
+    Index_ID current = *(pending.begin());
+    pending.erase(current);
+    output_used.set_bit(current);
+    fmt::print("dce using {}\n",current);
+
+    for(auto& c:g->inp_edges(current)) {
+      if(output_used.get_bit(c.get_out_pin().get_nid()))
+        continue;
+
+      pending.insert(c.get_out_pin().get_nid());
+    }
+  }
+
+  /*for(auto idx:g->backward()) {
     output_used.set_bit(idx);
     for(const auto &c:g->inp_edges(idx)) {
       output_used.set_bit(c.get_idx());
     }
     fmt::print("dce using {}\n",idx);
-  }
+  }*/
 
   for(auto idx:g->fast()) {
     if (output_used.get_bit(idx))
