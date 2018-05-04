ac4041c226e0e796e10c020a13d1ddf73caef949
Tanvir Heer <tanvirheer@gmail.com>
2018-05-04 14:49:13 -0700

    Update lambda-create.sh

--- clco/AWS-Lambda/lambda-create.sh
+++ clco/AWS-Lambda/lambda-create.sh
@@ -3,7 +3,7 @@
 aws lambda create-function \
   --function-name "helloworld" \
   --runtime "nodejs6.10" \
-  --role "arn:aws:iam::914232668340:role/Lambda-Role-FullAccess" \
+  --role "arn:aws:iam::[ROLE ID]:role/Lambda-Role-FullAccess" \
   --handler "/app/index.handler" \
   --timeout 5 \
   --zip "fileb://./app.zip" \
