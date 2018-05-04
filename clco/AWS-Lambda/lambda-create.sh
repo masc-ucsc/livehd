619c4eab2a82e313f7aa32a9b02e2bf2ffbd3d91
Tanvir Heer <tanvirheer@gmail.com>
2018-05-04 14:46:14 -0700

    Hello World Json Script on AWS Cloud

---
+++ clco/AWS-Lambda/lambda-create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aws lambda create-function \
+  --function-name "helloworld" \
+  --runtime "nodejs6.10" \
+  --role "arn:aws:iam::914232668340:role/Lambda-Role-FullAccess" \
+  --handler "/app/index.handler" \
+  --timeout 5 \
+  --zip "fileb://./app.zip" \
+  --region "us-west-2"
+
+``
