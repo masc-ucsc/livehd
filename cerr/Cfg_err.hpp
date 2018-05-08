9c6a6d814f2301bd2ead4cb7c1c79beb908a3015
Jose Renau <renau@ucsc.edu>
2018-05-08 16:49:08 -0700

    skeleton for error notification

---
+++ cerr/Cfg_err.hpp
@@ -0,0 +1,31 @@
+
+
+#define CFG_ERR_1      "asdasdasd asd P{} asd asd"
+#define CFG_NOT_FOUND  "asdasdasd asd P{} asd asd"
+
+class Cfg_err {
+
+public:
+  static void error(error_id, varg args);
+  static void warn(error_id, varg args);
+  static void info(error_id, varg args);
+
+};
+
+
+#if 0
+#define CORE_ERR_1      "asdasdasd asd P{} asd asd"
+#define CORE_NOT_FOUND  "asdasdasd asd P{} asd asd"
+
+// Future, another class for Core errors (diff file)
+class Core_err {
+
+public:
+  static void error(error_id, varg args);
+  static void warn(error_id, varg args);
+  static void info(error_id, varg args);
+
+};
+#endif
+
+
