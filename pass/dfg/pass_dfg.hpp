9d3d6bad0fc4ab7d116d212e4919eacb351c8984
H. Blake Skinner <hskinner@soe.ucsc.edu>
2018-05-23 05:39:25 +0000

    add initial dfg version

--- pass/dfg/pass_dfg.hpp
+++ pass/dfg/pass_dfg.hpp
@@ -11,9 +11,31 @@ public:
 };
 
 class Pass_dfg : public Pass {
-  public:
-    Pass_dfg() : Pass("dfg") { }
+public:
+  Pass_dfg() : Pass("dfg") { }
 
+  void cfg_2_dfg(LGraph *dfg, const LGraph *cfg);
+
+private:
+  // CFG 2 DFG conversion methods
+  Index_ID                 find_root(const LGraph *cfg);
+  void                     process_node(LGraph *dfg, const LGraph *cfg, std::unordered_map<std::string, Index_ID> last_refs, Index_ID node);
+  void                     process_assign(  LGraph *dfg,
+                                            const LGraph *cfg,
+                                            std::unordered_map<std::string, Index_ID> last_refs,
+                                            const CFG_Node_Data &data,
+                                            Index_ID node );
+  void                     process_if(      LGraph *dfg,
+                                            const LGraph *cfg,
+                                            std::unordered_map<std::string, Index_ID> last_refs,
+                                            const CFG_Node_Data &data,
+                                            Index_ID node );
+  void                     process_operands(LGraph *dfg,
+                                            const LGraph *cfg,
+                                            std::unordered_map<std::string, Index_ID> last_refs,
+                                            const CFG_Node_Data &data,
+                                            Index_ID node,
+                                            std::vector<Index_ID>& );
 };
 
 #endif
