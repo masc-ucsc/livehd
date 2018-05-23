e94a8c28955436d09ba827302bb4be4b8636b33a
H. Blake Skinner <hskinner@soe.ucsc.edu>
2018-05-23 12:23:33 -0700

    fix naming convetion issue

--- pass/dfg/pass_dfg.hpp
+++ pass/dfg/pass_dfg.hpp
@@ -3,11 +3,16 @@
 
 #include "options.hpp"
 #include "pass.hpp"
+#include "cfg_node_data.hpp"
 
 #include <string>
 
 class Pass_dfg_options_pack : public Options_pack {
 public:
+  Pass_dfg_options_pack();
+
+  std::string dfg_output;
+  std::string cfg_input;
 };
 
 class Pass_dfg : public Pass {
@@ -15,11 +20,15 @@ public:
   Pass_dfg() : Pass("dfg") { }
 
   void cfg_2_dfg(LGraph *dfg, const LGraph *cfg);
+  virtual void transform(LGraph *g);
 
 private:
   // CFG 2 DFG conversion methods
   Index_ID                 find_root(const LGraph *cfg);
-  void                     process_node(LGraph *dfg, const LGraph *cfg, std::unordered_map<std::string, Index_ID> last_refs, Index_ID node);
+  void                     process_node(    LGraph *dfg,
+                                            const LGraph *cfg,
+                                            std::unordered_map<std::string, Index_ID> last_refs,
+                                            Index_ID node);
   void                     process_assign(  LGraph *dfg,
                                             const LGraph *cfg,
                                             std::unordered_map<std::string, Index_ID> last_refs,
@@ -36,6 +45,8 @@ private:
                                             const CFG_Node_Data &data,
                                             Index_ID node,
                                             std::vector<Index_ID>& );
+
+  Index_ID get_child(const LGraph *cfg, Index_ID node);
 };
 
 #endif
