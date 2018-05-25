43137ba1de1cd7756fb33afcc28ca0f743dd5a22
H. Blake Skinner <hskinner@soe.ucsc.edu>
2018-05-25 06:52:13 +0000

    incremental updates to CFG->DFG

--- pass/dfg/pass_dfg.hpp
+++ pass/dfg/pass_dfg.hpp
@@ -4,8 +4,11 @@
 #include "options.hpp"
 #include "pass.hpp"
 #include "cfg_node_data.hpp"
+#include "lgedge.hpp"
 
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 class Pass_dfg_options_pack : public Options_pack {
 public:
@@ -14,10 +17,17 @@ public:
   bool generate_dots_flag;
 };
 
-struct CF2DF_State {
+class CF2DF_State {
+public:
   std::unordered_map<std::string, Index_ID> last_refs;
   std::unordered_map<Index_ID, Index_ID> node_mapping;
 };
+
+const char REGISTER_MARKER = '@';
+const char INPUT_MARKER = '$';
+const char OUTPUT_MARKER = '%';
+const Port_ID REG_INPUT = 'D';
+const Port_ID REG_OUTPUT = 'Q';
 
 class Pass_dfg : public Pass {
 public:
@@ -47,14 +57,22 @@ private:
                                             CF2DF_State *state,
                                             const CFG_Node_Data &data,
                                             Index_ID node );
-  void                     process_operands(LGraph *dfg,
+  std::vector<Index_ID>    process_operands(LGraph *dfg,
                                             const LGraph *cfg,
                                             CF2DF_State *state,
                                             const CFG_Node_Data &data,
-                                            Index_ID node,
-                                            std::vector<Index_ID>& );
+                                            Index_ID node );
 
   Index_ID get_child(const LGraph *cfg, Index_ID node);
+
+  bool is_register(const std::string &v) { return v[0] == REGISTER_MARKER; }
+  bool is_input(const std::string &v) { return v[0] == INPUT_MARKER; }
+  bool is_output(const std::string &v) { return v[0] == OUTPUT_MARKER; }
+
+  Index_ID create_register(LGraph *g, CF2DF_State *state, const std::string &var_name);
+  Index_ID create_input(LGraph *g, CF2DF_State *state, const std::string &var_name);
+  Index_ID create_output(LGraph *g, CF2DF_State *state, const std::string &var_name);
+  Index_ID default_constant(LGraph *g);
 };
 
 #endif
