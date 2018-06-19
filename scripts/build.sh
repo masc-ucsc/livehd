ce608e27b749db5a9e3baee03803dfb320a4c077
Jose Renau <renau@ucsc.edu>
2018-06-19 14:03:03 -0700

    new bazel build with docker. Includes image with pre-cached bazel for speed

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,40 +2,16 @@
 
 # Defaults for configuration variables used by script
 : ${LGRAPH_SRC:=${HOME}/lgraph}
-: ${LGRAPH_BUILD_DIR:=${HOME}/build}
-: ${LGRAPH_BUILD_TYPE:=Debug}
-: ${LGRAPH_HOST_PROCS:=$(nproc)}
+: ${LGRAPH_BUILD_MODE:=fastbuild}
 
-BUILD_DIR=${LGRAPH_BUILD_DIR}/${LGRAPH_BUILD_TYPE,,}
-
-if [ ! -e ${LGRAPH_SRC}/CMakeLists.txt ]; then
-  echo "BUILD ERROR: '${LGRAPH_SRC}' does not contain LGraph source code"
+if [ ! -e ${LGRAPH_SRC}/WORKSPACE ]; then
+  echo "ERROR: '${LGRAPH_SRC}' does not contain LGRAPH source code"
   exit -1
 fi
 
 echo "build.sh: Building....."
-
 echo "build.sh: Getting sub directories..."
 cd ${LGRAPH_SRC}
 git submodule update --init --recursive
-
-echo "build.sh: compiling..."
-if [ -d ${BUILD_DIR} ]; then
-  if [ -e ${BUILD_DIR}/Makefile ]; then
-    cd ${BUILD_DIR}
-    make -j${LGRAPH_HOST_PROCS}
-    echo "build.sh: cmake is broken, re-run make without -j"
-    make
-  else
-    echo "BUILD ERROR: '${BUILD_DIR}' already exists"
-    exit -2
-  fi
-else
-  mkdir -p ${BUILD_DIR}
-  cd ${BUILD_DIR}
-  cmake -DCMAKE_BUILD_TYPE=${LGRAPH_BUILD_TYPE} ${LGRAPH_SRC}
-  make -j${LGRAPH_HOST_PROCS}
-  echo "build.sh: cmake is broken, re-run make without -j"
-  make
-fi
+bazel build -c ${LGRAPH_BUILD_MODE} //...
 
--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -2,25 +2,17 @@
 
 # Defaults for configuration variables used by script
 : ${LGRAPH_SRC:=${HOME}/lgraph}
-: ${LGRAPH_BUILD_DIR:=${HOME}/build}
-: ${LGRAPH_BUILD_TYPE:=Debug}
-: ${LGRAPH_ENABLE_LIVE:=0}
+: ${LGRAPH_BUILD_MODE:=fastbuild}
 
-BUILD_DIR=${LGRAPH_BUILD_DIR}/${LGRAPH_BUILD_TYPE,,}
-
-if [ ! -e ${LGRAPH_SRC}/CMakeLists.txt ]; then
+if [ ! -e ${LGRAPH_SRC}/WORKSPACE ]; then
   echo "ERROR: '${LGRAPH_SRC}' does not contain LGRAPH source code"
   exit -1
 fi
 
-if [ ! -e ${BUILD_DIR}/inou/json/lgjson ]; then
-  echo "ERROR: '${BUILD_DIR}' does not contain LGRAPH binary"
-  exit -2
-fi
+cd ${LGRAPH_SRC}
 
-cd ${BUILD_DIR}
+bazer test -c ${LGRAPH_BUILD_MODE} //...
 
-make test
 if [ $? -eq 0 ]; then
   exit 0
 else
