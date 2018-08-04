dbf0ba6057f1b82007d1a450fb9025440de64870
sheng hong wang <swang203@ucsc.edu>
2018-08-03 21:46:50 -0700

    add python subprocess of Bash to execute yosys commands

--- pyth/test_dfg.py
+++ pyth/test_dfg.py
@@ -2,20 +2,28 @@
 import sys
 import os
 import shutil
+import subprocess
+
+#To do 1: the pybind11 outputs are somehow out of order now, have to fix it later.
+#To do 2: add some assert for yosys
+#To do 3: need to redirect/cp terminal messages to a log file
 
 base_dir = os.path.dirname(sys.argv[0]) or '.'
 sys.path.insert(1, base_dir)
+print("base_dir: " + base_dir)
 package_dir_a = os.path.join(base_dir, '__main__')
 sys.path.insert(1, package_dir_a)
+print("package_dir_a: " + package_dir_a)
 
 print("test running directory: ", os.getcwd())
+
+
 shutil.rmtree("lgdb", ignore_errors=True)  # remove previous garbage
 
 try:
   import lgraph
 
   cfg_name = 'pt_simple_add'
-  # cfg_name = 'pt1'
   dfg_name = cfg_name + '_dfg'
 
   cfg_opts = {
@@ -24,10 +32,9 @@ try:
         "src": "inou/cfg/tests/%s.cfg" % cfg_name
         }
 
-  sys.stdout.flush()
   cfg = lgraph.Inou_cfg(cfg_opts).generate()
-  sys.stdout.flush()
   print("===================== cfg pass ======================")
+  # sys.stdout.flush()
 
   dfg_opts = {
         "lgdb": "lgdb",
@@ -39,15 +46,40 @@ try:
 
   assert not (dfg is None)
   assert not (dfg[0] is None)
-  sys.stdout.flush()
   print("===================== dfg pass ======================")
+  # sys.stdout.flush()
 
 
-  #g = lgraph.find_lgraph("lgdb",dfg_name)
-  #assert dfg[0].lg_id() == g.lg_id()
-  #g.dump()
-  #print("==================== dump pass ======================")
+  g = lgraph.find_lgraph("lgdb",dfg_name)
+  assert dfg[0].lg_id() == g.lg_id()
+  g.dump()
+  # sys.stdout.flush()
+  print("==================== dump pass ======================")
 
+
+  # Yosys related variables
+  # don't forget extra space between command and /path/files
+  opt_yosys = subprocess.check_output('which yosys', shell=True).decode('utf-8').strip()
+  opt_log_dir = './logs'
+  opt_verilog_dir = './verilog'
+  opt_hierarchy = ''
+  opt_inou_yosys = '../../../inou/yosys/liblgraph_yosys.so' 
+  opt_graph_input = dfg_name
+  yosys_log = opt_log_dir + '/' + opt_graph_input + '_to_yosys.log' 
+  yosys_write_verilog = 'write_verilog ' + opt_graph_input + '_dirty.v; opt -fast; opt_clean -purge; write_verilog ' + opt_graph_input + '.v;' 
+  yosys_cmds =  '" dump_yosys ' + opt_hierarchy + '-graph_name ' + opt_graph_input + '; ' + yosys_write_verilog + '"'
+
+  bash_cmds =[ 'rm -f ' + opt_log_dir + "/*.*", 
+               'mkdir '  + opt_log_dir,
+                opt_yosys + ' -ql ' + yosys_log + ' -m  ' + opt_inou_yosys + ' -p  ' + yosys_cmds,
+               'rm -f ' + opt_verilog_dir + "/*.*", 
+               'mkdir ' + opt_verilog_dir,
+               'mv *.v ' + opt_verilog_dir
+                ]
+
+  for cmd in bash_cmds:
+      print('Executing bash cmd: ' + cmd)
+      subprocess.run(cmd, shell=True)
 
 
 except:
