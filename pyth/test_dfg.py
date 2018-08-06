df9570ec8f3f13e3116d8f8ec5afeb5d2f4b7bc5
sheng hong wang <swang203@ucsc.edu>
2018-08-05 20:49:36 -0700

    only way to solve test_dfg.py terminal log out of order is to set environment variable PYTHONUNBUFFERED=1

--- pyth/test_dfg.py
+++ pyth/test_dfg.py
@@ -4,15 +4,16 @@ import os
 import shutil
 import subprocess
 
-#To do 1: the pybind11 outputs are somehow out of order now, have to fix it later.
-#To do 2: add some assert for yosys
-#To do 3: need to redirect/cp terminal messages to a log file
+#run "export PYTHONUNBUFFERED=1" in the shell if you found terminal output is out of order
+#To do : add some assert for yosys
+
 
 base_dir = os.path.dirname(sys.argv[0]) or '.'
 sys.path.insert(1, base_dir)
-print("base_dir: " + base_dir)
 package_dir_a = os.path.join(base_dir, '__main__')
 sys.path.insert(1, package_dir_a)
+
+print("base_dir: " + base_dir)
 print("package_dir_a: " + package_dir_a)
 print("test running directory: ", os.getcwd())
 
@@ -37,9 +38,10 @@ try:
         "src": "inou/cfg/tests/%s.cfg" % cfg_name
         }
 
+  print("\n\n\n")
   cfg = lgraph.Inou_cfg(cfg_opts).generate()
-  print("===================== cfg pass ======================")
-  # sys.stdout.flush()
+  print("===================== cfg pass ======================\n\n\n", flush=True)
+
 
   dfg_opts = {
         "lgdb": "lgdb",
@@ -51,15 +53,15 @@ try:
 
   assert not (dfg is None)
   assert not (dfg[0] is None)
-  print("===================== dfg pass ======================")
-  # sys.stdout.flush()
+  sys.stdout.flush()
+  print("===================== dfg pass ======================\n\n\n", flush=True)
 
 
   g = lgraph.find_lgraph("lgdb",dfg_name)
   assert dfg[0].lg_id() == g.lg_id()
   g.dump()
-  # sys.stdout.flush()
-  print("==================== dump pass ======================")
+  sys.stdout.flush()
+  print("==================== dump pass ======================\n\n\n", flush=True)
 
 
   # Yosys related variables
@@ -84,6 +86,7 @@ try:
       print('Executing bash cmd: ' + cmd)
       subprocess.run(cmd, shell=True)
 
+  print("\n\n\n")
 
 except:
   print("lgraph raised exception. Test fails")
