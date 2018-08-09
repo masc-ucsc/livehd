c641777f4bf8fe8ce338c7975a883fe70e0ca5c3
sheng hong wang <swang203@ucsc.edu>
2018-08-09 08:06:29 -0700

    (1) adding some cfg patterns (2) modify test_dfg to handle multiple cfgs (3) add environmental variable change direction to Bazel.md

--- pyth/test_dfg.py
+++ pyth/test_dfg.py
@@ -3,11 +3,9 @@ import sys
 import os
 import shutil
 import subprocess
+import glob
 
-#run "export PYTHONUNBUFFERED=1" in the shell if you found terminal output is out of order
 #To do : add some assert for yosys
-
-
 base_dir = os.path.dirname(sys.argv[0]) or '.'
 sys.path.insert(1, base_dir)
 package_dir_a = os.path.join(base_dir, '__main__')
@@ -19,29 +17,51 @@ print("test running directory: ", os.getcwd())
 
 if not os.path.exists('./logs'):
     os.mkdir('logs')
+else:
+    files = glob.glob('./logs/*') 
+    for f in files:  os.remove(f)
 
 if not os.path.exists('./verilog'):
     os.mkdir('verilog')
+else:
+    files = glob.glob('./verilog/*') 
+    for f in files:  os.remove(f)
 
-shutil.rmtree("lgdb",    ignore_errors=True)  # remove previous garbage
+if not os.path.exists('./lgdb'):
+    os.mkdir('lgdb')
+else:
+    files = glob.glob('./lgdb/*') 
+    for f in files:  os.remove(f)
 
 
 try:
   import lgraph
+  # cfg_files = ['simple_add', 'top']
+  cfg_files = ['simple_add']
+  
+  for cfg_name in cfg_files:
+      dfg_name = cfg_name + '_dfg'
 
-  cfg_name = 'pt_simple_add'
-  dfg_name = cfg_name + '_dfg'
+      cfg_opts = {
+            "lgdb": "lgdb",
+            "graph_name": cfg_name,
+            "src": "inou/cfg/tests/%s.cfg" % cfg_name
+            }
 
-  cfg_opts = {
-        "lgdb": "lgdb",
-        "graph_name": cfg_name,
-        "src": "inou/cfg/tests/%s.cfg" % cfg_name
-        }
+      print("\n\n\n")
+      cfg = lgraph.Inou_cfg(cfg_opts).generate()
+      
+      #temporary start: wait for json dump integrate into pybind11 system
+      bash_json_cmds = ['~/lgraph/bazel-bin/inou/json/lgjson --graph_name ' + cfg_name + ' --json_output ' + cfg_name + '.json ' + '&&' + 'mv *.json ./logs']
+                        # need to find a way to create json files for build-in sub-graphs
+                        # '~/lgraph/bazel-bin/inou/json/lgjson --graph_name ' + cfg_name + '1' + ' --json_output ' + cfg_name + '1' + '.json ' + '&&' + 'mv *.json ./logs',
+                        # '~/lgraph/bazel-bin/inou/json/lgjson --graph_name ' + cfg_name + '2' + ' --json_output ' + cfg_name + '2' + '.json ' + '&&' + 'mv *.json ./logs']
+      for cmd in bash_json_cmds:
+          print('Executing bash json cmd: ' + cmd)
+          subprocess.run(cmd, shell=True)
+      #temporary end
 
-  print("\n\n\n")
-  cfg = lgraph.Inou_cfg(cfg_opts).generate()
-  print("===================== cfg pass ======================\n\n\n", flush=True)
-
+  print("\n===================== cfg pass ======================\n\n\n", flush=True)
 
   dfg_opts = {
         "lgdb": "lgdb",
@@ -76,13 +96,12 @@ try:
   yosys_write_verilog = 'write_verilog ' + opt_graph_input + '_dirty.v; opt -fast; opt_clean -purge; write_verilog ' + opt_graph_input + '.v;' 
   yosys_cmds =  '" dump_yosys ' + opt_hierarchy + '-graph_name ' + opt_graph_input + '; ' + yosys_write_verilog + '"'
 
-  bash_cmds =[ 'rm -f ' + opt_log_dir + "/*.*", 
-                opt_yosys + ' -ql ' + yosys_log + ' -m  ' + opt_inou_yosys + ' -p  ' + yosys_cmds,
-               'rm -f ' + opt_verilog_dir + "/*.*", 
-               'mv *.v ' + opt_verilog_dir
-                ]
+  bash_yosys_cmds =[  
+                      opt_yosys + ' -ql ' + yosys_log + ' -m  ' + opt_inou_yosys + ' -p  ' + yosys_cmds,
+                     'mv *.v ' + opt_verilog_dir
+                   ]
 
-  for cmd in bash_cmds:
+  for cmd in bash_yosys_cmds:
       print('Executing bash cmd: ' + cmd)
       subprocess.run(cmd, shell=True)
 
