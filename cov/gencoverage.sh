5a857cc451e3487fa76e5d413915db34bba8997f
sheng hong wang <swang203@ucsc.edu>
2018-10-17 23:40:54 -0700

    cfg->verilog code generation bazel test script upload

--- cov/gencoverage.sh
+++ cov/gencoverage.sh
@@ -1,12 +1,13 @@
 
 bazel coverage -k //...
-./bazel-bin/third_party/misc/ezsat/testbench
-./pass/abc/tests/abc.sh
 ./inou/yosys/tests/yosys.sh
 ./inou/yosys/tests/synth.sh
 ./inou/json/tests/rand_json.sh
+./inou/cfg/tests/pyrope.sh
+./pass/abc/tests/abc.sh
 ./bazel-bin/eprp/eprp_test
 ./bazel-bin/core/thread_pool_test
+./bazel-bin/third_party/misc/ezsat/testbench
 
 gcovr -r .  bazel-out/k8-fastbuild/bin/core bazel-out/k8-fastbuild/bin/meta bazel-out/k8-fastbuild/bin/cops bazel-out/k8-fastbuild/bin/main bazel-out/k8-fastbuild/bin/inou bazel-out/k8-fastbuild/bin/pass --html-details --html -o cov/coverage.html
 
