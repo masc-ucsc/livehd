a150cb431f1805086a5b9f80ef7e17c796a23922
Jose Renau <renau@ucsc.edu>
2018-10-18 14:29:57 -0700

    patch dockers setup, try both gcc and clang in travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@
 # Defaults for configuration variables used by script
 : ${LGRAPH_SRC:=${HOME}/lgraph}
 : ${LGRAPH_BUILD_MODE:=fastbuild}
+: ${LGRAPH_COMPILER:=g++}
 
 if [ ! -e ${LGRAPH_SRC}/WORKSPACE ]; then
   echo "ERROR: '${LGRAPH_SRC}' does not contain LGRAPH source code"
@@ -13,6 +14,5 @@ fi
 echo "build.sh: Building....."
 echo "build.sh: Getting sub directories..."
 cd ${LGRAPH_SRC}
-git submodule update --init --recursive
-bazel build -c ${LGRAPH_BUILD_MODE} //...
+CXX=${LGRAPH_COMPILER} CC=${LGRAPH_COMPILER} bazel build -c ${LGRAPH_BUILD_MODE} //...
 
