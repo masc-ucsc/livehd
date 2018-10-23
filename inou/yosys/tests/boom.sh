49d59fc1e3a53aa7d0dd0c3ae2e2fb2aba348dff
rafaeltp <rafaeltp@soe.ucsc.edu>
2018-10-23 12:30:36 -0700

    adding a test on boom

--- inou/yosys/tests/boom.sh
+++ inou/yosys/tests/boom.sh
@@ -1,14 +1,47 @@
 
 OPT_LGSHELL=./bazel-bin/main/lgshell
+YOSYS=./inou/yosys/lgyosys
+LGCHECK=./inou/yosys/lgcheck
 
-if [ ! -f ${OPT_LGSHELL} ]
+BENCH_DIR=./projects/boom/
+TEST_CASE=boom.system.TestHarness.BoomConfig.v
+BOOM_FILE=${BENCH_DIR}/${TEST_CASE}
+
+if [ ! -f ${OPT_LGSHELL} ]; then
   echo "boom test could not find lgshell"
   exit 1
 fi
 
-echo "inou.yosys.tolg files:./test/benchmarks/boom/boombase.v |> inou.yosys.fromlg odir:tmp" | ./bazel-bin/main/lgshell
+if [ ! -f ${YOSYS} ]; then
+  echo "boom test could not find lgyosys"
+  exit 1
+fi
+
+
+if [ ! -f ${LGCHECK} ]; then
+  echo "boom test could not find lgcheck"
+  exit 1
+fi
+
+
+echo "live.parse files:${BOOM_FILE}" | ${LGSHELL}
+
+for i in lgdb/parse/*; do
+  echo "inou.yosys.tolg files:${i} |> inou.yosys.fromlg odir:tmp" | ${LGSHELL}
+
+  if [ $? -ne 0 ]; then
+    echo "Failed to read/write verilog for module $i"
+    exit 1
+  fi
+done
 
 for i in tmp/*; do
-  name=`basename ${i%.*}`
-  ./inou/yosys/lgcheck --reference=./test/benchmarks/boom/boombase.v --implementation=./tmp/ --top=$name
+  name=`basename ${i%.*}`i
+  filename="chunk_..`echo ${BOOM_FILE} | tr '/' '.'`"
+  ${LGCHECK} --reference=./lgdb/parse/chunk_..${filename}:${name} --implementation=./tmp/${i} --top=$name
+
+  if [ $? -ne 0 ]; then
+    echo "Module $i does not match"
+    exit 1
+  fi
 done
