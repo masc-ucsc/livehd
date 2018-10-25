ecf6ef85caff28377142531ca65e38420b0fe66a
rafaeltp <rafaeltp@soe.ucsc.edu>
2018-10-25 12:21:32 -0700

    fixing verilog's  issue

--- inou/yosys/tests/reduce.v
+++ inou/yosys/tests/reduce.v
@@ -1,9 +1,10 @@
 
-module reduce(input [7:0] in, output a, output b, output c);
+module reduce(input [7:0] in, output a, output b, output c, output d);
 
 assign a = &in;
 assign b = |in;
 assign c = ^in;
+assign d = !in;
 
 endmodule
 
