ab71c8df851d1dd4993a26c4044c4dcd4443e615
Sheng Hong Wang <swang203@ucsc.edu>
2018-10-26 16:58:37 -0700

    modify prp patterns for pseudo bits inference development

--- inou/cfg/tests/GEN_CFG.sh
+++ inou/cfg/tests/GEN_CFG.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 #rm -f *.cfg
-#~/pyrope/parser/bin/prp sp_add.prp             > yyy && column -t yyy > xxx && mv xxx sp_add.cfg
-#~/pyrope/parser/bin/prp top.prp                > yyy && column -t yyy > xxx && mv xxx top.cfg
-#~/pyrope/parser/bin/prp const.prp              > yyy && column -t yyy > xxx && mv xxx const.cfg
-~/pyrope/parser/bin/prp sp_if_0.prp            > yyy && column -t yyy > xxx && mv xxx sp_if_0.cfg     && rm -f yyy  
-~/pyrope/parser/bin/prp sp_if_1.prp            > yyy && column -t yyy > xxx && mv xxx sp_if_1.cfg     && rm -f yyy 
-~/pyrope/parser/bin/prp sp_if_2.prp            > yyy && column -t yyy > xxx && mv xxx sp_if_2.cfg     && rm -f yyy
-~/pyrope/parser/bin/prp sp_if_3.prp            > yyy && column -t yyy > xxx && mv xxx sp_if_3.cfg     && rm -f yyy
-~/pyrope/parser/bin/prp nested_if_0.prp        > yyy && column -t yyy > xxx && mv xxx nested_if_0.cfg && rm -f yyy
-~/pyrope/parser/bin/prp nested_if_1.prp        > yyy && column -t yyy > xxx && mv xxx nested_if_1.cfg && rm -f yyy
-~/pyrope/parser/bin/prp nested_if_2.prp        > yyy && column -t yyy > xxx && mv xxx nested_if_2.cfg && rm -f yyy
-~/pyrope/parser/bin/prp nested_if_3.prp        > yyy && column -t yyy > xxx && mv xxx nested_if_3.cfg && rm -f yyy
+~/pyrope/parser/bin/prp sp_add.prp          > yyy && column -t yyy > xxx && mv xxx sp_add.cfg
+~/pyrope/parser/bin/prp top.prp             > yyy && column -t yyy > xxx && mv xxx top.cfg
+~/pyrope/parser/bin/prp constant.prp        > yyy && column -t yyy > xxx && mv xxx constant.cfg
+~/pyrope/parser/bin/prp sp_if_0.prp         > yyy && column -t yyy > xxx && mv xxx sp_if_0.cfg     && rm -f yyy  
+~/pyrope/parser/bin/prp nested_if_0.prp     > yyy && column -t yyy > xxx && mv xxx nested_if_0.cfg && rm -f yyy
+~/pyrope/parser/bin/prp nested_if_1.prp     > yyy && column -t yyy > xxx && mv xxx nested_if_1.cfg && rm -f yyy
+~/pyrope/parser/bin/prp nested_if_2.prp     > yyy && column -t yyy > xxx && mv xxx nested_if_2.cfg && rm -f yyy
+~/pyrope/parser/bin/prp nested_if_3.prp     > yyy && column -t yyy > xxx && mv xxx nested_if_3.cfg && rm -f yyy
+~/pyrope/parser/bin/prp if_elif_else.prp    > yyy && column -t yyy > xxx && mv xxx if_elif_else.cfg && rm -f yyy
+
+~/pyrope/parser/bin/prp sp_add_nb.prp       > yyy && column -t yyy > xxx && mv xxx sp_add_nb.cfg
+~/pyrope/parser/bin/prp top_nb.prp          > yyy && column -t yyy > xxx && mv xxx top_nb.cfg
+~/pyrope/parser/bin/prp constant_nb.prp     > yyy && column -t yyy > xxx && mv xxx constant_nb.cfg
+~/pyrope/parser/bin/prp sp_if_0_nb.prp      > yyy && column -t yyy > xxx && mv xxx sp_if_0_nb.cfg     && rm -f yyy  
+~/pyrope/parser/bin/prp nested_if_0_nb.prp  > yyy && column -t yyy > xxx && mv xxx nested_if_0_nb.cfg && rm -f yyy
+~/pyrope/parser/bin/prp nested_if_1_nb.prp  > yyy && column -t yyy > xxx && mv xxx nested_if_1_nb.cfg && rm -f yyy
+~/pyrope/parser/bin/prp nested_if_2_nb.prp  > yyy && column -t yyy > xxx && mv xxx nested_if_2_nb.cfg && rm -f yyy
+~/pyrope/parser/bin/prp nested_if_3_nb.prp  > yyy && column -t yyy > xxx && mv xxx nested_if_3_nb.cfg && rm -f yyy
+~/pyrope/parser/bin/prp if_elif_else_nb.prp > yyy && column -t yyy > xxx && mv xxx if_elif_else_nb.cfg && rm -f yyy
