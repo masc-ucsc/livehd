02096d9c92c54185c75081222eaa31f8e29118e7
Jose Renau <renau@ucsc.edu>
2018-10-28 09:14:39 -0700

    rename topo to sample to match directory

--- pass/sample/pass_sample.cpp
+++ pass/sample/pass_sample.cpp
@@ -1,11 +1,13 @@
 //  This file is distributed under the BSD 3-Clause License. See LICENSE for details.
 
-#include "pass_topological.hpp"
+#include "lgedgeiter.hpp"
 
-Pass_topo::Pass_topo() {
+#include "pass_sample.hpp"
+
+Pass_sample::Pass_sample() {
 }
 
-void Pass_topo::trans(LGraph *orig) {
+void Pass_sample::trans(LGraph *orig) {
   int cells = 0;
   for(const auto& idx : orig->forward()) {
     (void)idx;
