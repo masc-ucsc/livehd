31bf8265461fb741ad3c7773c185722553856ac8
Jose Renau <renau@ucsc.edu>
2018-10-28 09:14:39 -0700

    rename topo to sample to match directory

--- main/pass_sample_api.hpp
+++ main/pass_sample_api.hpp
@@ -1,7 +1,7 @@
 
 #include "eprp.hpp"
 
-class Pass_topological_api {
+class Pass_sample_api {
 protected:
   static void trans(Eprp_var &var);
 
--- pass/sample/pass_sample.hpp
+++ pass/sample/pass_sample.hpp
@@ -1,27 +1,26 @@
 //  This file is distributed under the BSD 3-Clause License. See LICENSE for details.
-#ifndef PASS_TOPO_H
-#define PASS_TOPO_H
+#ifndef PASS_SAMPLE_H
+#define PASS_SAMPLE_H
 
 #include "pass.hpp"
 #include "options.hpp"
-#include "lgedgeiter.hpp"
 #include "lgraph.hpp"
 
 #include <string>
 //sample pass that counts number of nodes, but traverses the graph in
 //topological order from inputs to output
-class Pass_topological_options_pack : public Options_base {
+class Pass_sample_options_pack : public Options_base {
 public:
   void set(const std::string &label, const std::string &value) {
     //nothing to do?
   }
 };
 
-class Pass_topo : public Pass {
+class Pass_sample : public Pass {
 protected:
-  Pass_topological_options_pack opack;
+  Pass_sample_options_pack opack;
 public:
-  Pass_topo();
+  Pass_sample();
 
   void trans(LGraph *orig) final;
 
