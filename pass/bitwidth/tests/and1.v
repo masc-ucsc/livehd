94ee2eaa96ddc2edbc916790448c431450527405
Hunter Coffman <coffmanhunter@gmail.com>
2018-10-31 13:55:34 -0700

    Introducing basic version of bitwidth

---
+++ pass/bitwidth/tests/and1.v
@@ -0,0 +1,3 @@
+module and1(input [1:0] a, input [1:0] b, output [2:0] c);
+  assign c = a & b;
+endmodule
---
+++ pass/bitwidth/tests/massign.v
@@ -0,0 +1,3 @@
+module mAssign ( input [3:0] a, output [1:0] b);
+assign b = a;
+endmodule
