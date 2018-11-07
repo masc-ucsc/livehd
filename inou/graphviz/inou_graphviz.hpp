03d413313dc3dbe0fe47d9eab3bf682f31095714
Jose Renau <renau@ucsc.edu>
2018-11-06 19:07:50 -0800

    migrate inou.graphviz to new pass interfae

--- inou/graphviz/inou_graphviz.hpp
+++ inou/graphviz/inou_graphviz.hpp
@@ -1,46 +1,29 @@
 //  This file is distributed under the BSD 3-Clause License. See LICENSE for details.
-//
-
 #ifndef INOU_graphviz_H
 #define INOU_graphviz_H
 
 #include <atomic>
-#include <string>
 
-#include "inou.hpp"
-#include "options.hpp"
+#include "pass.hpp"
+#include "lgraph.hpp"
 
-class Inou_graphviz_options : public Options_base {
-public:
-  Inou_graphviz_options()
-    : odir(".") {
-  }
-
-  std::string odir;
-
-  void set(const std::string &key, const std::string &value) final;
-};
-
-class Inou_graphviz : public Inou {
+class Inou_graphviz : public Pass {
 private:
 protected:
-  Inou_graphviz_options opack;
+  std::string odir;
 
   std::atomic<int> total;
   void inc_total(Index_ID idx) {
     total++;
   };
 
+  static void fromlg(Eprp_var &var);
+
+  void do_fromlg(std::vector<const LGraph *> &out);
 public:
   Inou_graphviz();
-  virtual ~Inou_graphviz();
 
-  std::vector<LGraph *> tolg() final;
-  void fromlg(std::vector<const LGraph *> &out) final;
-
-  void set(const std::string &key, const std::string &value) final {
-    opack.set(key,value);
-  }
+  void setup() final;
 };
 
 #endif
