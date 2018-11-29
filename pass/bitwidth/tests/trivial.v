c55f3a3bb214d1c1190a7a02309db1323ca32d58
Hunter Coffman <coffmanhunter@gmail.com>
2018-11-28 20:41:32 -0800

    Match with current

--- pass/bitwidth/tests/trivial.v
+++ pass/bitwidth/tests/trivial.v
@@ -1,4 +1,5 @@
-module trivial( input a, input b, output c);
-assign c = a & b;
+module trivial( input [3:0] a, input [3:0] b, output [3:0] c, output [3:0] d);
+assign c = a + b;
+assign d = a - b;
 endmodule
 
