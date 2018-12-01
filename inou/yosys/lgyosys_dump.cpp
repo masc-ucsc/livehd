4df84a9183e2273b9795251c92625c6498003432
Jose Renau <renau@ucsc.edu>
2018-12-01 11:36:43 -0800

    undo assert (must be fixed, but breaks regression now)

--- inou/yosys/lgyosys_dump.cpp
+++ inou/yosys/lgyosys_dump.cpp
@@ -314,7 +314,10 @@ void Lgyosys_dump::create_subgraph_outputs(const LGraph *g, RTLIL::Module *modul
 
     std::string name;
     Index_ID    port_nid = g->find_idx_from_pid(idx, edge.get_out_pin().get_pid());
-    assert(port_nid == edge.get_idx());
+    // edge.get_self_root_nid() edge.get_self_master_root_nid() 
+    // edge.get_dst_root_nid() edge.get_dst_master_root_nid()
+    // edge.get_src_root_nid() edge.get_src_master_root_nid()
+    //assert(port_nid == edge.get_idx());
     if(g->get_wid(port_nid)) {
       name = "\\" + std::string(g->get_node_wirename(port_nid));
     } else {
