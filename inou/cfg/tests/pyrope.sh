f132e47b2d46c77fc902ed319f0138dd323d8275
Sheng Hong Wang <swang203@ucsc.edu>
2019-01-16 19:46:07 -0800

    for debug

--- inou/cfg/tests/pyrope.sh
+++ inou/cfg/tests/pyrope.sh
@@ -15,8 +15,8 @@ pwd
 
 # pts='top_ooo  sp_add  sp_if_0  top  nested_if_0  nested_if_1  nested_if_2  if_elif_else'
 # pts='constant_pos constant_neg sp_if_0 nested_if_0 nested_if_1 nested_if_2 nested_if_3'
-# pts='top_inline_add'
-pts='nested_if_0'
+pts='top_inline_add'
+# pts='nested_if_0'
 
 LGSHELL=./bazel-bin/main/lgshell
 
