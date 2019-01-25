c7a4da96dc13bbc9cbe944129f0ac15b25d8b2c9
Jose Renau <renau@ucsc.edu>
2019-01-24 18:16:02 -0800

    new tree structure

--- elab/tree.hpp
+++ elab/tree.hpp
@@ -9,27 +9,32 @@
 #include "iassert.hpp"
 
 using Tree_level = Explicit_type<int32_t, struct Tree_level_struct>;  // Must be signed
-using Tree_pos   = Explicit_type<uint32_t, struct Tree_pos_struct>;
+using Tree_pos   = Explicit_type<int32_t, struct Tree_pos_struct>;    // Must be unsigned, <0 not connected
 
 class Tree_index {
+public:
   const Tree_level level;
   const Tree_pos   pos;
 
-public:
   Tree_index() = delete;
   Tree_index(Tree_level l, Tree_pos i) : level(l), pos(i) {}
-
-  inline Tree_level get_level() const { return level; }
-  inline Tree_pos   get_pos() const { return pos; }
 
   bool operator==(const Tree_index &i) const { return level == i.level && pos == i.pos; }
 };
 
 template <typename X>
 class Tree {
-  std::vector<std::vector<X>>          data_stack;
-  std::vector<std::vector<Tree_index>> parent_stack;
-  int                                  pending_parent;  // Must be signed
+  struct Tree_pointers {
+    Tree_pointers(Tree_pos p) : parent(p), eldest_child(-1), younger_child(-1), younger_sibling(-1) {  }
+
+    const Tree_pos  parent; // Parent can never change
+    Tree_pos        eldest_child;
+    Tree_pos        younger_child;
+    Tree_pos        younger_sibling;
+  };
+  std::vector<std::vector<X>>             data_stack;
+  std::vector<std::vector<Tree_pointers>> pointers_stack;
+  int                                     pending_parent;  // Must be signed
 
   void adjust_to_level(Tree_level level);
 
@@ -43,9 +48,9 @@ public:
   X &      get_data(const Tree_index &leaf);
   const X &get_data(const Tree_index &leaf) const;
 
-  const Tree_index &get_parent(const Tree_index &child) const;
-  const Tree_index &get_root() const;
-  void              set_root(const X &data);
+  const Tree_index get_parent(const Tree_index &child) const;
+  const Tree_index get_root() const;
+  void             set_root(const X &data);
 
   void add_lazy_child(const Tree_level &child_level, const X &data);
 
@@ -74,7 +79,7 @@ void Tree<X>::adjust_to_level(Tree_level level) {
 
   while (data_stack.size() <= level) {
     data_stack.emplace_back();
-    parent_stack.emplace_back();
+    pointers_stack.emplace_back();
   }
 };
 
@@ -85,12 +90,12 @@ Tree<X>::Tree() {
 
 template <typename X>
 const Tree_index Tree<X>::add_child(const Tree_index &parent, const X &data) {
-  const auto parent_level = parent.get_level();
-  const auto parent_pos   = parent.get_pos();
+  const auto parent_level = parent.level;
+  const auto parent_pos   = parent.pos;
 
   I(data_stack.size() > parent_level);
   I(data_stack[parent_level].size() > parent_pos);
-  I(data_stack.size() == parent_stack.size());
+  I(data_stack.size() == pointers_stack.size());
 
   auto child_level = parent_level + 1;
 
@@ -100,15 +105,25 @@ const Tree_index Tree<X>::add_child(const Tree_index &parent, const X &data) {
 
   I(pending_parent != child_level);
   data_stack[child_level].emplace_back(data);
-  parent_stack[child_level].emplace_back(Tree_index(parent_level, parent_pos));
+  pointers_stack[child_level].emplace_back(parent_pos);
+
+  if (pointers_stack[parent_level][parent_pos].eldest_child < 0) {
+    I(pointers_stack[parent_level][parent_pos].younger_child < 0);
+    pointers_stack[parent_level][parent_pos].younger_child = child_pos;
+  }else{
+    auto older_sibling = pointers_stack[parent_level][parent_pos].eldest_child;
+    I(pointers_stack[child_level][older_sibling].younger_sibling<0);
+    pointers_stack[child_level][older_sibling].younger_sibling = child_pos;
+  }
+  pointers_stack[parent_level][parent_pos].eldest_child  = child_pos;
 
   return Tree_index(child_level, child_pos);
 };
 
 template <typename X>
 const Tree_index Tree<X>::add_sibling(const Tree_index &sibling, const X &data) {
-  const auto sibling_level = sibling.get_level();
-  const auto sibling_pos   = sibling.get_pos();
+  const auto sibling_level = sibling.level;
+  const auto sibling_pos   = sibling.pos;
 
   I(sibling_level > 0);  // No siblings to root
 
@@ -120,25 +135,34 @@ const Tree_index Tree<X>::add_sibling(const Tree_index &sibling, const X &data) 
 
   I(pending_parent != child_level);
   data_stack[child_level].emplace_back(data);
-  parent_stack[child_level].emplace_back(parent_stack[sibling_level][sibling_pos]);
+
+  auto parent_level = child_level - 1;
+  auto parent_pos   = pointers_stack[sibling_level][sibling_pos].parent;
+
+  pointers_stack[child_level].emplace_back(parent_pos);
+
+  I(pointers_stack[parent_level][parent_pos].eldest_child >= 0);
+  auto older_sibling = pointers_stack[parent_level][parent_pos].eldest_child;
+  pointers_stack[parent_level][parent_pos].eldest_child  = child_pos;
+  pointers_stack[child_level][older_sibling].younger_sibling = child_pos;
 
   return Tree_index(child_level, child_pos);
 }
 
 template <typename X>
 X &Tree<X>::get_data(const Tree_index &leaf) {
-  I(data_stack.size() > leaf.get_level());
-  I(data_stack[leaf.get_level()].size() > leaf.get_pos());
+  I(data_stack.size() > leaf.level);
+  I(data_stack[leaf.level].size() > leaf.pos);
 
-  return data_stack[leaf.get_level()][leaf.get_pos()];
+  return data_stack[leaf.level][leaf.pos];
 }
 
 template <typename X>
 const X &Tree<X>::get_data(const Tree_index &leaf) const {
-  I(data_stack.size() > leaf.get_level());
-  I(data_stack[leaf.get_level()].size() > leaf.get_pos());
+  I(data_stack.size() > leaf.level);
+  I(data_stack[leaf.level].size() > leaf.pos);
 
-  return data_stack[leaf.get_level()][leaf.get_pos()];
+  return data_stack[leaf.level][leaf.pos];
 }
 
 template <typename X>
@@ -150,7 +174,7 @@ void Tree<X>::add_lazy_child(const Tree_level &child_level, const X &data) {
   adjust_to_level(child_level);
 
   Tree_level parent_level = child_level - 1;
-  Tree_pos   parent_pos   = parent_stack[parent_level].size();
+  Tree_pos   parent_pos   = pointers_stack[parent_level].size();
   if (parent_level == 0) {
     parent_pos = parent_pos - 1;  // No lazy root
   } else {
@@ -158,34 +182,64 @@ void Tree<X>::add_lazy_child(const Tree_level &child_level, const X &data) {
     pending_parent = parent_level;
   }
 
+  auto child_pos = data_stack[child_level].size();
+  if (child_pos>0 && pointers_stack[child_level][child_pos-1].parent == parent_pos) {
+    pointers_stack[child_level][child_pos-1].younger_sibling = child_pos;
+  }
+
   data_stack[child_level].emplace_back(data);
-  parent_stack[child_level].emplace_back(Tree_index(parent_level, parent_pos));
+  pointers_stack[child_level].emplace_back(parent_pos);
+
+  if (pointers_stack.size() <= (child_level+1))
+    return;
+
+  if (pointers_stack[child_level+1].empty())
+    return;
+
+  if (pointers_stack[child_level+1].back().parent != child_pos)
+    return;
+  pointers_stack[child_level].back().younger_child = child_pos;
+
+  auto eldest_child = child_pos;
+  for(int i = pointers_stack[child_level+1].size()-1; i>=0 ; --i) {
+    if (pointers_stack[child_level+1][i].parent != child_pos)
+      break;
+    eldest_child = i;
+  }
+
+  pointers_stack[child_level].back().eldest_child = eldest_child;
 }
 
 template <typename X>
-const Tree_index &Tree<X>::get_parent(const Tree_index &child) const {
-  I(child.get_level() < parent_stack.size());
-  I(child.get_pos() < parent_stack[child.get_level()].size());
+const Tree_index Tree<X>::get_parent(const Tree_index &child) const {
+  I(child.level < pointers_stack.size());
+  I(child.pos   < pointers_stack[child.level].size());
 
-  return parent_stack[child.get_level()][child.get_pos()];
+  if (child.level>0)
+    return Tree_index(child.level-1, pointers_stack[child.level][child.pos].parent);
+
+  I(pointers_stack[0].size() == 1); // One single root
+
+  return Tree_index(0,0);
 }
 
 template <typename X>
-const Tree_index &Tree<X>::get_root() const {
-  return parent_stack[0][0];
+const Tree_index Tree<X>::get_root() const {
+  return Tree_index(0,0);
 }
 
 template <typename X>
 void Tree<X>::set_root(const X &data) {
   adjust_to_level(0);
 
-  if (!data_stack[0].empty()) {
+  if (data_stack[0].empty()) {
+    pointers_stack[0].emplace_back(0);
+  }else{
+    I(pointers_stack[0][0].parent == 0);
     data_stack[0].clear();
-    parent_stack[0].clear();
   }
 
   data_stack[0].emplace_back(data);
-  parent_stack[0].emplace_back(0, 0);
 }
 
 template <typename X>
@@ -193,8 +247,14 @@ void Tree<X>::each_bottom_first_fast(std::function<void(const Tree_index &parent
   auto sz = data_stack.size();
   for (int i = sz - 1; i >= 0; --i) {  // WARNING: must be signed to handle -1 for loop exit
     auto sz2 = data_stack[i].size();
+    Tree_level parent_level;
+    if (i>0)
+      parent_level = i - 1;
+    else
+      parent_level = 0;
+
     for (size_t j = 0; j < sz2; j++) {
-      fn(parent_stack[i][j], Tree_index(i, j), data_stack[i][j]);
+      fn(Tree_index(parent_level, pointers_stack[i][j].parent), Tree_index(i, j), data_stack[i][j]);
     }
   }
 }
@@ -204,8 +264,14 @@ void Tree<X>::each_breadth_first_fast(std::function<void(const Tree_index &paren
   auto sz = data_stack.size();
   for (size_t i = 0; i < sz; i++) {
     auto sz2 = data_stack[i].size();
+    Tree_level parent_level;
+    if (i>0)
+      parent_level = i - 1;
+    else
+      parent_level = 0;
+
     for (size_t j = 0; j < sz2; j++) {
-      fn(parent_stack[i][j], Tree_index(i, j), data_stack[i][j]);
+      fn(Tree_index(parent_level, pointers_stack[i][j].parent), Tree_index(i, j), data_stack[i][j]);
     }
   }
 }
@@ -214,12 +280,16 @@ template <typename X>
 const std::vector<Tree_index> Tree<X>::get_children(const Tree_index &top) const {
   std::vector<Tree_index> children;
 
-  I(top.get_level() < parent_stack.size());
-  if (top.get_level() == (parent_stack.size())) return children;
+  // FIXME: build iterator
 
-  for (size_t j = 0; j < parent_stack[top.get_level() + 1].size(); j++) {
-    if (parent_stack[top.get_level() + 1][j].get_pos() == top.get_pos()) children.emplace_back(top.get_level() + 1, j);
+  I(top.level < pointers_stack.size());
+  if (top.level == (pointers_stack.size())) return children;
+
+  for (size_t j = 0; j < pointers_stack[top.level + 1].size(); j++) {
+    if (pointers_stack[top.level + 1][j].parent == top.pos)
+      children.emplace_back(top.level + 1, j);
   }
 
   return children;
 }
+
