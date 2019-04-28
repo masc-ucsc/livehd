ee4e9cba96141a8d167c28e95cb58ecbeb44ac59
Rohan Ganpati <rganpati@ucsc.edu>
2019-04-28 12:03:10 -0700

    Adding them temporarily. To be deleted later.

---
+++ pass/opentimer/ot_examples/simple.cpp
@@ -0,0 +1,54 @@
+// This program demonstrates the basica usage of OpenTimer C++ API
+// on timing report and path reports.
+//
+// Design : simple.v
+// SDC    : simple.sdc
+// Library: osu018_stdcells.lib
+
+#include <ot/timer/timer.hpp>
+
+int main(int argc, char *argv[]) {
+
+  // 
+
+
+  ot::Timer timer;
+  
+  // Read design
+  timer.read_celllib("osu018_stdcells.lib", ot::MIN)
+       .read_celllib("osu018_stdcells.lib", ot::MAX)
+       .read_verilog("simple.v")
+       .read_sdc("simple.sdc");
+
+  // get the top-5 worst critical paths
+  auto paths = timer.report_timing(5);
+
+  for(size_t i=0; i<paths.size(); ++i) {
+    std::cout << "----- Critical Path " << i << " -----\n";
+    std::cout << paths[i] << '\n';
+  }
+
+  // dump the timing graph to dot format for debugging
+  timer.dump_graph(std::cout);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
