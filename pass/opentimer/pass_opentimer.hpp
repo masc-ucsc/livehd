a3f1d4d86bd1671543f3be9cc1eb8934f374cb05
Rohan Ganpati <rganpati@ucsc.edu>
2019-04-29 15:42:43 -0700

    all ot api work except read_sdc...

--- pass/opentimer/pass_opentimer.hpp
+++ pass/opentimer/pass_opentimer.hpp
@@ -4,15 +4,23 @@
 #include "lghierarchy.hpp"
 #include "options.hpp"
 #include "pass.hpp"
+#include "ot/timer/timer.hpp"
 
 class Pass_opentimer : public Pass {
 protected:
+  ot::Timer timer;
 
   static void work(Eprp_var &var);
 
+  //Temporary debug
   void list_cells(LGraph *g);             // To be removed later
-  void file_reader();
-  void circuit_builder();
+  void example_ot();
+
+// Methods to use finally
+  void read_file();
+  void build_circuit();
+  void compute_timing();
+  void populate_table();
 
 public:
   Pass_opentimer();
