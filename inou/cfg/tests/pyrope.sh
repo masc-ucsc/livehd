3a00f864d07a30ba938f0d25e0bbd2c4e03ebf34
Sheng Hong Wang <swang203@ucsc.edu>
2019-05-02 10:15:21 -0700

    nested_if_1 pass!

--- inou/cfg/tests/pyrope.sh
+++ inou/cfg/tests/pyrope.sh
@@ -17,7 +17,7 @@ pwd
 # pts='top_ooo  sp_add  sp_if_0  top  nested_if_0  nested_if_1  nested_if_2  nested_if_3  if_elif_else'
 # pts='top_inline_add constant_pos constant_neg sp_if_0 nested_if_0 nested_if_1 nested_if_2'
 # pts='nested_if_1'
-pts='sp_if_0 sp_if_1 constant_pos sp_and nested_if_0'
+pts='sp_if_0 sp_if_1 constant_pos sp_and nested_if_0 nested_if_1'
 
 LGSHELL=./bazel-bin/main/lgshell
 
