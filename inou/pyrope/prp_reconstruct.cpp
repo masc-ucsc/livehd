1d8e37347c36cf136ed6921078d4863fe16dac35
Kenneth Mayer <krmayer@ucsc.edu>
2019-05-02 23:08:31 +0000

    remove erroneous files

--- inou/pyrope/prp_reconstruct.cpp
+++
@@ -1,5 +0,0 @@
-#include "prp_reconstruct.hpp"
-
-prp_reconstruct::reconstruct_from_ast(std::unique_ptr<Ast_parser> ast){
-  
-}
--- inou/pyrope/prp_reconstruct.hpp
+++
@@ -1,8 +0,0 @@
-#include "prp.hpp"
-
-class Prp_reconstruct{
-protected:
-  
-public:
-  reconstruct_from_ast(std::unique_ptr<Ast_parser> ast);
-}
