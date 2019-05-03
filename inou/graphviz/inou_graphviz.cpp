5770c85bc84d25ffa37d4cba87dabb1e409736e8
Jose Renau <renau@ucsc.edu>
2019-05-03 16:55:29 -0700

    update graphviz for talk

--- inou/graphviz/inou_graphviz.cpp
+++ inou/graphviz/inou_graphviz.cpp
@@ -17,7 +17,6 @@ void setup_inou_graphviz() {
 }
 
 void Inou_graphviz::setup() {
-
   Eprp_method m2("inou.graphviz", "export lgraph to graphviz dot format", &Inou_graphviz::fromlg);
 
   m2.add_label_optional("bits", "dump bits (true/false)", "false");
@@ -26,27 +25,23 @@ void Inou_graphviz::setup() {
   register_inou(m2);
 }
 
-Inou_graphviz::Inou_graphviz()
-    : Pass("graphviz") {
-
-  bits = false;
+Inou_graphviz::Inou_graphviz() : Pass("graphviz") {
+  bits    = false;
   verbose = false;
 }
 
 void Inou_graphviz::fromlg(Eprp_var &var) {
-
   Inou_graphviz p;
 
-  p.odir = var.get("odir");
-  p.bits = var.get("bits") == "true";
+  p.odir    = var.get("odir");
+  p.bits    = var.get("bits") == "true";
   p.verbose = var.get("verbose") == "true";
 
   bool ok = p.setup_directory(p.odir);
-  if(!ok)
-    return;
+  if (!ok) return;
 
   std::vector<LGraph *> lgs;
-  for(const auto &l : var.lgs) {
+  for (const auto &l : var.lgs) {
     lgs.push_back(l);
   }
 
@@ -54,17 +49,16 @@ void Inou_graphviz::fromlg(Eprp_var &var) {
 }
 
 void Inou_graphviz::do_fromlg(std::vector<LGraph *> &lgs) {
-
-  for(const auto g : lgs) {
+  for (const auto g : lgs) {
     const auto hier = g->get_hierarchy();
-    fmt::print("hierarchy for {}\n",g->get_name());
-    for(auto &[name,lgid]:hier) {
-      fmt::print("  {} {}\n",name,lgid);
+    fmt::print("hierarchy for {}\n", g->get_name());
+    for (auto &[name, lgid] : hier) {
+      fmt::print("  {} {}\n", name, lgid);
     }
   }
-  for(const auto lg_parent : lgs) {
+  for (const auto lg_parent : lgs) {
     populate_data(lg_parent);
-    lg_parent->each_sub_graph_fast([lg_parent,this](Node node, Lg_type_id lgid){
+    lg_parent->each_sub_graph_fast([lg_parent, this](Node node, Lg_type_id lgid) {
       fmt::print("subgraph lgid:{}\n", lgid);
       LGraph *lg_child = LGraph::open(lg_parent->get_path(), lgid);
       populate_data(lg_child);
@@ -72,46 +66,41 @@ void Inou_graphviz::do_fromlg(std::vector<LGraph *> &lgs) {
   }
 }
 
-void Inou_graphviz::populate_data(LGraph* g){
+void Inou_graphviz::populate_data(LGraph *g) {
   std::string data = "digraph {\n";
 
-
   g->each_node_fast([&data](const Node &node) {
-    const auto &ntype = node.get_type();
     auto node_name = node.has_name() ? node.get_name() : "";
 
-    //SH:FIXME:ASK: bug!! for graph io, visit two nodes?
-    data += fmt::format(" {} [label=\"{} :{} :{}\"];\n", node.debug_name(), node.debug_name(), ntype.get_name(), node_name);
+    data += fmt::format(" {} [label=\"{} :{} :{}\"];\n"
+           , node.debug_name(), node.debug_name(), node.get_type().get_name(), node_name);
 
-    for(auto &out : node.out_edges()){
-      Node_pin dpin = out.driver;
-      Node_pin spin = out.sink;
-      auto dnode_name = dpin.get_node().debug_name();
-      auto snode_name = spin.get_node().debug_name();
-      auto dpin_name = dpin.has_name() ? dpin.get_name() : ""; // only driver pin has name attribute
-      auto bits = dpin.get_bits();
+    for (auto &out : node.out_edges()) {
+      auto &dpin       = out.driver;
+      auto  dnode_name = dpin.get_node().debug_name();
+      auto  snode_name = out.sink.get_node().debug_name();
+      auto  dpin_name  = dpin.has_name() ? dpin.get_name() : "";
+      auto  bits       = dpin.get_bits();
 
-        data += fmt::format(" {}->{}[label=\"{}b :{} :{} :{}\"];\n",
-                dnode_name, snode_name, bits, dpin.get_pid(), spin.get_pid(), dpin_name);
+      data += fmt::format(" {}->{}[label=\"{}b :{} :{} :{}\"];\n"
+          , dnode_name, snode_name, bits, dpin.get_pid(), out.sink.get_pid(), dpin_name);
     }
   });
 
   g->each_graph_output([&data](const Node_pin &pin) {
     std::string_view dst_str = "virtual_dst_module";
-    auto bits = pin.get_bits();
-      data += fmt::format(" {}->{}[label=\"{}b\"];\n", pin.get_node().debug_name(), dst_str, bits);
+    auto             bits    = pin.get_bits();
+    data += fmt::format(" {}->{}[label=\"{}b\"];\n", pin.get_node().debug_name(), dst_str, bits);
   });
-
 
   data += "}\n";
 
   std::string file = absl::StrCat(odir, "/", g->get_name(), ".dot");
   int         fd   = ::open(file.c_str(), O_CREAT | O_WRONLY | O_TRUNC, 0644);
-  if(fd < 0) {
+  if (fd < 0) {
     Pass::error("inou.graphviz unable to create {}", file);
     return;
   }
   write(fd, data.c_str(), data.size());
   close(fd);
 }
-
