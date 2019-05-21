8f09856c9e379249f671670d7ff22cd11d6d368c
Sheng Hong Wang <swang203@ucsc.edu>
2019-05-21 00:53:38 -0700

    lnast: node type analysis function

--- elab/lnast.cpp
+++ elab/lnast.cpp
@@ -4,7 +4,7 @@
 //------------- Language_neutral_ast member function start -----
 Language_neutral_ast::Language_neutral_ast(std::string_view _buffer, Lnast_ntype_id ntype_top) : buffer(_buffer) {
   I(!buffer.empty());
-  set_root(Lnast_node(ntype_top,0));
+  set_root(Lnast_node(ntype_top, 0, 0));
 }
 
 
@@ -12,24 +12,148 @@ Language_neutral_ast::Language_neutral_ast(std::string_view _buffer, Lnast_ntype
 
 void Lnast_parser::elaborate(){
   lnast = std::make_unique<Language_neutral_ast>(get_buffer(), Lnast_ntype_top);
-  build_statement();
+  build_statements(0);
 }
 
 
-void Lnast_parser::build_statement(){
+void Lnast_parser::build_statements(Scope_id scope){
+  auto tree_idx_sts  = lnast->add_child(lnast->get_root(), Lnast_node(Lnast_ntype_statement, scan_token(), scope));
+  add_statement(tree_idx_sts, scope);
+}
 
-  fmt::print("statement:{}, line:{}\n", scan_text(), line_num);
-  lnast->add_child(lnast->get_root(), Lnast_node(Lnast_ntype_statement, scan_token()));
+void Lnast_parser::add_statement(const Tree_index &tree_idx_sts, Scope_id cur_scope) {
+  fmt::print("line:{}, statement:{}\n", line_num, scan_text());
+
+  int line_tkcnt = 0;
+  Token_entry    node_name;
+  Lnast_ntype_id node_type;
+  Token_entry    cfg_token_beg;
+  Token_entry    cfg_token_end;
+  Scope_id       token_scope;
 
   while(line_num == scan_calc_lineno()){
-    if(scan_is_end())
-      return;
+    if(scan_is_end()) return;
+
+    line_tkcnt += 1;
+    switch (line_tkcnt) {
+      case CFG_NODE_NAME_POS:
+        node_name = scan_token();  //must be a complete alnum
+        break;
+      case CFG_SCOPE_ID_POS:
+        token_scope = process_scope(cur_scope); //recursive build subgraph here
+        break;
+      case CFG_TOKEN_POS_BEG:
+        cfg_token_beg = scan_token(); //must be a complete alnum
+        break;
+      case CFG_TOKEN_POS_END:
+        cfg_token_end = scan_token(); //must be a complete alnum
+        break;
+      case CFG_OP_POS_BEG:
+        //note: no regulation after operator position
+        operator_analysis(node_type, line_tkcnt);
+        break;
+      default: add_operator_subtree(node_type, line_tkcnt);
+    }
 
     fmt::print("token:{}\n", scan_text());
     scan_next();
   }
 
   line_num += 1;
-  build_statement();
+  add_statement(tree_idx_sts, cur_scope);
 }
 
+Scope_id Lnast_parser::process_scope(Scope_id cur_scope) {
+  auto token_scope= (uint8_t)std::stoi(scan_text());
+  if(token_scope == cur_scope){
+    return token_scope;
+  } else if (token_scope < cur_scope){
+    for(int i = 0; i < CFG_SCOPE_ID_POS; i++) scan_prev();
+    //going back to parent scope
+    return token_scope;
+  } else {//(token_scope > cur_scope)
+    for(int i = 0; i < CFG_SCOPE_ID_POS; i++) scan_prev();
+    add_subgraph(); //SH:FIXME: deal with subgraph later
+    return token_scope;
+  }
+}
+
+void Lnast_parser::add_subgraph() {
+  ;
+}
+
+void Lnast_parser::operator_analysis(Lnast_ntype_id & node_type, int& line_tkcnt) {
+  if (scan_is_token(Token_id_op)) { //deal with ()
+    node_type = Lnast_ntype_tuple; // must be a tuple op
+    I(scan_next_token_is(Token_id_cp));
+    scan_next();
+    line_tkcnt += 1;
+  } else if (scan_is_token(Token_id_colon)) {
+      if (scan_next_token_is(Token_id_colon)) { //deal with ::{
+        node_type = Lnast_ntype_func_def;
+        scan_next();
+        I(scan_next_token_is(Token_id_ob)); //must be a function def op
+        scan_next();
+        line_tkcnt += 2;
+      } else if (scan_next_token_is(Token_id_eq)) { //deal with :=
+        node_type = Lnast_ntype_dp_assign;
+        scan_next();
+        line_tkcnt += 1;
+      } else {
+        node_type = Lnast_ntype_lable;
+      }
+  } else if (scan_is_token(Token_id_dot)) { //deal with .()
+      if (scan_next_token_is(Token_id_op)){
+        node_type = Lnast_ntype_func_call; // must be a function call op
+        scan_next();
+        I(scan_next_token_is(Token_id_cp)); //must be a function def op
+        scan_next();
+        line_tkcnt += 2;
+      } else {
+        node_type = Lnast_ntype_dot;
+      }
+  } else if (scan_is_token(Token_id_alnum) && scan_text() == "as") {
+    node_type = Lnast_ntype_as;
+  } else if (scan_is_token(Token_id_alnum) && scan_text() == "for") {
+    node_type = Lnast_ntype_for;
+  } else if (scan_is_token(Token_id_alnum) && scan_text() == "while") {
+    node_type = Lnast_ntype_while;
+  } else if (scan_is_token(Token_id_alnum) && scan_text() == "if") {
+    node_type = Lnast_ntype_if;
+  } else if (scan_is_token(Token_id_alnum) && scan_text() == "uif") {
+    node_type = Lnast_ntype_uif;
+  } else if (scan_is_token(Token_id_alnum) && scan_text() == "I") {
+    node_type = Lnast_ntype_assert;
+  } else if (scan_is_token(Token_id_eq)) {
+    node_type = Lnast_ntype_pure_assign;
+  } else if (scan_is_token(Token_id_and)) {
+    node_type = Lnast_ntype_and;
+  } else if (scan_is_token(Token_id_or)) {
+    node_type = Lnast_ntype_or;
+  } else if (scan_is_token(Token_id_xor)) {
+    node_type = Lnast_ntype_xor;
+  } else if (scan_is_token(Token_id_plus)) {
+    node_type = Lnast_ntype_plus;
+  } else if (scan_is_token(Token_id_minus)) {
+    node_type = Lnast_ntype_minus;
+  } else if (scan_is_token(Token_id_mult)) {
+    node_type = Lnast_ntype_mult;
+  } else if (scan_is_token(Token_id_div)) {
+    node_type = Lnast_ntype_div;
+  } else if (scan_is_token(Token_id_eq)) {
+    node_type = Lnast_ntype_eq;
+  } else if (scan_is_token(Token_id_le)) {
+    node_type = Lnast_ntype_le;
+  } else if (scan_is_token(Token_id_lt)) {
+    node_type = Lnast_ntype_lt;
+  } else if (scan_is_token(Token_id_ge)) {
+    node_type = Lnast_ntype_ge;
+  } else if (scan_is_token(Token_id_gt)) {
+    node_type = Lnast_ntype_gt;
+  } else {
+    node_type = Lnast_ntype_invalid;
+  }
+}
+void Lnast_parser::add_operator_subtree(Lnast_ntype_id & node_type, int& line_tkcnt) {
+  ;
+}
--- elab/lnast.hpp
+++ elab/lnast.hpp
@@ -4,13 +4,21 @@
 #include "elab_scanner.hpp"
 #include "tree.hpp"
 
-//using Lnast_ntype_id = Explicit_type<int8_t, struct Lnast_ntype_id_struct>;
 using Lnast_ntype_id = uint8_t ;
+//using Scope_id       = Explicit_type<uint8_t, struct Scope_ID_struct>;
+using Scope_id       = uint8_t ;
+
+static inline constexpr int   CFG_NODE_NAME_POS    = 1;
+static inline constexpr int   CFG_SCOPE_ID_POS     = 3;
+static inline constexpr int   CFG_TOKEN_POS_BEG    = 4;
+static inline constexpr int   CFG_TOKEN_POS_END    = 5;
+static inline constexpr int   CFG_OP_POS_BEG       = 6;
 
 struct Lnast_node {
   const Lnast_ntype_id   node_type;
-  const Token_entry      token_entry;
-  Lnast_node(Lnast_ntype_id node_type, Token_entry te) : node_type(node_type), token_entry(te) {
+  const Token_entry      node_name;
+  const Scope_id         scope;
+  Lnast_node(Lnast_ntype_id node_type, Token_entry te, Scope_id scope):node_type(node_type), node_name(te), scope(scope) {
     I(node_type);
   }
 };
@@ -36,29 +44,49 @@ protected:
     Lnast_ntype_invalid = 0,  // zero is not a valid Lnast_ntype_id
     Lnast_ntype_statement,
     Lnast_ntype_pure_assign,
-    Lnast_ntype_dp_assign,    //dp = deprecate
-    Lnast_ntype_as,
+    Lnast_ntype_dp_assign,    // :=, dp = deprecate
+    Lnast_ntype_as,           // as
+    Lnast_ntype_lable,        // :
+    Lnast_ntype_dot,          // .
     Lnast_ntype_logical_and,
     Lnast_ntype_logical_or,
-    Lnast_ntype_arith,
+    Lnast_ntype_and,
+    Lnast_ntype_or,
+    Lnast_ntype_xor,
+    Lnast_ntype_plus,
+    Lnast_ntype_minus,
+    Lnast_ntype_mult,
+    Lnast_ntype_div,
+    Lnast_ntype_eq,
+    Lnast_ntype_lt,
+    Lnast_ntype_le,
+    Lnast_ntype_gt,
+    Lnast_ntype_ge,
+    Lnast_ntype_tuple,        // ()
     Lnast_ntype_ref,
     Lnast_ntype_const,
-    Lnast_ntype_attr_bits,
-    Lnast_ntype_assert,
+    Lnast_ntype_attr_bits,    // __bits
+    Lnast_ntype_assert,       // I
     Lnast_ntype_if,
     Lnast_ntype_uif,
-    Lnast_ntype_tuple,
-    Lnast_ntype_funccall,
-    Lnast_ntype_funcdef,
+    Lnast_ntype_for,
+    Lnast_ntype_while,
+    Lnast_ntype_func_call,    // .()
+    Lnast_ntype_func_def,     // ::{
     Lnast_ntype_sub,
     Lnast_ntype_top
   };
 
 
-  void elaborate() override;
-  void build_statement();
+  void         elaborate() override;
+  void         build_statements(Scope_id scope);
+  void         add_statement(const Tree_index& statements, Scope_id cur_scope);
+  void         add_subgraph();
+  Scope_id     process_scope(Scope_id cur_scope);
+  void         operator_analysis(Lnast_ntype_id & node_type, int& line_tkcnt);
+  void         add_operator_subtree(Lnast_ntype_id & node_type, int& line_tkcnt);
 
 private:
   std::unique_ptr<Language_neutral_ast>  lnast;
-  uint32_t                               line_num;
+  int                                    line_num;
 };
