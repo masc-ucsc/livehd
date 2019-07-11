e6045c48790fc93884967d112712b87f75716859
Sheng Hong Wang <swang203@ucsc.edu>
2019-07-10 17:38:36 -0700

    lnast:nested-if-else pass! next: SSA transformation

--- elab/lnast_parser.hpp
+++ elab/lnast_parser.hpp
@@ -17,7 +17,7 @@ static inline constexpr int   CFG_TARGET_TMP_REF_RANGE =  1; //K14 K15  0 59 96 
 
 class Lnast_parser : public Elab_scanner {
 public:
-  Lnast_parser() : line_num(0), line_tkcnt(0) { setup_ntype_str_mapping();};
+  Lnast_parser() : line_num(0), line_tkcnt(0), knum1(0), knum2(0) { setup_ntype_str_mapping();};
   const std::unique_ptr<Language_neutral_ast<Lnast_node>>&  get_ast(){return lnast;};
   std::string                                               ntype_dbg(Lnast_ntype_id ntype);
 
@@ -25,25 +25,29 @@ protected:
   void       elaborate() override;
   void       build_top_statements              (const Tree_index& tree_idx_top);
   void       add_statement                     (const Tree_index& tree_top_sts);
-  Scope_id   process_scope                     ();
   void       process_assign_like_op            (const Tree_index& tree_idx_opr, const Token& target_name);
   void       process_label_op                  (const Tree_index& tree_idx_opr, const Token& target_name);
   void       process_binary_op                 (const Tree_index& tree_idx_opr, const Token& target_name);
   void       process_func_call_op              (const Tree_index& tree_idx_opr, const Token& target_name);
   void       process_func_def_op               (const Tree_index& tree_idx_opr, const Token& target_name);
   void       process_if_op                     (const Tree_index& tree_idx_opr, const Token& target_name);
-  Tree_index process_operator_node (const Tree_index& opr_parent_sts, Lnast_ntype_id type, uint32_t knum1, uint32_t knum2);
   void       add_operator_subtree              (const Tree_index& tree_idx_opr, const Token& target_name);
+  Tree_index process_operator_node             (const Tree_index& opr_parent_sts, Lnast_ntype_id type);
+  Scope_id        process_scope();
   Lnast_ntype_id  operand_analysis();
   Lnast_ntype_id  operator_analysis();
-  void            function_name_correction(Lnast_ntype_id type, const Tree_index& sts_idx);
+  bool            token_is_valid_ref();
   void            setup_ntype_str_mapping();
-
+  void            function_name_correction(Lnast_ntype_id type, const Tree_index& sts_idx);
+  void            process_if_else_sts_range(const Tree_index& tree_idx_if);
+  void            final_else_sts_type_correction();
 private:
   std::unique_ptr<Language_neutral_ast<Lnast_node>>   lnast;
   absl::flat_hash_map<Lnast_ntype_id , std::string>   ntype2str;
   uint32_t line_num;
   uint8_t  line_tkcnt;
+  uint32_t knum1;
+  uint32_t knum2;//for if-else or fdef chunk
   std::vector<std::tuple<Tree_index, Lnast_ntype_id, uint32_t, uint32_t>> range_stack; //(parent_sts, sts_type, min, max)
 };
 
