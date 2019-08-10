3546957bfcec5066e0bfcde9ed7ea10fce82ced6
Jose Renau <renau@ucsc.edu>
2019-08-10 09:54:14 -0700

    patch coverage scripts

--- scripts/gencoveralls.sh
+++ scripts/gencoveralls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for a in cloud cops core eprp inou live main meta pass
+for a in cops core eprp inou live main pass
 do
   lcov --ignore-errors source --capture --rc geninfo_auto_base=1 --rc lcov_branch_coverage=1 --compat-libtool --base-directory . --directory bazel-out/k8-fastbuild/bin/${a} --exclude '/usr/*' --exclude 'external/*' --output-file cov/coverage_${a}.info
 echo $a
@@ -11,7 +11,7 @@ if [ -s cov/coverage.info ]; then
   cp -a cov/coverage.info cov/coverage_prev_run.info
 fi
 LCOV_ADD=""
-for a in cloud cops core eprp inou live main meta pass prev_run
+for a in cops core eprp inou live main pass
 do
   if [ -s cov/coverage_${a}.info ]; then
     LCOV_ADD="${LCOV_ADD} --add-tracefile cov/coverage_${a}.info"
