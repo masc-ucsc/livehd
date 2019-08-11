14b7a573d898268aabbc8433f233151a9756cc05
Jose Renau <renau@ucsc.edu>
2019-08-11 16:53:56 -0700

    patch coverage path

--- scripts/gencoveralls.sh
+++ scripts/gencoveralls.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-lcov --ignore-errors source --capture --rc geninfo_auto_base=1 --rc lcov_branch_coverage=1 --compat-libtool --base-directory . --directory bazel-out/k8-fastbuild/bin/ --exclude '/usr/*' --exclude 'external/*' --output-file cov/coverage_all.info 2>cov_warnings.log
+lcov --ignore-errors source --capture --rc geninfo_auto_base=1 --rc lcov_branch_coverage=1 --compat-libtool --base-directory . --directory bazel-out/k8-fastbuild/bin/ --exclude '/usr/*' --exclude '*external*' --output-file cov/coverage_all.info 2>cov_warnings.log
 
 if [ -s cov/coverage.info ]; then
   cp -a cov/coverage.info cov/coverage_prev_run.info
