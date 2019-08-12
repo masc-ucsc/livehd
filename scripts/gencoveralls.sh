93b847e8a82a787a7ccca4412aaf860c3c5a0727
Jose Renau <renau@ucsc.edu>
2019-08-11 22:41:00 -0700

    simplify coverage command

--- scripts/gencoveralls.sh
+++ scripts/gencoveralls.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-lcov --ignore-errors source --capture --rc geninfo_auto_base=1 --rc lcov_branch_coverage=1 --compat-libtool --base-directory . --directory bazel-out/k8-fastbuild/bin/ --exclude '/usr/*' --exclude '*external*' --output-file cov/coverage_all.info 2>cov_warnings.log
+lcov --ignore-errors source --capture --rc geninfo_auto_base=1 --rc lcov_branch_coverage=1 --compat-libtool --base-directory . --directory bazel-out/k8-fastbuild/bin/ --exclude '/usr/*' --exclude '*external*' --output-file cov/coverage.info 2>cov_warnings.log
 
-if [ -s cov/coverage.info ]; then
-  cp -a cov/coverage.info cov/coverage_prev_run.info
-  lcov cov/coverage_prev_run.info cov/coverage_all.info --output-file cov/coverage.info
-else
-  cp -a cov/coverage_all.info cov/coverage.info
-fi
-
+#  lcov cov/coverage_prev_run.info cov/coverage_all.info --output-file cov/coverage.info
