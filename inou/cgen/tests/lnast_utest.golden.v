1b140bc61c42afc14f0e6007559890ed0c48f4b2
Joshua Pena <penacalata@gmail.com>
2019-10-14 16:06:25 -0700

    fixed the verilog golden file

--- inou/cgen/tests/lnast_utest.golden.v
+++ inou/cgen/tests/lnast_utest.golden.v
@@ -1,39 +1,39 @@
 module lnast_utest (input clk,
                     input reset,
-                    input [0] $a,
-                    input [0] $b,
-                    input     $c,
-                    input     $d,
-                    input     $e,
-                    input     $f,
-                    output     %o1,
-                    output     %o2,
-                    output [0] %s);
+                    input [0] a_i,
+                    input [0] b_i,
+                    input     c_i,
+                    input     d_i,
+                    input     e_i,
+                    input     f_i,
+                    output     o1_o,
+                    output     o2_o,
+                    output [0] s_o);
   wire result;
   wire x;
   wire y;
 
   always @(*) begin
-    *(LNAST: $a as __bits:1)*
-    *(LNAST: $b as __bits:1)*
-    *(LNAST: %s as __bits:1)*
-    %s = $a & $b;
+    *(LNAST: a_i as __bits:1)*
+    *(LNAST: b_i as __bits:1)*
+    *(LNAST: s_o as __bits:1)*
+    s_o = a_i & b_i;
     result = lnast_utest_fun1(clk, reset, a:3, b:4);
-    x = $a;
-    if ($a > 1) {
-      x = $e;
-      if ($a > 2) {
-        x = $b;
-      } elif ($a + 1 > 3) {
-        x = $c;
+    x = a_i;
+    if (a_i > 1) {
+      x = e_i;
+      if (a_i > 2) {
+        x = b_i;
+      } elif (a_i + 1 > 3) {
+        x = c_i;
       } else {
-        x = $d;
+        x = d_i;
       }
-      y = $e;
+      y = e_i;
     } else {
-      x = $f;
+      x = f_i;
     }
-    %o1 = x + $a;
-    %o2 = y + $a;
+    o1_o = x + a_i;
+    o2_o = y + a_i;
   end
 end module
