b92cd398b01794df2fa5b55048f8a2ceb6ab91e6
Sheng Hong Wang <swang203@ucsc.edu>
2019-12-04 00:14:50 -0800

    structure of phi-node-resovling method

--- elab/lnast.cpp
+++ elab/lnast.cpp
@@ -1,3 +1,4 @@
+#include <mmap_vector.hpp>
 #include "lnast.hpp"
 
 void Lnast_node::dump() const {
@@ -29,6 +30,7 @@ void Lnast::ssa_if_subtree(const Lnast_nid &if_node){
     if (get_data(itr).type.is_statements()) {
       absl::flat_hash_map<std::string_view, uint8_t> if_sts_phi_resolve_table;
       phi_resolve_tables[get_data(itr).token.get_text(buffer)] = if_sts_phi_resolve_table;
+
       for (const auto &opr_node : children(itr)) {
         I(!get_data(opr_node).type.is_func_def());
         if (get_data(opr_node).type.is_if())
@@ -38,13 +40,35 @@ void Lnast::ssa_if_subtree(const Lnast_nid &if_node){
       }
     }
   }
-  phi_node_insertion(if_node);
+  phi_node_resolve(if_node);
 }
 
-void Lnast::phi_node_insertion(const Lnast_nid &if_node) {
-  ;
+void Lnast::phi_node_resolve(const Lnast_nid &if_node) {
+  std::vector<Lnast_nid> if_statements_vec;
+  for (const auto & itr : children(if_node)) {
+    if (get_data(itr).type.is_statements()) {
+      if_statements_vec.push_back(itr);
+    }
+  }
+
+  for (auto itr = if_statements_vec.rbegin(); itr != if_statements_vec.rend(); ++itr ) {
+    if (itr == if_statements_vec.rbegin()) {
+      if (has_else_statements(if_node)) {
+        ;//handle last two statements first
+      } else {
+        ;//handle last and upper-scope statements first
+      }
+    } else {
+      ;//resolve normal case
+    }
+  }
 }
 
+bool Lnast::has_else_statements(const Lnast_nid &if_node) {
+    Lnast_nid last_child = get_last_child(if_node);
+    Lnast_nid second_last_child = get_sibling_prev(last_child);
+  return !(get_data(last_child).type.is_statements() and get_data(second_last_child).type.is_cstatements());
+}
 
 void Lnast::ssa_handle_statement(const Lnast_nid &psts_node, const Lnast_nid &opr_node){
   const auto type = get_data(opr_node).type;
@@ -74,9 +98,9 @@ void Lnast::update_ssa_cnt_table(Lnast_node& target_data){
 }
 
 
-//sh:fixme:cannot using Tree_index as absl index...
+//sh:fixme:currently we cannot use Tree_index as absl index...
 void Lnast::update_phi_resolve_table(const Lnast_nid &psts_node, Lnast_node& target_data){
-  const auto  target_name = target_data.token.get_text(buffer);
+  const auto target_name = target_data.token.get_text(buffer);
   fmt::print("phi_resovle_table:{}\n", get_data(psts_node).token.get_text(buffer));
   auto &phi_resolve_table = phi_resolve_tables[get_data(psts_node).token.get_text(buffer)];
   phi_resolve_table[target_name] = target_data.subs;
--- elab/lnast.hpp
+++ elab/lnast.hpp
@@ -39,12 +39,13 @@ public:
 private:
   const std::string_view buffer;  // const because it can not change at runtime
   void do_ssa_trans                  (const Lnast_nid &top);
-  void ssa_handle_statement             (const Lnast_nid &psts_node, const Lnast_nid &opr_node);
+  void ssa_handle_statement          (const Lnast_nid &psts_node, const Lnast_nid &opr_node);
   void ssa_if_subtree                (const Lnast_nid &if_node);
-  void phi_node_insertion            (const Lnast_nid &if_node);
+  void phi_node_resolve              (const Lnast_nid &if_node);
   bool elder_sibling_is_label        (const Lnast_nid &self);
   void update_ssa_cnt_table          (Lnast_node& target_data);
   void update_phi_resolve_table      (const Lnast_nid &psts_node, Lnast_node& target_data);
+  bool has_else_statements           (const Lnast_nid &if_node);
 
   absl::flat_hash_map<std::string_view, absl::flat_hash_map<std::string_view, uint8_t>> phi_resolve_tables;
   absl::flat_hash_map<std::string_view, uint8_t> ssa_cnt_table;
