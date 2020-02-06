a1020d6a66727a445299fd6e2c8b853e71094674
Huijie Pan <hpan5@mada0.soe.ucsc.edu>
2020-02-05 23:35:16 -0800

    setup.sh update, compile rw failed still

--- inou/rapidwright/setup.sh
+++ inou/rapidwright/setup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-wget https://github.com/Xilinx/RapidWright
+git clone https://github.com/Xilinx/RapidWright.git
+cd RapidWright
+export RAPIDWRIGHT_PATH=$(pwd)
+wget https://github.com/Xilinx/RapidWright/releases/download/v2019.2.0-beta/rapidwright_data.zip
+unzip rapidwright_data.zip
+wget https://github.com/Xilinx/RapidWright/releases/download/v2019.2.0-beta/rapidwright_jars.zip
+unzip rapidwright_jars.zip
+
+export CLASSPATH=$RAPIDWRIGHT_PATH/bin:$(echo $RAPIDWRIGHT_PATH/jars/*.jar | tr ' ' ':')
+gradle build -p $RAPIDWRIGHT_PATH
