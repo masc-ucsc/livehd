1ec556dad9ca833d906bf680446738edc0b6e5ca
Jose Renau <renau@ucsc.edu>
2020-05-02 13:16:41 -0700

    add yosys to the simlib benchmarking (c++ backend)

--- simlib/example/verilog_yosys/main.cpp
+++ simlib/example/verilog_yosys/main.cpp
@@ -4,9 +4,9 @@ uint64_t global_time   = 0;
 
 void advance_clock(cxxrtl_design::p_sample &top) {
 
-  top.p_clk.next = value<1> {1u};
+  top.p_clk = value<1> {1u};
   top.step();
-  top.p_clk.next = value<1> {0u};
+  top.p_clk = value<1> {0u};
   top.step();
 
   global_time++;
@@ -15,12 +15,12 @@ void advance_clock(cxxrtl_design::p_sample &top) {
 int main() {
   cxxrtl_design::p_sample sample;
 
-  sample.p_clk.next = value<1> {0u};
-  sample.p_reset.next = value<1> {1u};
+  sample.p_clk = value<1> {0u};
+  sample.p_reset = value<1> {1u};
   while(global_time<1000) {
     advance_clock(sample);
   }
-  sample.p_reset.next = value<1> {0u};
+  sample.p_reset = value<1> {0u};
 
   while(global_time < 100000000) {
     advance_clock(sample);
