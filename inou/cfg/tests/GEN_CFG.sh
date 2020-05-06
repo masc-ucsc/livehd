c85419e29237954640be5adc78d6f2e4a24043af
Sheng Hong Wang <swang203@ucsc.edu>
2020-05-06 09:57:03 -0700

    bring back cfg generate function for cfg patterns

--- inou/cfg/tests/GEN_CFG.sh
+++ inou/cfg/tests/GEN_CFG.sh
@@ -5,22 +5,29 @@ function generate () {
   ./prp $1.prp | sort -n > y && column -t y > x && rm -f y && mv x $1.cfg
 }
 
-# wait for no bug cfg = ="
-# generate function_call
-# generate nested_if
-# generate tuple
-# generate ssa_if
-# generate ssa_nested_if
-# generate ssa_no_else_if
-./prp simple_tuple.prp  | sort -n  > y && column -t y > x && rm -f y && mv x simple_tuple.cfg
-./prp function_call.prp | sort -n  > y && column -t y > x && rm -f y && mv x function_call.cfg
-./prp tuple.prp         | sort -n  > y && column -t y > x && rm -f y && mv x tuple.cfg
-./prp tuple_if.prp      | sort -n  > y && column -t y > x && rm -f y && mv x tuple_if.cfg
-./prp tuple_if2.prp     | sort -n  > y && column -t y > x && rm -f y && mv x tuple_if2.cfg
-./prp if.prp            | sort -n  > y && column -t y > x && rm -f y && mv x if.cfg
-./prp if2.prp           | sort -n  > y && column -t y > x && rm -f y && mv x if2.cfg
-./prp if3_err.prp       | sort -n  > y && column -t y > x && rm -f y && mv x if3_err.cfg
-./prp nested_if.prp     | sort -n  > y && column -t y > x && rm -f y && mv x nested_if.cfg
-./prp nested_if_err.prp | sort -n  > y && column -t y > x && rm -f y && mv x nested_if_err.cfg
+generate simple_tuple
+generate function_call
+generate tuple
+generate tuple_if
+generate tuple_if2
+generate nested_if
+generate if
+generate if2
+generate if3_err
+generate nested_if
+generate nested_if_err
+generate ssa_rhs
+generate not
+
+# ./prp simple_tuple.prp  | sort -n  > y && column -t y > x && rm -f y && mv x simple_tuple.cfg
+# ./prp function_call.prp | sort -n  > y && column -t y > x && rm -f y && mv x function_call.cfg
+# ./prp tuple.prp         | sort -n  > y && column -t y > x && rm -f y && mv x tuple.cfg
+# ./prp tuple_if.prp      | sort -n  > y && column -t y > x && rm -f y && mv x tuple_if.cfg
+# ./prp tuple_if2.prp     | sort -n  > y && column -t y > x && rm -f y && mv x tuple_if2.cfg
+# ./prp if.prp            | sort -n  > y && column -t y > x && rm -f y && mv x if.cfg
+# ./prp if2.prp           | sort -n  > y && column -t y > x && rm -f y && mv x if2.cfg
+# ./prp if3_err.prp       | sort -n  > y && column -t y > x && rm -f y && mv x if3_err.cfg
+# ./prp nested_if.prp     | sort -n  > y && column -t y > x && rm -f y && mv x nested_if.cfg
+# ./prp nested_if_err.prp | sort -n  > y && column -t y > x && rm -f y && mv x nested_if_err.cfg
 
 
