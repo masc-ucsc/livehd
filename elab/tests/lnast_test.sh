62f60dc55ea0d58b6d362479937c68aed406c6a1
Sheng Hong Wang <swang203@ucsc.edu>
2020-05-06 10:38:55 -0700

    upload not pattern

--- elab/tests/lnast_test.sh
+++ elab/tests/lnast_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf ./lgdb
 
-pts='if if2 if3_err nested_if_err nested_if ssa_rhs'
+pts='not if if2 if3_err nested_if_err nested_if ssa_rhs'
 # pus='tuple_if2'
 # pts='tuple_if2 tuple_if ssa_rhs ssa_nested_if ssa_if nested_if tuple simple_tuple function_call tuple '
 # pts='ssa_rhs'
@@ -199,6 +199,7 @@ do
     rm -f lnast.dot.gld
     rm -f lnast.nodes
     rm -f lnast.nodes.gld
+    rm -f *.dot
 done #end of for
 
 
