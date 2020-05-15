e63600e81e198864aeb2593703f5f5825d48de0e
sakshi garg <sgarg3@ucsc.edu>
2020-05-14 18:31:29 -0700

    minor fix for env variable

--- simlib/example/simlib/main.cpp
+++ simlib/example/simlib/main.cpp
@@ -7,7 +7,11 @@
 int main(int argc, char **argv) {
   Simlib_checkpoint<Sample_stage> top("ckpt");
   //top.enable_trace(".");
-  top.enable_trace(getenv("SIMLIB_DUMPDIR"));//to dump the created files in scrap folder so as to not saturate the NFS
+  if(getenv("SIMLIB_DUMPDIR")) {
+    top.enable_trace(getenv("SIMLIB_DUMPDIR"));//to dump the created files in scrap folder so as to not saturate the NFS
+  } else {
+    top.enable_trace(".");
+  }
   top.advance_clock(100000000);
   // Replay last cycles:
   top.load_intermediate_checkpoint(3500000);
