30d25f3f190fa7761c247951bff2159b604c2103
sakshi garg <sgarg3@ucsc.edu>
2020-05-27 01:48:38 -0700

    timestamp not being passed in each change() call now

--- simlib/example/simlib/sample1_stage.hpp
+++ simlib/example/simlib/sample1_stage.hpp
@@ -2,41 +2,42 @@
 #include "vcd_writer.hpp"
 
 struct Sample1_stage {
-  uint64_t hidx; // lgraph hierarchy index
+  uint64_t hidx;  // lgraph hierarchy index
 
-  UInt<1> to2_aValid;
+  UInt<1>  to2_aValid;
   UInt<32> to2_a;
   UInt<32> to2_b;
 
-  UInt<1> to3_cValid;
+  UInt<1>  to3_cValid;
   UInt<32> to3_c;
 
   UInt<32> tmp;
 
 #ifdef SIMLIB_VCD
-  std::string scope_name;
+  std::string     scope_name;
   vcd::VCDWriter* vcd_writer;
   //  vcd::VCDWriter* vcd_writer = vcd::initialize_vcd_writer();
-  vcd::VarPtr vcd_clk = vcd_writer->register_var(scope_name, "clk", vcd::VariableType::wire, 1);
-  vcd::VarPtr vcd_reset = vcd_writer->register_var(scope_name, "reset", vcd::VariableType::wire, 1);
+  vcd::VarPtr vcd_clk        = vcd_writer->register_var(scope_name, "clk", vcd::VariableType::wire, 1);
+  vcd::VarPtr vcd_reset      = vcd_writer->register_var(scope_name, "reset", vcd::VariableType::wire, 1);
   vcd::VarPtr vcd_to1_aValid = vcd_writer->register_var(scope_name, "to1_aValid", vcd::VariableType::wire, 1);
-  vcd::VarPtr vcd_to1_a = vcd_writer->register_var(scope_name, "to1_a[31:0]", vcd::VariableType::wire, 32);
-  vcd::VarPtr vcd_to1_b = vcd_writer->register_var(scope_name, "to1_b[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_to1_a      = vcd_writer->register_var(scope_name, "to1_a[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_to1_b      = vcd_writer->register_var(scope_name, "to1_b[31:0]", vcd::VariableType::wire, 32);
   vcd::VarPtr vcd_to2_aValid = vcd_writer->register_var(scope_name, "to2_aValid", vcd::VariableType::wire, 1);
-  vcd::VarPtr vcd_to2_a = vcd_writer->register_var(scope_name, "to2_a[31:0]", vcd::VariableType::wire, 32);
-  vcd::VarPtr vcd_to2_b = vcd_writer->register_var(scope_name, "to2_b[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_to2_a      = vcd_writer->register_var(scope_name, "to2_a[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_to2_b      = vcd_writer->register_var(scope_name, "to2_b[31:0]", vcd::VariableType::wire, 32);
   vcd::VarPtr vcd_to3_cValid = vcd_writer->register_var(scope_name, "to3_cValid", vcd::VariableType::wire, 1);
-  vcd::VarPtr vcd_to3_c = vcd_writer->register_var(scope_name, "to3_c[31:0]", vcd::VariableType::wire, 32);
-  Sample1_stage(uint64_t _hidx, const std::string &parent_name, vcd::VCDWriter* writer);
+  vcd::VarPtr vcd_to3_c      = vcd_writer->register_var(scope_name, "to3_c[31:0]", vcd::VariableType::wire, 32);
+  Sample1_stage(uint64_t _hidx, const std::string& parent_name, vcd::VCDWriter* writer);
   void vcd_reset_cycle();
-   void vcd_cycle(UInt<32> s3_to1_b, UInt<1> s2_to1_aValid, UInt<32> s2_to1_a);
+  void vcd_posedge();
+  void vcd_negedge();
+  void vcd_comb(UInt<32> s3_to1_b, UInt<1> s2_to1_aValid, UInt<32> s2_to1_a);
 #else
   Sample1_stage(uint64_t _hidx);
   void reset_cycle();
-   void cycle(UInt<32> s3_to1_b, UInt<1> s2_to1_aValid, UInt<32> s2_to1_a);
+  void cycle(UInt<32> s3_to1_b, UInt<1> s2_to1_aValid, UInt<32> s2_to1_a);
 #endif
 #ifdef SIMLIB_TRACE
-  void add_signature(Simlib_signature &sign);
+  void add_signature(Simlib_signature& sign);
 #endif
 };
-
--- simlib/example/simlib/sample_stage.hpp
+++ simlib/example/simlib/sample_stage.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+
 #include "sample1_stage.hpp"
 #include "sample2_stage.hpp"
 #include "sample3_stage.hpp"
@@ -7,29 +8,29 @@
 
 struct Sample_stage {
   uint64_t hidx;
-  #ifdef SIMLIB_VCD
-    std::string scope_name;
-    vcd::VCDWriter* vcd_writer;
-  Sample_stage(uint64_t _hidx,const std::string &parent_name, vcd::VCDWriter* writer);
+#ifdef SIMLIB_VCD
+  std::string     scope_name;
+  vcd::VCDWriter* vcd_writer;
+  Sample_stage(uint64_t _hidx, const std::string& parent_name, vcd::VCDWriter* writer);
   void vcd_reset_cycle();
   void vcd_comb();
   void vcd_posedge();
   void vcd_negedge();
-   // vcd::VCDWriter &vcd_writer = vcd::initialize_vcd_writer();
-    vcd::VarPtr vcd_to2_aValid = vcd_writer->register_var(scope_name,"to2_aValid",vcd::VariableType::wire, 1 );
-    vcd::VarPtr vcd_to2_a = vcd_writer->register_var(scope_name,"to2_a[31:0]",vcd::VariableType::wire, 32 );
-    vcd::VarPtr vcd_to2_b = vcd_writer->register_var(scope_name,"to2_b[31:0]",vcd::VariableType::wire, 32 );
-    vcd::VarPtr vcd_to3_cValid = vcd_writer->register_var(scope_name,"to3_cValid",vcd::VariableType::wire, 1 );
-    vcd::VarPtr vcd_to3_c = vcd_writer->register_var(scope_name,"to3_c[31:0]",vcd::VariableType::wire, 32 );
-    vcd::VarPtr vcd_to3_dValid = vcd_writer->register_var(scope_name,"to3_dValid",vcd::VariableType::wire, 1 );
-    vcd::VarPtr vcd_to3_d = vcd_writer->register_var(scope_name,"to3_d[31:0]",vcd::VariableType::wire, 32 );
-    vcd::VarPtr vcd_to1_aValid = vcd_writer->register_var(scope_name,"to1_aValid",vcd::VariableType::wire, 1 );
-    vcd::VarPtr vcd_to1_a = vcd_writer->register_var(scope_name,"to1_a[31:0]",vcd::VariableType::wire, 32 );
-    vcd::VarPtr vcd_to2_eValid = vcd_writer->register_var(scope_name,"to2_eValid",vcd::VariableType::wire, 1 );
-    vcd::VarPtr vcd_to2_e = vcd_writer->register_var(scope_name,"to2_e[31:0]",vcd::VariableType::wire, 32 );
-    vcd::VarPtr vcd_to1_b = vcd_writer->register_var(scope_name,"to1_b[31:0]",vcd::VariableType::wire, 32 );
-    vcd::VarPtr vcd_clk = vcd_writer->register_var(scope_name,"clk",vcd::VariableType::wire, 1 );
-    vcd::VarPtr vcd_reset = vcd_writer->register_var(scope_name,"reset",vcd::VariableType::wire, 1 );
+  // vcd::VCDWriter &vcd_writer = vcd::initialize_vcd_writer();
+  vcd::VarPtr vcd_to2_aValid = vcd_writer->register_var(scope_name, "to2_aValid", vcd::VariableType::wire, 1);
+  vcd::VarPtr vcd_to2_a      = vcd_writer->register_var(scope_name, "to2_a[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_to2_b      = vcd_writer->register_var(scope_name, "to2_b[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_to3_cValid = vcd_writer->register_var(scope_name, "to3_cValid", vcd::VariableType::wire, 1);
+  vcd::VarPtr vcd_to3_c      = vcd_writer->register_var(scope_name, "to3_c[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_to3_dValid = vcd_writer->register_var(scope_name, "to3_dValid", vcd::VariableType::wire, 1);
+  vcd::VarPtr vcd_to3_d      = vcd_writer->register_var(scope_name, "to3_d[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_to1_aValid = vcd_writer->register_var(scope_name, "to1_aValid", vcd::VariableType::wire, 1);
+  vcd::VarPtr vcd_to1_a      = vcd_writer->register_var(scope_name, "to1_a[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_to2_eValid = vcd_writer->register_var(scope_name, "to2_eValid", vcd::VariableType::wire, 1);
+  vcd::VarPtr vcd_to2_e      = vcd_writer->register_var(scope_name, "to2_e[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_to1_b      = vcd_writer->register_var(scope_name, "to1_b[31:0]", vcd::VariableType::wire, 32);
+  vcd::VarPtr vcd_clk        = vcd_writer->register_var(scope_name, "clk", vcd::VariableType::wire, 1);
+  vcd::VarPtr vcd_reset      = vcd_writer->register_var(scope_name, "reset", vcd::VariableType::wire, 1);
 #else
   Sample_stage(uint64_t _hidx);
   void reset_cycle();
@@ -40,6 +41,6 @@ struct Sample_stage {
   Sample3_stage s3;
 
 #ifdef SIMLIB_TRACE
-  void add_signature(Simlib_signature &sign);
+  void add_signature(Simlib_signature& sign);
 #endif
 };
