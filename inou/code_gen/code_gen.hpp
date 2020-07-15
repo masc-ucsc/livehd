a05fe147d86562abb19490fef93566c6417caf7b
sakshi garg <sgarg3@ucsc.edu>
2020-07-15 11:41:31 -0700

    updated:Code_gen. update:able to process dummy file :inou/code_gen/tests/testing.prp

--- inou/code_gen/code_gen.hpp
+++ inou/code_gen/code_gen.hpp
@@ -7,12 +7,15 @@ protected:
   //Lnast *top;
   std::shared_ptr<Lnast> lnast;
   std::string_view       path;
-  std::string            buffer_to_print;
+  std::string            buffer_to_print = "";
 public:
   Code_gen(std::shared_ptr<Lnast>_lnast, std::string_view _path);
   //virtual void generate() = 0;
   void generate();
   void do_stmts(const mmap_lib::Tree_index& stmt_node_index);
+  void do_assign(const mmap_lib::Tree_index& assign_node_index);
+  bool is_temp_var(std::string_view test_string);//can go to private/protected section!?
+  std::string_view get_node_name(Lnast_node node);//can go to private/protected section!?
   virtual std::string_view stmt_sep() = 0;
 };
 
