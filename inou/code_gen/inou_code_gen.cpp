c744e925cfc0f76279e940a547a5ebc10ab958f9
sakshi garg <sgarg3@ucsc.edu>
2020-07-15 20:56:49 -0700

    as per the directory structure

--- inou/code_gen/inou_code_gen.cpp
+++ inou/code_gen/inou_code_gen.cpp
@@ -1,6 +1,6 @@
 //  This file is distributed under the BSD 3-Clause License. See LICENSE for details.
 
-#include "inou_cgen.hpp"
+#include "inou_code_gen.hpp"
 
 #include <strings.h>
 
@@ -13,33 +13,33 @@
 #include "cfg_lnast.hpp"
 #include "lnast_generic_parser.hpp"
 
-void setup_inou_cgen() { Inou_cgen::setup(); }
+void setup_inou_code_gen() { Inou_code_gen::setup(); }
 
-Inou_cgen::Inou_cgen(const Eprp_var &var) : Pass("inou_cgen", var) { lg = 0; }
+Inou_code_gen::Inou_code_gen(const Eprp_var &var) : Pass("inou_code_gen", var) { lg = 0; }
 
-void Inou_cgen::setup() {
-  Eprp_method m2("inou.cgen.cfg", "parse cfg_test -> build lnast -> generate cfg_text", &Inou_cgen::to_cfg);
+void Inou_code_gen::setup() {
+  Eprp_method m2("inou.cgen.cfg", "parse cfg_test -> build lnast -> generate cfg_text", &Inou_code_gen::to_cfg);
   m2.add_label_optional("odir", "path to put the cfg[s]", ".");
 
   register_inou("cgen", m2);
 
-  Eprp_method m3("inou.cgen.verilog", "parse cfg_test -> build lnast -> generate verilog", &Inou_cgen::to_verilog);
+  Eprp_method m3("inou.cgen.verilog", "parse cfg_test -> build lnast -> generate verilog", &Inou_code_gen::to_verilog);
   m3.add_label_optional("odir", "path to put the verilog[s]", ".");
 
   register_inou("cgen", m3);
 
-  Eprp_method m4("inou.cgen.prp", "parse cfg_test -> build lnast -> generate pyrope", &Inou_cgen::to_prp);
+  Eprp_method m4("inou.cgen.prp", "parse cfg_test -> build lnast -> generate pyrope", &Inou_code_gen::to_prp);
   m4.add_label_optional("odir", "path to put the pyrope[s]", ".");
 
   register_inou("cgen", m4);
 
-  Eprp_method m5("inou.cgen.cpp", "parse cfg_text -> build lnast -> generate cpp", &Inou_cgen::to_cpp);
+  Eprp_method m5("inou.cgen.cpp", "parse cfg_text -> build lnast -> generate cpp", &Inou_code_gen::to_cpp);
   m5.add_label_optional("odir", "path to put the cpp[s}", ".");
 
   register_inou("cgen", m5);
 }
 
-void Inou_cgen::to_xxx(Cgen_type cgen_type, std::shared_ptr<Lnast> lnast) {
+void Inou_code_gen::to_xxx(Cgen_type cgen_type, std::shared_ptr<Lnast> lnast) {
   std::shared_ptr<Code_gen> lnast_to;
 
 /*  if (cgen_type == Cgen_type::Type_verilog) {
@@ -67,42 +67,42 @@ void Inou_cgen::to_xxx(Cgen_type cgen_type, std::shared_ptr<Lnast> lnast) {
   lnast_to->generate();
 }
 
-void Inou_cgen::to_verilog(Eprp_var &var) {
-  Inou_cgen p(var);
+void Inou_code_gen::to_verilog(Eprp_var &var) {
+  Inou_code_gen p(var);
   for (const auto &l : var.lnasts) {
     p.to_xxx(Cgen_type::Type_verilog, l);
   }
 }
 
-void Inou_cgen::to_prp(Eprp_var &var) {
-  Inou_cgen p(var);
+void Inou_code_gen::to_prp(Eprp_var &var) {
+  Inou_code_gen p(var);
   for (const auto &l : var.lnasts) {
     p.to_xxx(Cgen_type::Type_prp, l);
   }
 }
 
-void Inou_cgen::to_cfg(Eprp_var &var) {
-  Inou_cgen p(var);
+void Inou_code_gen::to_cfg(Eprp_var &var) {
+  Inou_code_gen p(var);
   for (const auto &l : var.lnasts) {
     p.to_xxx(Cgen_type::Type_cfg, l);
   }
 }
 
-void Inou_cgen::to_cpp(Eprp_var &var) {
-  Inou_cgen p(var);
+void Inou_code_gen::to_cpp(Eprp_var &var) {
+  Inou_code_gen p(var);
   for (const auto &l : var.lnasts) {
     p.to_xxx(Cgen_type::Type_cpp, l);
   }
 }
 
-void Inou_cgen::Declaration::format_raw(std::ostringstream &w) const {
+void Inou_code_gen::Declaration::format_raw(std::ostringstream &w) const {
   constexpr std::string_view str_type[] = {"local", "input", "output", "sflop", "aflop", "fflop", "latch"};
 
   w << fmt::format("dec {} bits:{} pos:{} sign:{} order:{} type:{}\n", name, bits, pos, is_signed, order,
                    str_type[static_cast<int>(type)]);
 }
 
-void Inou_cgen::iterate_declarations(Node_pin &pin) {
+void Inou_code_gen::iterate_declarations(Node_pin &pin) {
   (void)pin;
   I(false);
   /*
@@ -149,7 +149,7 @@ void Inou_cgen::iterate_declarations(Node_pin &pin) {
    */
 }
 
-void Inou_cgen::setup_declarations() {
+void Inou_code_gen::setup_declarations() {
   declarations.clear();
   declaration_root.clear();
 
@@ -195,7 +195,7 @@ void Inou_cgen::setup_declarations() {
   });
 }
 
-void Inou_cgen::generate_prp(LGraph *g, std::string_view filename) {
+void Inou_code_gen::generate_prp(LGraph *g, std::string_view filename) {
   assert(lg == nullptr);
   lg = g;
 
