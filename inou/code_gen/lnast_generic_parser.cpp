668b9d331a44e69ce858bfeba5113d22b17b7fed
sakshi garg <sgarg3@ucsc.edu>
2020-07-22 14:37:37 -0700

    all mentions/lang specific constructs generalized

--- inou/code_gen/lnast_generic_parser.cpp
+++ inou/code_gen/lnast_generic_parser.cpp
@@ -7,3 +7,30 @@ std::string_view Prp_parser::stmt_sep(){
 std::string_view Cpp_parser::stmt_sep(){
   return stmt_separator;
 }
+std::string_view Ver_parser::stmt_sep(){
+  return stmt_separator;
+}
+
+std::string_view Prp_parser::get_lang_type(){
+  return lang_type;
+}
+std::string_view Cpp_parser::get_lang_type(){
+  return lang_type;
+}
+std::string_view Ver_parser::get_lang_type(){
+  return lang_type;
+}
+
+
+std::string_view Prp_parser::debug_name_lang(Lnast_ntype node_type){
+  return node_type.debug_name_pyrope();
+}
+std::string_view Cpp_parser::debug_name_lang(Lnast_ntype node_type){
+  return node_type.debug_name_cpp();
+}
+std::string_view Ver_parser::debug_name_lang(Lnast_ntype node_type){
+  return node_type.debug_name_verilog();
+}
+
+
+
