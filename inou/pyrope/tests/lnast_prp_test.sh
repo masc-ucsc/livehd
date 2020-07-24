4d8168aa146dba03d292432ec9c5a5d3b9735baf
Sheng Hong Wang <swang203@ucsc.edu>
2020-07-24 16:14:12 -0700

    for merge

--- inou/pyrope/tests/lnast_prp_test.sh
+++ inou/pyrope/tests/lnast_prp_test.sh
@@ -10,7 +10,8 @@ pts='lhs_wire lhs_wire2 scalar_tuple attr_set
      '
 
 #make sure to call Pyrope_compile() in the end of script
-pts_hier='sum funcall'
+pts='test'
+# pts_hier='sum funcall'
 # pts_hier2='sum funcall4'   
 
 
