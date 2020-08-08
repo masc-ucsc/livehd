19ae31214ab3268283ab42785a944ddddd5e776f
sakshi garg <sgarg3@ucsc.edu>
2020-08-08 16:27:01 -0700

    assign statement's keyword in verilog

--- inou/code_gen/code_gen.cpp
+++ inou/code_gen/code_gen.cpp
@@ -156,7 +156,7 @@ void Code_gen::do_assign(const mmap_lib::Tree_index& assign_node_index) {
       absl::StrAppend(&buffer_to_print, indent(), lnast_to->ref_name(ref_map.find(key)->second), " ", lnast_to->debug_name_lang(assign_node_data.type), " ", lnast_to->ref_name(ref), lnast_to->stmt_sep());
     }
   } else {
-    absl::StrAppend(&buffer_to_print, indent(), lnast_to->ref_name(key), " ", lnast_to->debug_name_lang(assign_node_data.type), " ", lnast_to->ref_name(ref), lnast_to->stmt_sep());
+    absl::StrAppend(&buffer_to_print, indent(), lnast_to->assign_node_strt(), lnast_to->ref_name(key), " ", lnast_to->debug_name_lang(assign_node_data.type), " ", lnast_to->ref_name(ref), lnast_to->stmt_sep());
   }
 }
 //-------------------------------------------------------------------------------------
