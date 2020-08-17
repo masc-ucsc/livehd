5d0d220c80582541a763bec0c320d809bee3c0c4
Jose Renau <renau@ucsc.edu>
2020-08-17 12:58:38 -0700

    perf testing

--- scripts/livehd_perf.sh
+++ scripts/livehd_perf.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 
-
-# git clean -fdx 
+git clean -fdx
 bazel build -c opt //...
 
+export NOCHECK=1
 for a in `bazel query "tests(//...)" 2>/dev/null | grep ^\/ | sed -e 's/^\//.\/bazel-bin/g' | sed -e 's/:/\//g'`
 do
-  perf stat -i ${a}.perf_stat ${a}
+  PFILE=$(echo $(echo ${a} | sed -e 's/\//_/g' | sed -e 's/bazel-bin_//g'))
+  perf stat -o ${PFILE}.perf_stat ${a} >${PFILE}.log 2>${PFILE}.err
 done
 
