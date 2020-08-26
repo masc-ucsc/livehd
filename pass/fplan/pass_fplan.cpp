e8446bc05efdaa2cf12a69be716f3f2eb79254f4
Kyle Neil <kyle@johnneil.net>
2020-08-26 14:29:39 -0700

    fixed bug where fplan wouldn't handle lgraphs with multiple instantiations

--- pass/fplan/pass_fplan.cpp
+++ pass/fplan/pass_fplan.cpp
@@ -7,6 +7,8 @@
 #include "lgedgeiter.hpp"
 #include "lgraph.hpp"
 
+#include <set>
+
 void setup_pass_fplan() { Pass_fplan::setup(); }
 
 void Pass_fplan::setup() {
@@ -17,6 +19,9 @@ void Pass_fplan::setup() {
 
 // turn an LGraph into a graph suitable for HiReg.
 void Pass_fplan::make_graph(Eprp_var& var) {
+  // TODO: check to make sure the lgraph(s) are actually valid before traversing them
+  // TODO: after I get this working, harden this code.  Assert stuff.
+
   std::cout << "  creating floorplan graph...";
 
   Hierarchy_tree* root_tree;
@@ -31,145 +36,106 @@ void Pass_fplan::make_graph(Eprp_var& var) {
     }
   }
 
-  // nodes and compact nodes are basically just pointers to the internal lgraph rep.
-  // they can be deleted and re-created at will.
+  root_tree->dump();
 
-  //root_tree->dump();
+  // TODO: optimize this.
 
-  absl::flat_hash_map<Node, vertex_t> nvmap;
-
-  //unsigned long unique_value_counter = 0;
-
-  for (auto hidx : root_tree->depth_preorder()) {
-    auto lg = root_tree->ref_lgraph(hidx);
-    //fmt::print("LG: {}\n", lg->get_name());
-    lg->each_sub_fast([&](Node& n, Lg_type_id id) -> bool {
-      auto new_v = gi.al.insert_vert();
-
-      //LGraph* child_graph = LGraph::open(lg->get_path(), n.get_type_sub());
-
-      //gi.ids[new_v]         = unique_value_counter++;
-      //gi.debug_names[new_v] = n.debug_name();
-      //gi.areas[new_v]       = lg->size();
-      //gi.sets[0].insert(new_v);  // all verts start in set zero, and get dividied up during hierarchy discovery
-
-      // Node constructor needs root lgraph, not parent lgraph
-      auto hn = Node(root_lg, hidx, n.get_compact_class());
-      nvmap.emplace(hn, new_v);
-      fmt::print("Node: {}, (height: {}, pos: {}), nid: {}\n",
-                  n.debug_name(),
-                  int32_t(hidx.level),
-                  int32_t(hidx.pos),
-                  n.get_compact().get_nid()
-                  );
-
-      return true;
-    });
-  }
-/*
+  absl::flat_hash_set<std::tuple<Hierarchy_index, Hierarchy_index, uint32_t>> edges;
+  absl::flat_hash_map<Hierarchy_index, vertex_t> vm;
+  unsigned long unique_vector_id = 0;
 
   for (auto hidx : root_tree->depth_preorder()) {
-    auto lg = root_tree->ref_lgraph(hidx);
-    //fmt::print("2LG: {}\n", lg->get_name());
-    lg->each_sub_fast([&](Node& n, Lg_type_id id) -> bool {
-      //Node hnode(root_lg, hidx, n.get_compact_class());
-      //for(auto e:hnode.out_edges()) {}
-      //for(auto e:hnode.inp_edges()) {}
-      return true;
-    });
-  }
-  */
+    LGraph* lg = root_tree->ref_lgraph(hidx);
 
-  // we need the hidx in order to identify nodes.
-  // we need the nid in order for nv.first.get_node(root_lg) to return an actual node.
+    Node temp(root_lg, hidx, Node::Hardcoded_input_nid);
 
-  // not sure about including the root node right now...
-  // auto root_v = gi.al.insert_vert();
-  // gi.ids[root_v] = unique_value_counter++;
-  // gi.debug_names[root_v] = root_lg->get_self_sub_node().get_name();
-  // gi.areas[root_v] = root_lg->size();
-  // gi.sets[0].insert(root_v);
+    auto new_v = gi.al.insert_vert();
 
-  // nvmap.emplace(Node::Compact(Hierarchy_index(), 1), gi.al.insert_vert());
+    gi.ids[new_v]         = unique_vector_id++;
+    gi.debug_names[new_v] = temp.debug_name();
+    gi.areas[new_v]       = lg->size();
+    gi.sets[0].insert(new_v);
 
-  for (auto nv : nvmap) {
-    for (auto lg_e : nv.first.inp_edges()) {
+    vm.emplace(hidx, new_v);
 
-      vertex_t v1 = gi.al.null_vert();
-      vertex_t v2 = gi.al.null_vert();
-
-      fmt::print("looking for driver hidx: (level: {}, pos: {})\n", int32_t(lg_e.driver.get_hidx().level), int32_t(lg_e.driver.get_hidx().pos));
-      fmt::print("looking for sink hidx: (level: {}, pos: {})\n", int32_t(lg_e.sink.get_hidx().level), int32_t(lg_e.sink.get_hidx().pos));
-
-      if (lg_e.driver.get_hidx() == lg_e.sink.get_hidx()) {
+    for (auto e : temp.inp_edges()) {
+      auto ei = std::tuple(e.driver.get_hidx(), hidx, e.get_bits());
+      if (e.driver.get_hidx() == hidx) {
         continue;
       }
+      if (edges.contains(ei)) {
+        continue;
+      }
+      edges.emplace(ei);
+    }
 
-      for (auto cnv : nvmap) {
-        if (cnv.first.get_hidx() == lg_e.driver.get_hidx()) {
-          v1 = nvmap.at(cnv.first);
-        } else if (cnv.first.get_hidx() == lg_e.sink.get_hidx()) {
-          v2 = nvmap.at(cnv.first);
+    for (auto e : temp.out_edges()) {
+      auto ei = std::tuple(hidx, e.sink.get_hidx(), e.get_bits());
+      if (hidx == e.sink.get_hidx()) {
+        continue;
+      }
+      if (edges.contains(ei)) {
+        continue;
+      }
+      edges.emplace(ei);
+    }
+  }
+
+  for (auto ei : edges) {
+    // all connections have to be symmetrical
+
+    auto [src, dst, weight] = ei;
+
+    auto v1 = vm.at(src);
+    auto v2 = vm.at(dst);
+
+    auto find_edge = [&](vertex_t v_src, vertex_t v_dst) -> edge_t {
+      for (auto e : gi.al.edges()) {
+        if (gi.al.tail(e) == v_src && gi.al.head(e) == v_dst) {
+          return e;
         }
       }
 
-      I(v1 != gi.al.null_vert());
-      I(v2 != gi.al.null_vert());
+      return gi.al.null_edge();
+    };
 
-      auto find_edge = [&](vertex_t v_src, vertex_t v_dst) -> edge_t {
-        for (auto e : gi.al.edges()) {
-          if (gi.al.tail(e) == v_src && gi.al.head(e) == v_dst) {
-            return e;
-          }
-        }
+    auto e_1_2 = find_edge(v1, v2);
+    if (e_1_2 == gi.al.null_edge()) {
+      auto new_e        = gi.al.insert_edge(v1, v2);
+      gi.weights[new_e] = weight;
+    } else {
+      gi.weights[e_1_2] += weight;
+    }
 
-        return gi.al.null_edge();
-      };
-
-      // all connections have to be symmetrical
-      auto e_1_2 = find_edge(v1, v2);
-      if (e_1_2 == gi.al.null_edge()) {
-        auto new_e        = gi.al.insert_edge(v1, v2);
-        gi.weights[new_e] = lg_e.driver.get_bits();
-      } else {
-        gi.weights[e_1_2] += lg_e.driver.get_bits();
-      }
-
-      auto e_2_1 = find_edge(v2, v1);
-      if (e_2_1 == gi.al.null_edge()) {
-        auto new_e        = gi.al.insert_edge(v2, v1);
-        gi.weights[new_e] = lg_e.driver.get_bits();
-      } else {
-        gi.weights[e_2_1] += lg_e.driver.get_bits();
-      }
+    auto e_2_1 = find_edge(v2, v1);
+    if (e_2_1 == gi.al.null_edge()) {
+      auto new_e        = gi.al.insert_edge(v2, v1);
+      gi.weights[new_e] = weight;
+    } else {
+      gi.weights[e_2_1] += weight;
     }
   }
 
   using namespace graph::attributes;
-  std::cout << gi.al.dot_format("weight"_of_edge = gi.weights, "name"_of_vert = gi.debug_names, "area"_of_vert = gi.areas) << std::endl;
+  std::cout << gi.al.dot_format("weight"_of_edge = gi.weights, "name"_of_vert = gi.debug_names, "area"_of_vert = gi.areas, "id"_of_vert = gi.ids) << std::endl;
 
-/*
-
-
-
-// if the graph is not fully connected, ker-lin fails to work.
-for (const auto& v : gi.al.verts()) {
-  for (const auto& other_v : gi.al.verts()) {
-    bool found = false;
-    for (const auto& e : gi.al.out_edges(v)) {
-      if (gi.al.head(e) == other_v && gi.al.tail(e) == v) {
-        found = true;
-        break;
+  // if the graph is not fully connected, ker-lin fails to work.
+  for (const auto& v : gi.al.verts()) {
+    for (const auto& other_v : gi.al.verts()) {
+      bool found = false;
+      for (const auto& e : gi.al.out_edges(v)) {
+        if (gi.al.head(e) == other_v && gi.al.tail(e) == v) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        auto temp_e        = gi.al.insert_edge(v, other_v);
+        gi.weights[temp_e] = 0;
       }
     }
-    if (!found) {
-      auto temp_e        = gi.al.insert_edge(v, other_v);
-      gi.weights[temp_e] = 0;
-    }
   }
-}
-std::cout << "done." << std::endl;
-*/
+  std::cout << "done." << std::endl;
 }
 
 void Pass_fplan::pass(Eprp_var& var) {
@@ -178,9 +144,9 @@ void Pass_fplan::pass(Eprp_var& var) {
 
   p.make_graph(var);
 
-  //Hier_tree h(std::move(p.gi), 1);
-  //h.collapse(0.0);
-  //h.discover_regularity();
+  Hier_tree h(std::move(p.gi), 1);
+  h.collapse(0.0);
+  h.discover_regularity();
 
   // 3. <finish HiReg>
   // 4. write code to use the existing hierarchy instead of throwing it away...?
