206cec66d03f81a78d1e4db7f3774512781aadf0
Kyle Neil <kyle@johnneil.net>
2020-09-02 18:11:57 -0700

    added a new pass to output the hier I generated from lgraph

--- pass/fplan/pass_fplan.hpp
+++ pass/fplan/pass_fplan.hpp
@@ -3,11 +3,8 @@
 #include "graph_info.hpp"
 #include "pass.hpp"
 
-// not sure if we're an inou or a pass.  Assuming a pass for now.
-
 class Pass_fplan : public Pass {
 public:
-  // creates the parser and registers it with LGraph...?
   Pass_fplan(const Eprp_var &var) : Pass("pass.fplan", var), gi() {}
 
   static void setup();
@@ -15,8 +12,21 @@ public:
   static void pass(Eprp_var &v);
 
 private:
-  // TODO: if a Graph_info class is used here, things break beacause of "i_resolve_header" not being included last.
+  friend class Pass_fplan_dump;
+
   void make_graph(Eprp_var &var);
 
   Graph_info gi;
+};
+
+class Pass_fplan_dump : public Pass {
+public:
+  Pass_fplan_dump(const Eprp_var &var) : Pass("pass.fplan", var) {}
+
+  static void setup();
+
+  static void pass(Eprp_var &v);
+
+private:
+  
 };
