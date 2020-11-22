fdc15e62944d78a15210bf6d7754cdbff1cf1de2
Sheng Hong Wang <swang203@ucsc.edu>
2020-11-22 11:10:08 -0800

    more thinking on max/min, dpin_bits, lgraph signedness representation, and Tposs bits

--- inou/pyrope/tests/compiler/verilog_gld/firrtl_tail3.gld.v
+++ inou/pyrope/tests/compiler/verilog_gld/firrtl_tail3.gld.v
@@ -1,23 +1,22 @@
 module firrtl_tail3 (
-  input         clock,
+  input        clock,
   input  [3:0] inp,
   output [3:0] out
 );
 
-reg  [4:0] x_next;
-wire [5:0] minus = $signed(x) - $signed(2'b01);
+reg  [3:0] x_next;
 always @ (*) begin
   if (x == 0)
-    x_next = inp;
+    x_next = {inp};
   else
-    x_next = minus[4:0];
+    x_next = $signed(x) - $signed(2'b01);
 end
 
-reg [4:0] x;
+reg [3:0] x;
 always @ (posedge clock) begin
   x <= x_next;
 end
 
-assign out = x[3:0];
+assign out = x;
 
 endmodule
