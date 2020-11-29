b00534e2f64647ddef200070e081bc50b72dfab5
Jose Renau <renau@ucsc.edu>
2020-11-29 12:10:48 -0800

    pass sample fix

--- pass/sample/pass_sample.cpp
+++ pass/sample/pass_sample.cpp
@@ -190,7 +190,7 @@ void Pass_sample::create_sample_graph(LGraph *g) {
   shr_out_drv.set_name("shr_out");
 
   auto s_const = lg->create_node_const(Lconst(2));
-  I(s_const.get_driver_pin().get_bits() == 2);  // Automatically set bits for const
+  I(s_const.get_driver_pin().get_bits() == 3);  // Automatically set bits for const
 
   auto a_sink = shr_node.setup_sink_pin("a");
   auto b_sink = shr_node.setup_sink_pin("b");
