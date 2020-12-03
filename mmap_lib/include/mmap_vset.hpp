4977251d97c829f70b95f343ed3cb0895739603d
Sloan Liu <sloanyianliu@gmail.com>
2020-12-02 16:09:46 -0800

    Solved the find and bit traverse problems

--- mmap_lib/include/mmap_vset.hpp
+++ mmap_lib/include/mmap_vset.hpp
@@ -16,7 +16,7 @@ namespace mmap_lib {
 		public:
 			using VisitorSet = typename mmap_lib::map<Key, T>;
 			VisitorSet visitor_set;
-
+			T max_inserted = 0;
 			using iterator = typename VisitorSet::iterator;
 			using const_iterator = typename VisitorSet::const_iterator;
 
@@ -58,23 +58,25 @@ namespace mmap_lib {
 			 * Why the need for a template here?
 			 * Need to access single bits of the bitmap outside of the functions
 			 */
-			template <typename T_ = T, typename = std::enable_if_t<!is_array_serializable<T_>::value>>
-			[[nodiscard]] const T & bucket_get(const Key &key) const { return visitor_set.get(key); }
+
+		  template<typename T_ = T, typename = std::enable_if_t<!is_array_serializable<T_>::value>>
+  		[[nodiscard]] const T &        bucket_get_val(const Key &key) const { return visitor_set.get(key); }
+      
+			template<typename T_ = T, typename = std::enable_if_t<is_array_serializable<T_>::value>>
+      [[nodiscard]] T                bucket_get_val(const Key &key) const { return visitor_set.get(key); }
+ 			
+			template<typename T_ = T, typename = std::enable_if_t<!is_array_serializable<T_>::value>>
+  		[[nodiscard]] const T &        bucket_get_val(const iterator &it) const { return visitor_set.get(it); }
+      
+			template<typename T_ = T, typename = std::enable_if_t<!is_array_serializable<T_>::value>>
+      [[nodiscard]] const T &        bucket_get_val(const const_iterator &it) const { return visitor_set.get(it); }
+
+      template<typename T_ = T, typename = std::enable_if_t<is_array_serializable<T_>::value>>
+      [[nodiscard]] T                bucket_get_val(const iterator &it) const { return visitor_set.get(it); }
+      
+			template<typename T_ = T, typename = std::enable_if_t<is_array_serializable<T_>::value>>
+      [[nodiscard]] T                bucket_get_val(const const_iterator &it) const { return visitor_set.get(it); }
 			
-			template <typename T_ = T, typename = std::enable_if_t<is_array_serializable<T_>::value>>
-			[[nodiscard]] T & bucket_get(const Key &key) const { return visitor_set.get(key); }
-			
-			template <typename T_ = T, typename = std::enable_if_t<!is_array_serializable<T_>::value>>
-			[[nodiscard]] const T & bucket_get(const iterator &it) const { return visitor_set.get(it); }
-			
-			template <typename T_ = T, typename = std::enable_if_t<is_array_serializable<T_>::value>>
-			[[nodiscard]] T & bucket_get(const iterator &it) const { return visitor_set.get(it); }
-			
-			template <typename T_ = T, typename = std::enable_if_t<!is_array_serializable<T_>::value>>
-			[[nodiscard]] T bucket_get(const iterator &it) const { return visitor_set.get(it); }
-			
-			template <typename T_ = T, typename = std::enable_if_t<is_array_serializable<T_>::value>>
-			[[nodiscard]] T bucket_get(const const_iterator &it) const { return visitor_set.get(it); }
 		
 			[[nodiscard]] iterator bucket_find(const Key &key) { return visitor_set.find(key); }
 			[[nodiscard]] const_iterator bucket_find(const Key &key) const { return visitor_set.find(key); }
@@ -132,21 +134,25 @@ namespace mmap_lib {
 			 * All the erase() functions are void too
 			 * The funcs intake a number you wish to remove from the set
 			 */
-			[[nodiscard]] void insert(T &&ele) { 
+			[[nodiscard]] void insert(T &&ele) {
+				if (ele > max_inserted) { max_inserted = ele; }
+				
 				//find correct index the pos is at
 				const auto p = ele / (sizeof(T)*8); // p will be the key that points to the correct bitmap
 				const auto i = ele % (sizeof(T)*8); // i will be the bit we are interested in in the bitmap
-				auto hold = 0; // will hold the bitmap at index p if there is one
+				T hold = 0; // will hold the bitmap at index p if there is one
 				if (visitor_set.has((Key)p)) { hold = visitor_set.get((Key)p); }//is there a bitmap at key p
 				hold = hold | (1 << i); //modify the bit at pos
 				visitor_set.set((Key)p, (T)hold); //put it back in the bitmap
 			}
 
 			[[nodiscard]] void insert(const T &&ele) {
+				if (ele > max_inserted) { max_inserted = ele; }
+				
 				//find correct index the pos is at
 				const auto p = ele / (sizeof(T)*8); // p will be the key for the bitmap
 				const auto i = ele % (sizeof(T)*8); // i will be the bit we are interested in
-				auto hold = 0; // will hold the bitmap at index p if there is one
+				T hold = 0; // will hold the bitmap at index p if there is one
 				if (visitor_set.has((Key)p)) { hold = visitor_set.get((Key)p); }//is there a bitmap at key p
 				hold = hold | (1 << i); //modify the bit at pos
 				visitor_set.set((Key)p, (T)hold); //put it back in the bitmap
@@ -154,24 +160,118 @@ namespace mmap_lib {
 
 			[[nodiscard]] void erase(T &&ele) { 
 				//find correct index the pos is at
-				const auto p = ele / (sizeof(T)*8); // p will be the key that points to the correct bitmap
-				const auto i = ele % (sizeof(T)*8); // i will be the bit we are interested in in the bitmap
-				auto hold = 0; // will hold the bitmap at index p if there is one
-				if (visitor_set.has((Key)p)) { hold = visitor_set.get((Key)p); }
-				hold = hold & ~(1 << i); //modify the bit at pos
-				visitor_set.set((Key)p, (T)hold); //put it back in the bitmap
-			}
+				Key p = ele / (sizeof(T)*8); // p will be the key that points to the correct bitmap
+				uint8_t i = ele % (sizeof(T)*8); // i will be the bit we are interested in in the bitmap
+				T hold = 0; // will hold the bitmap at index p if there is one
 
-			[[nodiscard]] void erase(const T &&ele) {
+				if (visitor_set.has((Key)p)) { 
+					hold = visitor_set.get((Key)p);
+					hold = hold & ~(1 << i); //modify the bit at i
+					visitor_set.set((Key)p, (T)hold); //put it back in the bitmap
+					p--;
+				} 
+
+				// 1) detect if max needs to be updated
+				if (ele == max_inserted) {
+					std::cout << "Time to update max_inserted, ele = " << ele << "\n";
+					std::cout << "Initial hold = " << hold << "\n";
+
+					--max_inserted;
+					T mask = 1 << (i-1); // used to detect if bit is 1 or 0
+					std::cout << "Initial mask = " << mask << "\n";
+						
+					// mask will do this:
+					// 100000000...
+					// 010000000...
+					// 001000000...
+					// 000100000...
+					// 000010000...
+					// to check for bits starting from MSB
+
+					std::cout << "Loop Start\n\n";
+					for (; mask >= 0; mask = mask / 2) { 		
+						std::cout << "mask is: " << mask << "\n";
+						if (mask == 0) { // hit the end of the bitmap
+							if (p == 0) { // if this is the last index of map, no more elements left
+								max_inserted = 0; // max insert is 0
+								std::cout << "reset?\n";
+								break; // ready to remove 
+							} else {
+								std::cout << "exit1\n";
+							}
+							hold = visitor_set.get((Key)(p--)); // get next bitmap
+							std::cout << "reload hold: " << hold << "\n";
+							mask = 1 << ((sizeof(T)*8)-1); // reset mask
+							std::cout << "reload mask: " << mask << "\n";
+							max_inserted = max_inserted - 1;
+						}
+						if (hold & mask) { // if number detected by mask is there	
+							std::cout << "exit here?\n";
+							break; // ready to remove
+						} else { // if mask number is not there, add to decrement
+							max_inserted = max_inserted - 1;
+						}
+					}
+				}
+			}
+			
+			[[nodiscard]] void erase(const T &&ele) { 
 				//find correct index the pos is at
-				const auto p = ele / (sizeof(T)*8); // p will be the key for the bitmap
-				const auto i = ele % (sizeof(T)*8); // i will be the bit we are interested in
-				auto hold = 0; // will hold the bitmap at index p if there is one
-				if (visitor_set.has((Key)p)) { hold = visitor_set.get((Key)p); }
-				hold = hold & ~(1 << i); //modify the bit at pos
-				visitor_set.set((Key)p, (T)hold); //put it back in the bitmap
-			}
+				Key p = ele / (sizeof(T)*8); // p will be the key that points to the correct bitmap
+				uint8_t i = ele % (sizeof(T)*8); // i will be the bit we are interested in in the bitmap
+				T hold = 0; // will hold the bitmap at index p if there is one
 
+				if (visitor_set.has((Key)p)) { 
+					hold = visitor_set.get((Key)p);
+					hold = hold & ~(1 << i); //modify the bit at i
+					visitor_set.set((Key)p, (T)hold); //put it back in the bitmap
+					p--;
+				} 
+
+				// 1) detect if max needs to be updated
+				if (ele == max_inserted) {
+					std::cout << "Time to update max_inserted, ele = " << ele << "\n";
+					std::cout << "Initial hold = " << hold << "\n";
+
+					--max_inserted;
+					T mask = 1 << (i-1); // used to detect if bit is 1 or 0
+					std::cout << "Initial mask = " << mask << "\n";
+						
+					// mask will do this:
+					// 100000000...
+					// 010000000...
+					// 001000000...
+					// 000100000...
+					// 000010000...
+					// to check for bits starting from MSB
+
+					std::cout << "Loop Start\n\n";
+					for (; mask >= 0; mask = mask / 2) { 		
+						std::cout << "mask is: " << mask << "\n";
+						if (mask == 0) { // hit the end of the bitmap
+							if (p == 0) { // if this is the last index of map, no more elements left
+								max_inserted = 0; // max insert is 0
+								std::cout << "reset?\n";
+								break; // ready to remove 
+							} else {
+								std::cout << "exit1\n";
+							}
+							hold = visitor_set.get((Key)(p--)); // get next bitmap
+							std::cout << "reload hold: " << hold << "\n";
+							mask = 1 << ((sizeof(T)*8)-1); // reset mask
+							std::cout << "reload mask: " << mask << "\n";
+							max_inserted = max_inserted - 1;
+						}
+						if (hold & mask) { // if number detected by mask is there	
+							std::cout << "exit here?\n";
+							break; // ready to remove
+						} else { // if mask number is not there, add to decrement
+							//++dec;
+							max_inserted = max_inserted - 1;
+						}
+					}
+				}
+			}
 
 			/* Can add functions here to get a single bit of the bitmap
 			 * size_t find(const T &&ele) const {
@@ -185,7 +285,6 @@ namespace mmap_lib {
 				// finding the correct bitmap to get
 				const auto p = ele / (sizeof(T)*8);
 				const auto i = ele % (sizeof(T)*8);
-				auto hold = 0;
 				
 				// if this map is there and the bit we want is set high, return true
 				if (visitor_set.has((Key)p) && (visitor_set.get((Key)p) >> i & 1)) {
@@ -198,7 +297,6 @@ namespace mmap_lib {
 			[[nodiscard]] bool find(const T &&ele) {
 				const auto p = ele / (sizeof(T)*8);
 				const auto i = ele % (sizeof(T)*8);
-				auto hold = 0;
 
 				if (visitor_set.has((Key)p) && (visitor_set.get((Key)p) >> i & 1)) {
 					return true;
@@ -218,19 +316,17 @@ namespace mmap_lib {
 				return false;
 			}
 			
-			[[nodiscard]] bool is_end(T &&ele) { 
-				auto ex = visitor_set.capacity(); 
-				ex = (ex * sizeof(T)) - 1;
-				if (ele == ex) { return true; }
+			[[nodiscard]] bool is_end(T &&ele) {
+				if (ele == (T)max_inserted) { return true; }
 				return false;
 			}
 			
 			[[nodiscard]] bool is_end(const T &&ele) { 
-				auto ex = visitor_set.capacity(); 
-				ex = (ex * sizeof(T)) - 1;
-				if (ele == ex) { return true; }
+				if (ele == (T)max_inserted) { return true; }
 				return false;
 			}
+
+			[[nodiscard]] uint64_t get_max() { return max_inserted; }
 	};
 
 
