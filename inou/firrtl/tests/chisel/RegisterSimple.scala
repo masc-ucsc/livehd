cf8f92e440808d5cf6d01ce39e7ab2cb4312bace
Sheng Hong Wang <swang203@ucsc.edu>
2020-12-04 12:33:08 -0800

    update firrtl doc and create a temp hardcoded script for protobuf/firrtl generation

--- inou/firrtl/tests/chisel/RegisterSimple.scala
+++ inou/firrtl/tests/chisel/RegisterSimple.scala
@@ -4,11 +4,6 @@ package pb_tests
 
 import chisel3._
 
-/**
-  * Compute GCD using subtraction method.
-  * Subtracts the smaller from the larger until register y is zero.
-  * value in register x is then the GCD
-  */
 class RegisterSimple extends Module {
   val io = IO(new Bundle {
     val inVal         = Input(UInt(16.W))
