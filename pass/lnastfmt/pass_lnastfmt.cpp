f092d7f44a69b8c02f7986e20ef25be8cc50d170
sakshi garg <sgarg3@ucsc.edu>
2020-12-06 00:31:11 -0800

    pass_lnastfmt now supporting multi-level lnasts like if.prp. Todo: adding correct var.replace support

--- pass/lnastfmt/pass_lnastfmt.cpp
+++ pass/lnastfmt/pass_lnastfmt.cpp
@@ -42,18 +42,21 @@ void Pass_lnastfmt::parse_ln(std::shared_ptr<Lnast> ln, Eprp_var& var, std::stri
   const auto& stmt_index_fmt = lnastfmted->add_child(lnastfmted->get_root(), Lnast_node(ln->get_type(stmt_index), ln->get_token(stmt_index), ln->get_subs(stmt_index))); //stmt node of lnfmted (copied from ln)
 
   auto curr_index = ln->get_child(stmt_index);//1st child of ln after stmt
+
   while(curr_index!= ln->invalid_index()) {
     //iterate through all children of stmt of ln
-
+    bool curr_incremented = false;
     //check if curr_index's child is leaf child?
     //const auto& leaf_child_check = ln->get_child(curr_index);
     bool all_are_leaves = true;
     for (const mmap_lib::Tree_index& it : ln->children(curr_index)) {
+      //fmt::print("PARSING TO CHECK LEAVES:   {}:{}\n",ln->get_name(it), it.level );
       if (!(ln->is_leaf(it))) {
         all_are_leaves = false;
       }
     }
-    if(all_are_leaves) { //ln->is_leaf(leaf_child_check)) {
+    //fmt::print("?????all children leaves????: {}\n", all_are_leaves);
+    if(all_are_leaves) { //ln->is_leaf(leaf_child_check)) 
       if(ln->get_type(curr_index).is_assign()) {
 
         //if assign was curr_index; process assign subtree:
@@ -66,15 +69,16 @@ void Pass_lnastfmt::parse_ln(std::shared_ptr<Lnast> ln, Eprp_var& var, std::stri
         bool in_map = false;
         if(it != ref_hash_map.end()) { in_map = true;}
         //fmt::print("in_map:{}\n", in_map);
-        if(in_map && !(is_temp_var(it->second)) && !(is_temp_var(it->first))) {//sec child is ssa; do not attach this node!
+        if(in_map && !(is_temp_var(it->second)) && !(is_temp_var(it->first)) && (is_ssa(it->second) || is_ssa(it->first))) {//sec child is ssa; do not attach this node!
           //fmt::print("****not adding {} and {}\n", ln->get_name(frst_child_indx), ln->get_name(sec_child_indx));
           curr_index = ln->get_sibling_next(curr_index);
+          curr_incremented = true;
         }
       } 
       //if curr_index node was not ssa-assign
       //traverse and keep adding to lnfmted
       //if value from map used? replcae it and move on.
-      if(curr_index!= ln->invalid_index()) {
+      if(curr_index!= ln->invalid_index() && !curr_incremented) {
         auto curr_index_fmt = lnastfmted->add_child(stmt_index_fmt, Lnast_node(ln->get_type(curr_index), ln->get_token(curr_index), ln->get_subs(curr_index)));
 
         for (const mmap_lib::Tree_index& it : ln->children(curr_index)) {
@@ -82,28 +86,67 @@ void Pass_lnastfmt::parse_ln(std::shared_ptr<Lnast> ln, Eprp_var& var, std::stri
           auto is = ref_hash_map.find(ln->get_name(it));
           if(is != ref_hash_map.end() && is_ssa(ln->get_name(it))) {
             //auto frst_fmt = 
-            lnastfmted->add_child(curr_index_fmt, Lnast_node::create_ref(is->second));
+            lnastfmted->add_child(curr_index_fmt, Lnast_node::create_ref(lnastfmted->add_string(is->second)));
           } else {
             lnastfmted->add_child(curr_index_fmt, Lnast_node(ln->get_type(it), ln->get_token(it), ln->get_subs(it)));
           }
         }
 
       }
-    } else {
-      for (const mmap_lib::Tree_index& it : ln->depth_preorder(ln->get_root())) {
+    } else {//suppose it is "if" subtree
+
+      auto curr_index_fmt = lnastfmted->add_child(stmt_index_fmt,  Lnast_node(ln->get_type(curr_index), ln->get_token(curr_index), ln->get_subs(curr_index)));
+      auto curr_lev = curr_index.level;
+      auto curr_pos = curr_index.pos;
+      //fmt::print("curr_lev and pos: {}, {}\n", curr_lev, curr_pos);
+      for (const mmap_lib::Tree_index& it : ln->depth_preorder(curr_index)) {
+
+        auto new_lev = it.level;
+        auto new_pos = it.pos;
+        auto is = ref_hash_map.find(ln->get_name(it));
+
+        if(new_lev == curr_lev+1) {
+          curr_lev = new_lev; curr_pos = new_pos;
+          if(is != ref_hash_map.end() && is_ssa(ln->get_name(it))) {
+            curr_index_fmt = lnastfmted->add_child(curr_index_fmt, Lnast_node::create_ref(lnastfmted->add_string(is->second)));
+          } else {
+            curr_index_fmt = lnastfmted->add_child(curr_index_fmt, Lnast_node(ln->get_type(it), ln->get_token(it), ln->get_subs(it)));
+          }
+        } else if (new_lev==curr_lev && new_pos > curr_pos) {
+          if(is != ref_hash_map.end() && is_ssa(ln->get_name(it))) {
+            curr_index_fmt = lnastfmted->append_sibling(curr_index_fmt, Lnast_node::create_ref(lnastfmted->add_string(is->second)));
+          } else {
+            curr_index_fmt = lnastfmted->append_sibling(curr_index_fmt,Lnast_node(ln->get_type(it), ln->get_token(it), ln->get_subs(it)));
+          }
+          curr_lev = new_lev; curr_pos = new_pos;
+        } else if (new_lev+1 == curr_lev) {
+          if(is != ref_hash_map.end() && is_ssa(ln->get_name(it))) { 
+            curr_index_fmt = lnastfmted->append_sibling(lnastfmted->get_parent(curr_index_fmt), Lnast_node::create_ref(lnastfmted->add_string(is->second)));
+          } else {
+            curr_index_fmt = lnastfmted->append_sibling(lnastfmted->get_parent(curr_index_fmt), Lnast_node(ln->get_type(it), ln->get_token(it), ln->get_subs(it)));
+          }
+          curr_lev = new_lev; curr_pos = new_pos;
+        } else if (new_lev+2 == curr_lev) {
+          if(is != ref_hash_map.end() && is_ssa(ln->get_name(it))) {
+            curr_index_fmt = lnastfmted->append_sibling(lnastfmted->get_parent(lnastfmted->get_parent(curr_index_fmt)), Lnast_node::create_ref(lnastfmted->add_string(is->second)));
+          } else {
+            curr_index_fmt = lnastfmted->append_sibling(lnastfmted->get_parent(lnastfmted->get_parent(curr_index_fmt)), Lnast_node(ln->get_type(it), ln->get_token(it), ln->get_subs(it)));
+          }
+          curr_lev = new_lev; curr_pos = new_pos;
+        }
 
       }
     }
-    if(curr_index!= ln->invalid_index()) {
+    if(curr_index!= ln->invalid_index() && !curr_incremented) {
       curr_index = ln->get_sibling_next(curr_index);
     }
   } 
 
-  //fmt::print("****lnast fmted:  \n");
-  //lnastfmted->dump();
-  //fmt::print("\n:lnast fmted****\n");
-  var.replace(ln, std::move(lnastfmted));//just replace the pointer
-  //var.add(std::move(lnastfmted));
+  fmt::print("****lnast fmted:  \n");
+  lnastfmted->dump();
+  fmt::print("\n:lnast fmted****\n");
+  //var.replace(ln, std::move(lnastfmted));//just replace the pointer
+  var.add(std::move(lnastfmted));
 }
 
 void Pass_lnastfmt::observe_lnast(Lnast* ln) {
