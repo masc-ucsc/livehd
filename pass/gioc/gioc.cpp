e0eca97f5ca75203630d8d75502c4761b9a44e00
Sheng Hong Wang <swang203@ucsc.edu>
2020-12-10 09:38:22 -0800

    debug

--- pass/gioc/gioc.cpp
+++ pass/gioc/gioc.cpp
@@ -19,8 +19,13 @@ Gioc::Gioc(std::string_view _path) : path(_path){}
 void Gioc::do_trans(LGraph *lg) {
   for (auto node : lg->fast()) {
     if (node.get_type_op() == Ntype_op::Sub) {
-      if (node.get_name().substr(0, 5) == "__fir")
+      
+      /* if (node.get_type_sub_node().get_name().substr(0, 5) == "__fir") { */ //this also doesn't work
+      if (node.has_name() && node.get_name().substr(0, 5) == "__fir") {
+        fmt::print("subgraph node name:{}\n", node.get_name());
+        fmt::print("hit!\n");
         continue; 
+      }
       
       auto *library = Graph_library::instance(path);
       auto subg_paras = split_name(node.get_name(), ":");
