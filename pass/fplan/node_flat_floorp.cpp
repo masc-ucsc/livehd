ec77a39b3a52a76a7b50d018a370078aac5a3492
Jose Renau <renau@ucsc.edu>
2021-02-03 15:40:36 -0800

    fix compile issue at node_flat_floorp

--- pass/fplan/node_flat_floorp.cpp
+++ pass/fplan/node_flat_floorp.cpp
@@ -9,7 +9,7 @@ Node_flat_floorp::Node_flat_floorp(Node_tree&& nt_arg) : Lhd_floorplanner(std::m
 void Node_flat_floorp::load() {
   layouts[nt.get_root()] = new geogLayout();
 
-  Ntype_area na(nt.get_root_lg()->get_path());
+  Ntype_area narea(nt.get_root_lg()->get_path());
 
   for (auto n : nt.get_root_lg()->fast(true)) {
     Ntype_op op = n.get_type_op();
@@ -26,14 +26,14 @@ void Node_flat_floorp::load() {
         fmt::print("adding component {} to root\n", name);
       }
 
-      if (!(na.has_dim(op))) {
+      if (!(narea.has_dim(op))) {
         std::string errstr = "node type ";
         errstr.append(Ntype::get_name(op));
         errstr.append(" has no area information!");
         throw std::runtime_error(errstr);
       }
 
-      auto d = na.get_dim(op);
+      auto d = narea.get_dim(op);
 
       layouts[nt.get_root()]->addComponentCluster(op, 1, d.area, d.max_aspect, d.min_aspect, randomHint(1));
     }
