0c1da1882c360157cc303753c154e18a689591bc
olyad mulisa kebede <okebede@mada0.soe.ucsc.edu>
2021-02-23 14:21:09 -0800

    added test for stringview with size < 14

--- mmap_lib/tests/bench_pstr_use.cpp
+++ mmap_lib/tests/bench_pstr_use.cpp
@@ -62,6 +62,54 @@ void mmap_pstr(std::string_view name) {
   test5.print_e();
   std::cout << "Expecting nothing:  ";
   test5.print_key_val_str();  
+
+  std::cout << "================================ " << std::endl;
+  std::cout << "Constructor 3 (string_view) Tests: " << std::endl;
+  std::cout << "================================ " << std::endl;
+  std::string_view test_6("hello");
+  mmap_lib::str test6(test_6);
+  std::cout << "Expecting \"hell\":  ";
+  test6.print_PoS(); 
+  std::cout << "Expecting \"o\":  ";
+  test6.print_e();
+  std::cout << "Expecting nothing:  ";
+  test6.print_key_val_str();
+
+  std::string_view test_7("cat");
+  mmap_lib::str test7(test_7);
+  std::cout << "Expecting \"cat\":  ";
+  test7.print_PoS(); 
+  std::cout << "Expecting nothing:  ";
+  test7.print_e();
+  std::cout << "Expecting nothing:  ";
+  test7.print_key_val_str();
+
+  std::string_view test_8("abcd");
+  mmap_lib::str test8(test_8);
+  std::cout << "Expecting \"abcd\":  ";
+  test8.print_PoS(); 
+  std::cout << "Expecting nothing ";
+  test8.print_e();
+  std::cout << "Expecting nothing:  ";
+  test8.print_key_val_str();
+
+  std::string_view test_9("feedback");
+  mmap_lib::str test9(test_9);
+  std::cout << "Expecting \"feed\":  ";
+  test9.print_PoS(); 
+  std::cout << "Expecting \"back\":  ";
+  test9.print_e();
+  std::cout << "Expecting nothing:  ";
+  test9.print_key_val_str();
+
+  std::string_view test_10("neutralizatio");
+  mmap_lib::str test10(test_10);
+  std::cout << "Expecting \"neut\":  ";
+  test10.print_PoS(); 
+  std::cout << "Expecting \"ralizatio\":  ";
+  test10.print_e();
+  std::cout << "Expecting nothing:  ";
+  test10.print_key_val_str();
 }
 
 
