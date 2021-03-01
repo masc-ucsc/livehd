b1db9884eee9de44b5c090a955b9c667be85b6a1
Sheng Hong Wang <swang203@ucsc.edu>
2021-03-01 12:49:37 -0800

    fix q pin connection for tuple-reg

--- pass/cprop/cprop.cpp
+++ pass/cprop/cprop.cpp
@@ -605,98 +605,65 @@ std::tuple<std::string_view, std::string> Cprop::get_tuple_name_key(Node &node) 
 }
 
 
-bool Cprop::reg_q_pin_access_preparation2(Node &parent_node, Node_pin &ori_tg_dpin) {
-  (void) ori_tg_dpin;
-  fmt::print("DEBUG2: parent_node:{}\n", parent_node.debug_name());
-  I(parent_node.get_type_op() == Ntype_op::TupGet);
 
-  auto ptup_it = node2tuple.find(parent_node.get_compact());
-  ptup_it->second->dump();
-  auto raw_key_name = ptup_it->second->get_name();
-  
-  std::string raw_key_name2;   
-  auto pos = raw_key_name.find_last_of(":");
-  if (pos != std::string::npos) {
-    raw_key_name2 = raw_key_name.substr(pos+1);
-  } else {
-    raw_key_name2 = raw_key_name;
+void Cprop::split_hier_name(std::string_view full_name, std::vector<std::string_view> &subnames) {
+  auto start = 0u;
+  auto end   = full_name.find('.');
+  while (end != std::string_view::npos) {
+    std::string_view token = full_name.substr(start, end - start);
+    subnames.emplace_back(token);
+    start = end + 1;  //
+    end   = full_name.find('.', start);
   }
-  /* fmt::print("DEBUG2-0: raw_key_name:{}, clear_key_name:{}\n", raw_key_name, clear_key_name); */
-  
-  /* auto pos = */ 
-  /* if (po) */
-
-
-  /* std::string hier_reg_name; */
-
-  /* while (true) { */
-  /*   if (hier_reg_name.empty()) { */
-  /*     hier_reg_name = */ 
-  /*   } else { */
-      
-  /*   } */
-  /* } */
-
-  return true;
+  std::string_view token = full_name.substr(start, end - start);
+  subnames.emplace_back(token);
 }
 
-bool Cprop::reg_q_pin_access_preparation(Node &tgq_parent_node, Node_pin &ori_tg_dpin) {
 
-  auto cur_node = tgq_parent_node;
+
+bool Cprop::reg_q_pin_access_preparation(Node &tgq_parent_node, Node_pin &ori_tgq_dpin) {
+  I(tgq_parent_node.get_type_op() == Ntype_op::TupGet);
+  auto ptup_it = node2tuple.find(tgq_parent_node.get_compact());
+  auto raw_hier_name = ptup_it->second->get_name();
+  std::vector<std::string_view> subnames;
+  split_hier_name(raw_hier_name, subnames);
+
   std::string hier_reg_name;
-  while (true) {
-    auto [tup_name, key_name] = get_tuple_name_key(cur_node);
-
-    if (hier_reg_name.empty()) {
-      hier_reg_name = key_name;
-    } else{
-      hier_reg_name = absl::StrCat(key_name, ".", hier_reg_name);
+  int i = 0;
+  for (const auto &subname : subnames) {
+    if (i == 0) { // get rid of ssa postfix
+      auto pos = subname.find_last_of('_');
+      hier_reg_name = absl::StrCat(subname.substr(0, pos));
+    } else if (subname.substr(0, 2) != "__"){  
+      hier_reg_name = absl::StrCat(hier_reg_name, ".", subname);
     }
+    i++;
+  }
 
-    auto parent_dpin = cur_node.get_sink_pin("tuple_name").get_driver_pin();
-    auto parent_node = parent_dpin.get_node();
-    auto ptype       = parent_node.get_type_op();
-    if (ptype == Ntype_op::TupGet) {
-      cur_node = parent_node;
-      continue;
+  for (auto &e : ori_tgq_dpin.out_edges()) {
+    auto sink_node = e.sink.get_node();
+    auto sink_ntype = sink_node.get_type_op();
+    if (sink_ntype == Ntype_op::TupAdd && e.sink == sink_node.setup_sink_pin("value")) {
+      /* note: sometimes the tg(__q_pin) will drive value_dpin of graph output TA chain,
+         but this % TA chain will be deleted before the registers is created, so suddenly
+         the TA sink_pin("value") will disappear. Here I insert an dummy assignment node to
+         make sure there will always a valid spin for the future reg_q_pin to connect to. */
+      auto new_asg_node = tgq_parent_node.get_class_lgraph()->create_node(Ntype_op::Or);
+      auto asg_dpin = new_asg_node.setup_driver_pin();
+      asg_dpin.connect_sink(e.sink);
+      auto asg_spin = new_asg_node.setup_sink_pin("A");
+      reg_name2sink_pins[hier_reg_name].emplace_back(asg_spin);
+      e.del_edge();
+    } else if (sink_ntype == Ntype_op::Or && sink_node.inp_edges().size() == 1) {
+      /* originally, if it's an assignment_OR driven by TG(__q_pin), it will be deleted by cprop,
+         and it will cause the sink_pin floating. Here I put the asg_OR to dont_touch table at the first
+         cprop. This dont_touch table only needed at the first cprop as at the time of second cprop,
+         the TG(__q_pin) should be resolved and th asg_or could be cprop-ed at that time. */
+      dont_touch.insert(sink_node.get_compact());
+      reg_name2sink_pins[hier_reg_name].emplace_back(e.sink);
+    } else {
+      reg_name2sink_pins[hier_reg_name].emplace_back(e.sink);
     }
-
-    // hier_reg_name collection finished!
-    if (ptype != Ntype_op::TupAdd && ptype != Ntype_op::Mux)
-      return false;
-
-    I(parent_dpin.has_name());
-    auto reg_root_ssa_name = parent_dpin.get_name();
-    auto pos = reg_root_ssa_name.find_last_of("_");
-    auto reg_root_name = reg_root_ssa_name.substr(0, pos);
-    hier_reg_name = absl::StrCat(reg_root_name, ".", hier_reg_name);
-
-    for (auto &e : ori_tg_dpin.out_edges()) {
-      auto sink_node = e.sink.get_node();
-      auto sink_ntype = sink_node.get_type_op();
-      if (sink_ntype == Ntype_op::TupAdd && e.sink == sink_node.setup_sink_pin("value")) {
-        // note: sometimes the tg(__q_pin) will drive value_dpin of graph output TA chain,
-        // but this % TA chain will be deleted before the registers is created, so suddenly
-        // the TA sink_pin("value") will disappear. Here I insert an dummy assignment node to
-        // make sure there will always a valid spin for the future reg_q_pin to connect to.
-        auto new_asg_node = cur_node.get_class_lgraph()->create_node(Ntype_op::Or);
-        auto asg_dpin = new_asg_node.setup_driver_pin();
-        asg_dpin.connect_sink(e.sink);
-        auto asg_spin = new_asg_node.setup_sink_pin("A");
-        reg_name2sink_pins[hier_reg_name].emplace_back(asg_spin);
-        e.del_edge();
-      } else if (sink_ntype == Ntype_op::Or && sink_node.inp_edges().size() == 1) {
-        // originally, if it's an assignment_OR driven by TG(__q_pin), it will be deleted by cprop,
-        // and it will cause the sink_pin floating. Here I put the asg_OR to dont_touch table at the first
-        // cprop. This dont_touch table only needed at the first cprop as at the time of second cprop,
-        // the TG(__q_pin) should be resolved and th asg_or could be cprop-ed at that time.
-        dont_touch.insert(sink_node.get_compact());
-        reg_name2sink_pins[hier_reg_name].emplace_back(e.sink);
-      } else {
-        reg_name2sink_pins[hier_reg_name].emplace_back(e.sink);
-      }
-    }
-    return true;
   }
   return true;
 }
@@ -708,7 +675,6 @@ bool Cprop::process_tuple_get(Node &node, XEdge_iterator &inp_edges_ordered) {
   auto parent_node          = parent_dpin.get_node();
   auto [tup_name, key_name] = get_tuple_name_key(node);
   if (key_name == "__q_pin") {
-    fmt::print("DEBUG1: tgq_node:{}\n", node.debug_name());
     auto tg_dpin = node.setup_driver_pin();
     //FIXME->sh: is this for scalar register?
     
@@ -720,7 +686,7 @@ bool Cprop::process_tuple_get(Node &node, XEdge_iterator &inp_edges_ordered) {
     /*   collapse_forward_for_pin(node, parent_dpin); */
     /*   return true; */
     /* } */
-    return reg_q_pin_access_preparation2(parent_node, tg_dpin); // start from parent tg to collect the hierarchical reg name
+    return reg_q_pin_access_preparation(parent_node, tg_dpin); // start from parent tg to collect the hierarchical reg name
   }
 
   // this attr comes from tail of TG chain where the TG tail has been transformed into an AttrSet node.
