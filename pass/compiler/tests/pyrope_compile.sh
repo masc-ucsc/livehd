56dbb20b50def04f0e80543c92155e968a5a1a6d
Sheng Hong Wang <swang203@ucsc.edu>
2021-04-08 19:36:03 -0700

    minor

--- pass/compiler/tests/pyrope_compile.sh
+++ pass/compiler/tests/pyrope_compile.sh
@@ -19,7 +19,7 @@ hier_tuple_nested_if hier_tuple_nested_if3 hier_tuple_nested_if4
 hier_tuple_nested_if5 hier_tuple_nested_if6 hier_tuple_nested_if7 firrtl_tail
 firrtl_gcd_3bits nested_if firrtl_tail3 counter_nested_if tuple_nested1 tuple_empty_attr'
 
-pts='pp'
+# pts='pp'
 # pts='tuple_reg tuple_nested2'
 # pts='tuple_reg tuple_reg2'
 # pts='vector'
@@ -213,7 +213,7 @@ rm -rf ./lgdb
 #Pyrope_compile_hier "$pts_hier2"
 
 # Do not remove verilog, I tend to have tests cases in homedirectory
-#rm -f *.v
+# rm -f *.v
 # rm -f ./*.dot
 # rm -f ./lgcheck*
 # rm -f ./*.tcl
