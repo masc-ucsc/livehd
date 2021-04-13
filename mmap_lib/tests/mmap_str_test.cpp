af54e4e3a6348b37ff95c44ddb4d3b7a69119a60
Sloan Liu <slyliu@ucsc.edu>
2021-04-12 22:41:04 -0700

    pstr gtests

--- mmap_lib/tests/mmap_str_test.cpp
+++ mmap_lib/tests/mmap_str_test.cpp
@@ -3,39 +3,171 @@
 #include <functional>
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 #include "fmt/format.h"
 #include "gtest/gtest.h"
 
+#define RNDN 10 // number of rand strings
+#define MaxLen 32 // max len + 1 for rand strings
+#define MinLen 0  // min len for rand strings
+#define RUN 0
+
 class Mmap_str_test : public ::testing::Test {
-  std::vector<std::vector<std::string>> ast_sorted_verification;
+  //std::vector<std::vector<std::string>> ast_sorted_verification;
+  std::vector<std::string> str_bank;
+  std::vector<std::string> num_bank;
 
 public:
-  void SetUp() override {} // rand gen here
+  void SetUp() override {
+    srand(time(0));
+    uint8_t t_len = 0u, decPos = 0u;
+    bool isNeg = false, isFloat = false;
+    // random string generation
+    for (uint8_t i = 0; i < RNDN; ++i) { // # of strings in vectors
+      std::string ele, num;
+
+      t_len = MinLen + (rand() % MaxLen); // deciding string length (0-31)
+      isNeg = (rand() % 2) ? true : false;
+      isFloat = (rand() % 2) ? true : false;
+                        
+      // construct string with ASCII (32-126) -> 95 chars
+      for(uint8_t j = 0; j < t_len; ++j) { ele += (' ' + (rand() % 95)); }
+      str_bank.push_back(ele); // add string to vector
+
+      //construct string with ASCII (48-57 with - and .) -> 10 chars
+      int idx = 0;
+      if (isNeg) { num += '-'; idx = 1;}
+      if (isFloat) { 
+        decPos = 1 + (rand() % (t_len - 2));
+        for (; idx < decPos; ++idx) { num += ('0' + (rand() % 10)); } 
+        num += '.'; ++idx; 
+        for (; idx < t_len; ++idx) { num += ('0' + (rand() % 10)); }
+      } else {
+        for (auto k = 0; k < t_len; ++k) { num += ('0' + (rand() % 10)); }
+      }
+      num_bank.push_back(num);
+
+    }   
+    //for (auto i = str_bank.begin(); i != str_bank.end(); ++i) {std::cout << *i << "\n";}
+    for (auto i = num_bank.begin(); i != num_bank.end(); ++i) {std::cout << *i << "\n";}
+  }
+
+  // wrapper for vector.at() since str_bank is private
+  std::string get(int i) { return str_bank.at(i); }
+  std::string nget(int i) { return num_bank.at(i); }
 };
 
-#if 1
 // TEST_F are different types of tests
-TEST_F(Mmap_str_test, small_strings) {
+// >> TEST_F(class_name, test_name) {
+//      tests in here
+//    }
 
-  mmap_lib::str a("hello");
-  std::string_view a_sv{"hello"};
+TEST_F(Mmap_str_test, basic_ctor) {
+  mmap_lib::str a1("hello"), a2("hello");
+  
+  EXPECT_EQ(a1, a2);
 
-  mmap_lib::str b("hello");
+  std::string_view b_sv("schizophrenia");
+  std::string b_st("schizophrenia");
+  mmap_lib::str b1(b_sv), b2(b_st), b3("schizophrenia");
 
-  EXPECT_EQ(a, b);
+  EXPECT_EQ(b3, b1);
+  EXPECT_EQ(b3, b2);
+  
+  std::string_view c_sv("neutralization");
+  std::string c_st("neutralization");
+  mmap_lib::str c1(c_sv), c2(c_st), c3("neutralization");
+
+  EXPECT_EQ(c3, c1);
+  EXPECT_EQ(c3, c2);
+  
+  std::string_view d_sv("--this_var_will_be_very_longbuzzball");
+  std::string d_st("--this_var_will_be_very_longbuzzball");
+  mmap_lib::str d1(d_sv), d2(d_st), d3("--this_var_will_be_very_longbuzzball");
+
+  EXPECT_EQ(d3, d1);
+  EXPECT_EQ(d3, d2);
 }
 
-bool do_check(const mmap_lib::str &str) {
-  return (str == "hello");
-}
-bool do_check_string(const std::string &str) {
-  return (str == "hello");
-}
-bool do_check_sv(std::string_view str) {
-  return (str == "hello");
+TEST_F(Mmap_str_test, random_ctor) {
+  for (auto i = 0; i < RNDN; ++i) {
+    std::string r_st = get(i);
+    //std::cout << r_st << std::endl;
+    std::string_view r_sv = r_st;
+    mmap_lib::str r1(r_sv), r2(r_st), r3(r_st.c_str());
+    EXPECT_EQ(r3, r1);
+    EXPECT_EQ(r3, r2);
+  }
 }
 
+
+TEST_F(Mmap_str_test, random_comparisons) {
+  for (auto i = 0; i < RNDN; ++i) {
+    // from the str_bank, get string at current index and next index
+    std::string c_st = get(i % RNDN), n_st = get((i+1) % RNDN);
+    std::string_view c_sv = c_st, n_sv = n_st;
+    mmap_lib::str c_s1(c_st), n_s1(n_st), c_s2(c_sv), n_s2(c_sv);
+    
+    EXPECT_TRUE(c_s1 == c_s2);
+    EXPECT_TRUE(c_s1 == c_st);
+    EXPECT_TRUE(c_s1 == c_sv);
+    EXPECT_TRUE(c_s1 == c_st.c_str());
+
+    EXPECT_FALSE(c_s1 != c_s2); 
+    EXPECT_FALSE(c_s1 != c_st); 
+    EXPECT_FALSE(c_s1 != c_sv);
+    EXPECT_FALSE(c_s1 != c_st.c_str());
+  
+    //tests for next and curr
+    EXPECT_TRUE(c_s1 != n_s1);
+    EXPECT_TRUE(c_s1 != n_st);
+    EXPECT_TRUE(c_s1 != n_sv);
+    EXPECT_TRUE(c_s1 != n_st.c_str());
+    
+    EXPECT_FALSE(n_s1 == c_s1); 
+    EXPECT_FALSE(n_s1 == c_st); 
+    EXPECT_FALSE(n_s1 == c_sv);    
+    EXPECT_FALSE(n_s1 == c_st.c_str());
+  }
+}
+
+TEST_F(Mmap_str_test, random_at_operator) {
+  for (auto i = 0; i < RNDN; ++i) {
+    std::string hold = get(i);
+    mmap_lib::str temp(hold);
+    for (auto j = 0; j < temp.size(); ++j) {
+      EXPECT_EQ(hold[j], temp[j]);
+    }
+  }  
+}
+
+bool isi(std::string st) {
+  int idx = 0; // index start at 0
+  if ((st[0] != '-') && ((st[0] - '0' < 0) || (st[0] - '0' > 9))) {
+    return false; // first char not - or num, not integer
+  } else if (st[0] == '-') {
+    idx = 1; // first char is 
+  }
+  for (; idx < st.size(); ++idx) {
+    if ((st[idx] - '0' < 0) || (st[idx] - '0' > 9)) {
+      return false;
+    } 
+  }
+}
+
+TEST_F(Mmap_str_test, isI_operator) {
+  for (auto i = 0; i < RNDN; ++i) {
+    std::string hold = nget(i);
+    mmap_lib::str temp(hold); 
+    EXPECT_EQ(temp.is_i(), isi(hold));
+  }
+}
+
+
+
+#if 0
 TEST_F(Mmap_str_test, const_expr_trival_cmp) {
 
   mmap_lib::str long_a("hello_hello_hello_hello_hello_hello"); // not right, but it should compile
@@ -51,11 +183,13 @@ TEST_F(Mmap_str_test, const_expr_trival_cmp) {
   EXPECT_TRUE(do_check_string("hello"));
   EXPECT_TRUE(do_check_sv("hello"));
 
+  /*
   fmt::print("a[0]:{} size:{}\n",a[0], a.size());
   fmt::print("a[1]:{} size:{}\n",a[1], a.size());
   fmt::print("a[2]:{} size:{}\n",a[2], a.size());
   fmt::print("a[3]:{} size:{}\n",a[3], a.size());
   fmt::print("a[4]:{} size:{}\n",a[4], a.size());
+  */
 
   assert(a[0] == 'h');   // compile time check
   assert(a[1] == 'e');   // compile time check
@@ -68,5 +202,12 @@ TEST_F(Mmap_str_test, const_expr_trival_cmp) {
   //static_assert(a!="helxo");    // compile time check
   //static_assert(a!="hell");     // compile time check
   //static_assert(a!="hellox");   // compile time check
+}
+#endif
+
+#if RUN
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
 #endif
