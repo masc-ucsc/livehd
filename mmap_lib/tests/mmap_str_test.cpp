e13df37f1c958eccdbd465527c55389ecb8430e7
Sloan Liu <slyliu@ucsc.edu>
2021-04-26 12:47:18 -0700

    building more vectors for pstr testbench

--- mmap_lib/tests/mmap_str_test.cpp
+++ mmap_lib/tests/mmap_str_test.cpp
@@ -20,46 +20,44 @@ class Mmap_str_test : public ::testing::Test {
   //std::vector<std::vector<std::string>> ast_sorted_verification;
   std::vector<std::string> str_bank;
   std::vector<std::string> num_bank;
+  std::vector<std::string> no_underscore;
+  std::vector<std::string> no_dash;
 
 public:
 
   void SetUp() override {
     srand(time(0));
-    uint8_t t_len = 0u, decPos = 0u;
+    uint8_t t_len = 0u;
     bool isNeg = false, isFloat = false;
     // random string generation
     for (uint8_t i = 0; i < RNDN; ++i) { // # of strings in vectors
-      std::string ele;
+      std::string ele, ele2, ele3, ele4;
       t_len = MinLen + (rand() % (MaxLen-MinLen)); // deciding string length (0-31)
       // construct string with ASCII (32-126) -> 95 chars
       for(uint8_t j = 0; j < t_len; ++j) { 
+        uint8_t hd = rand() % 94;
         if (j % 2 == 0) {
-          uint8_t hd = rand() % 94;
-          while (hd >= 15 && hd <= 24) {
-            hd = rand() % 94;
-          }
+          while (hd >= 15 && hd <= 24) { hd = rand() % 94; }
           ele += ('!' + hd);
-        } else {
-          ele += ('!' + (rand() % 94)); 
-        }
+        } else { ele += ('!' + (rand() % 94)); }
+
+        ele3 += (('!' + hd) == '_') ? ('_' + 1) : ('!' + hd); 
+        ele4 += (('!' + hd) == '-') ? ('-' + 1) : ('!' + hd); 
       }
       str_bank.push_back(ele); // add string to vector
-    }
-
-    for (uint8_t i = 0; i < RNDN; ++i) {
-      std::string ele;
+      no_underscore.push_back(ele3); // add to no underscore
+      no_dash.push_back(ele4); // add to no dash
+  
       t_len = MinNum + (rand() % (MaxNum-MinNum));
       for (uint8_t j = 0; j < t_len; ++j) { 
         if (j == 0) {
           if ((rand() % 2) == 1) { 
-            ele += '-';
-            ele += ('1' + (rand() % 9)); 
-          } else { 
-            ele += ('1' + (rand() % 9)); 
-          }
-        } else { ele += ('0' + (rand() % 10)); }
+            ele2 += '-';
+            ele2 += ('1' + (rand() % 9)); 
+          } else { ele2 += ('1' + (rand() % 9)); }
+        } else { ele2 += ('0' + (rand() % 10)); }
       }
-      num_bank.push_back(ele);
+      num_bank.push_back(ele2);
     }
   }
 
@@ -68,44 +66,31 @@ public:
   std::string n_get(int i) { return num_bank.at(i); }
 };
 
-// TEST_F are different types of tests
-// >> TEST_F(class_name, test_name) {
-//      tests in here
-//    }
 
 TEST_F(Mmap_str_test, basic_ctor) {
   mmap_lib::str a1("hello"), a2("hello");
-  
   EXPECT_EQ(a1, a2);
 
   std::string_view b_sv("schizophrenia");
   std::string b_st("schizophrenia");
   mmap_lib::str b1(b_sv), b2(b_st), b3("schizophrenia");
-
   EXPECT_EQ(b3, b1);
   EXPECT_EQ(b3, b2);
   
   std::string_view c_sv("neutralization");
   std::string c_st("neutralization");
   mmap_lib::str c1(c_sv), c2(c_st), c3("neutralization");
-
   EXPECT_EQ(c3, c1);
   EXPECT_EQ(c3, c2);
   
   std::string_view d_sv("--this_var_will_be_very_longbuzzball");
   std::string d_st("--this_var_will_be_very_longbuzzball");
   mmap_lib::str d1(d_sv), d2(d_st), d3("--this_var_will_be_very_longbuzzball");
-
   EXPECT_EQ(d3, d1);
   EXPECT_EQ(d3, d2);
 
   mmap_lib::str empT("");
   mmap_lib::str empT2("");
-
-  std::cout << "testing empty str ctor:\n";
-  empT.print_string();
-  std::cout << std::endl;
-  empT2.print_string();
 }
 
 // random mmap_lib::str creation
@@ -200,11 +185,8 @@ TEST_F(Mmap_str_test, isI_operator) {
     std::cout << "\n";
     #endif
 
-    if ((rand() % 100) >= 50) {
-      EXPECT_TRUE(str1.is_i());
-    } else {
-      EXPECT_FALSE(str2.is_i());
-    }
+    if ((rand() % 100) >= 50) { EXPECT_TRUE(str1.is_i()); } 
+    else { EXPECT_FALSE(str2.is_i()); }
   }
 }
 
@@ -226,10 +208,8 @@ TEST_F(Mmap_str_test, starts_ends_with) {
     mmap_lib::str temp(orig);    // mmap_lib::str creation
 
     // gen start/end indx
-    if (temp.size() == 0) { 
-      end_sw = 0; 
-      start_ew = 0; 
-    } else { 
+    if (temp.size() == 0) { end_sw = 0; start_ew = 0; } 
+    else { 
       end_sw = (rand() % temp.size()) + 1; 
       start_ew = rand() % temp.size();
     }
@@ -240,7 +220,6 @@ TEST_F(Mmap_str_test, starts_ends_with) {
     std::string_view sv_check_ew = stable_ew;
     mmap_lib::str check_sw(stable_sw); // str
     mmap_lib::str check_ew(stable_ew); 
-
 
     #if 0    
     std::cout << "pstr temp: ";
@@ -372,21 +351,17 @@ TEST_F(Mmap_str_test, find) {
     std::string next = s_get((i+1) % RNDN);
     uint32_t start=0, start2=0, end=0, end2=0;    
     
-    
-    if (curr.size() == 0) { 
-      start = 0; end = 0;
-    } else { 
+    if (curr.size() == 0) { start = 0; end = 0; } \
+    else { 
       start = rand() % curr.size(); 
       end = (rand() % (curr.size()-start)) + 1; 
     }
     
-    if (next.size() == 0) { 
-      start2 = 0; end2 = 0;
-    } else { 
+    if (next.size() == 0) { start2 = 0; end2 = 0; } 
+    else { 
       start2 = rand() % next.size(); 
       end2 = (rand() % (next.size()-start2)) + 1; 
     }
-    
     
     std::string stable_c = curr.substr(start, end);
     std::string stable_n = next.substr(start2, end2);
@@ -422,21 +397,17 @@ TEST_F(Mmap_str_test, substr) {
     std::string next = s_get((i+1) % RNDN);
     uint32_t start=0, start2=0, end=0, end2=0;    
     
-    
-    if (curr.size() == 0) { 
-      start = 0; end = 0;
-    } else { 
+    if (curr.size() == 0) { start = 0; end = 0; } 
+    else { 
       start = rand() % curr.size(); 
       end = (rand() % (curr.size()-start)) + 1; 
     }
     
-    if (next.size() == 0) { 
-      start2 = 0; end2 = 0;
-    } else { 
+    if (next.size() == 0) { start2 = 0; end2 = 0; } 
+    else { 
       start2 = rand() % next.size(); 
       end2 = (rand() % (next.size()-start2)) + 1; 
     }
-    
     
     std::string stable_c = curr.substr(start, end);
     std::string stable_n = next.substr(start2, end2);
@@ -446,7 +417,6 @@ TEST_F(Mmap_str_test, substr) {
     mmap_lib::str next_str(next);
     mmap_lib::str curr_sub = curr_str.substr(start, end);
     mmap_lib::str next_sub = next_str.substr(start2, end2);
-
 
     #if 0
     std::cout << "curr_str: ";
@@ -472,43 +442,11 @@ TEST_F(Mmap_str_test, substr) {
   }
 }
 
-#if 0
-TEST_F(Mmap_str_test, const_expr_trival_cmp) {
 
-  mmap_lib::str long_a("hello_hello_hello_hello_hello_hello"); // not right, but it should compile
-  constexpr mmap_lib::str a("hello");
+TEST_F(Mmap_str_test, split) {
 
-  auto b = do_check("hello");
-  EXPECT_TRUE(b);
-
-  std::string_view a_sv{"hello"};
-
-  EXPECT_TRUE(do_check(a_sv));
-
-  EXPECT_TRUE(do_check_string("hello"));
-  EXPECT_TRUE(do_check_sv("hello"));
-
-  /*
-  fmt::print("a[0]:{} size:{}\n",a[0], a.size());
-  fmt::print("a[1]:{} size:{}\n",a[1], a.size());
-  fmt::print("a[2]:{} size:{}\n",a[2], a.size());
-  fmt::print("a[3]:{} size:{}\n",a[3], a.size());
-  fmt::print("a[4]:{} size:{}\n",a[4], a.size());
-  */
-
-  assert(a[0] == 'h');   // compile time check
-  assert(a[1] == 'e');   // compile time check
-  assert(a[2] == 'l');   // compile time check
-  assert(a[3] == 'l');   // compile time check
-  assert(a[4] == 'o');   // compile time check
-  assert(a.size() == 5); // compile time check
-  // TODO: static_assert(a==a_sv);       // compile time check
-  assert(a=="hello");    // compile time check
-  //static_assert(a!="helxo");    // compile time check
-  //static_assert(a!="hell");     // compile time check
-  //static_assert(a!="hellox");   // compile time check
 }
-#endif
+
 
 #if RUN
 int main(int argc, char **argv) {
