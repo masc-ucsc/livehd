7f1888e18ae59d74809823ffcf5d64b4edddff3e
Jose Renau <renau@ucsc.edu>
2021-04-28 15:03:02 -0700

    address replicated port generation

--- inou/cgen/cgen_verilog.cpp
+++ inou/cgen/cgen_verilog.cpp
@@ -506,7 +506,11 @@ void Cgen_verilog::create_registers(std::string &buffer, Lgraph *lg) {
 }
 
 void Cgen_verilog::add_to_pin2var(std::string &buffer, Node_pin &dpin, const std::string &name, bool out_unsigned) {
-  pin2var.emplace(dpin.get_compact_class(), name);
+  
+  auto [it, replaced] = pin2var.insert({dpin.get_compact_class(), name});
+  if (!replaced)
+    return;
+
   int bits = dpin.get_bits();
 
   std::string reg_str;
