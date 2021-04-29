7dd7787ac154699bc67dbfa7698d4464d71dc201
Jose Renau <renau@ucsc.edu>
2021-04-28 19:06:26 -0700

    missing mmap_str.cpp file

---
+++ mmap_lib/include/mmap_str.cpp
@@ -0,0 +1,6 @@
+//  This file is distributed under the BSD 3-Clause License. See LICENSE for details.
+
+#include "mmap_str.hpp"
+
+mmap_lib::map<std::string_view, uint32_t> mmap_lib::str::string_map2;
+
