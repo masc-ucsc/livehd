4c407d3f275fa1e4221f10e46cbf65d9a954fa25
Jose Renau <renau@ucsc.edu>
2021-05-28 09:44:28 -0700

    fix paths that trigger regression error

--- mmap_lib/include/mmap_str.cpp
+++ mmap_lib/include/mmap_str.cpp
@@ -6,13 +6,13 @@ template <int map_id>
 mmap_lib::map<std::string_view, bool> mmap_lib::str<map_id>::m0;
 
 template <int map_id>
-mmap_lib::map<std::string_view, bool> mmap_lib::str<map_id>::m1("lgdb/str_use", "strMap1");
+mmap_lib::map<std::string_view, bool> mmap_lib::str<map_id>::m1("lgdb", "strMap1");
 
 template <int map_id>
-mmap_lib::map<std::string_view, bool> mmap_lib::str<map_id>::m2("lgdb/str_use", "strMap2");
+mmap_lib::map<std::string_view, bool> mmap_lib::str<map_id>::m2("lgdb", "strMap2");
 
 template <int map_id>
-mmap_lib::map<std::string_view, bool> mmap_lib::str<map_id>::m3("lgdb/str_use", "strMap3");
+mmap_lib::map<std::string_view, bool> mmap_lib::str<map_id>::m3("lgdb", "strMap3");
 
 template class mmap_lib::str<0>;
 template class mmap_lib::str<1>;
