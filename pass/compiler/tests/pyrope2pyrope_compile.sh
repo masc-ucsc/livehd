cb9f8b4ba614a0453456d2b1bffdb6951be9c19c
sakshi garg <sgarg3@ucsc.edu>
2021-06-20 14:06:11 -0700

    partial.prp is still under progress

--- pass/compiler/tests/pyrope2pyrope_compile.sh
+++ pass/compiler/tests/pyrope2pyrope_compile.sh
@@ -10,7 +10,7 @@ pts_long_time='firrtl_gcd'
 pts_after_micro='hier_tuple4 tuple_reg3 '
 
 
-pts='flatten_bundle partial hier_tuple reg_bits_set reg__q_pin hier_tuple_io hier_tuple3
+pts='flatten_bundle hier_tuple reg_bits_set reg__q_pin hier_tuple_io hier_tuple3
 hier_tuple2 tuple_if ssa_rhs out_ssa attr_set if2 lhs_wire
 tuple_copy if hier_tuple_nested_if2 lhs_wire2 tuple_copy2 counter
 adder_stage capricious_bits4 logic capricious_bits2 tuple_reg 
