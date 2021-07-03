30878a3a145c4fbca2140db50ca6e2c1e47c9f00
swang203 <swang203@ucsc.edu>
2021-07-02 20:37:40 -0700

    remove simplified test case in firrtl

--- inou/firrtl/tests/verilog_gld/SimplifiedClockGroupAggregator.gld.v
+++
@@ -1,9 +0,0 @@
-module SimplifiedClockGroupAggregator(
-  input   auto_in_member_3_clock,
-  input   auto_in_member_3_reset,
-  output  auto_out_2_member_0_clock,
-  output  auto_out_2_member_0_reset
-);
-  assign auto_out_2_member_0_clock = auto_in_member_3_clock; // @[Nodes.scala 389:84 LazyModule.scala 181:31]
-  assign auto_out_2_member_0_reset = auto_in_member_3_reset; // @[Nodes.scala 389:84 LazyModule.scala 181:31]
-endmodule
--- inou/firrtl/tests/verilog_gld/SimplifiedSimpleClockGroupSource.gld.v
+++
@@ -1,9 +0,0 @@
-module SimplifiedSimpleClockGroupSource(
-  input   clock,
-  input   reset,
-  output  auto_out_member_0_clock,
-  output  auto_out_member_0_reset
-);
-  assign auto_out_member_0_clock = clock; // @[Nodes.scala 388:84 ClockGroup.scala 66:36]
-  assign auto_out_member_0_reset = reset; // @[Nodes.scala 388:84 ClockGroup.scala 66:54]
-endmodule
