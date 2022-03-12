7848177d77ccd8303783dfd134ab8da387ceb3a1
swang203 <swang203@ucsc.edu>
2022-03-11 21:29:12 -0800

    firrtl comparator_op are always unsigned and need to transform to signed representation by get_mask

--- inou/firrtl/tests/verilog_gld/AMOALU.gld.v
+++ inou/firrtl/tests/verilog_gld/AMOALU.gld.v
@@ -8,9 +8,7 @@ module AMOALU(
   output [63:0] io_out,
   output [63:0] io_out_unmasked
 );
-  wire  _T_9 = ~io_mask[3]; // @[AMOALU.scala 72:63]
-  wire [31:0] _T_10 = {_T_9, 31'h0}; // @[AMOALU.scala 72:79]
-  wire [63:0] _T_11 = {{32'd0}, _T_10}; // @[AMOALU.scala 72:98]
-  assign io_out = ~_T_11; // @[AMOALU.scala 72:16]
-  assign io_out_unmasked = ~_T_11; // @[AMOALU.scala 72:16]
+  wire  _T_23 = io_lhs[63] == io_rhs[63]; // @[AMOALU.scala 88:18]
+  assign io_out = {{63'd0}, _T_23}; // @[AMOALU.scala 104:10]
+  assign io_out_unmasked = {{63'd0}, _T_23}; // @[AMOALU.scala 105:19]
 endmodule
--- pass/firmap/firmap.cpp
+++ pass/firmap/firmap.cpp
@@ -652,17 +652,28 @@ void Firmap::map_node_fir_neq(Node &old_node, Lgraph *new_lg, PinMap &pinmap) {
   for (auto old_spin : old_node.inp_connected_pins()) pinmap.insert_or_assign(old_spin, new_node_eq.setup_sink_pin("A"));
 
   new_node_eq.connect_driver(new_node_not.setup_sink_pin("a"));
+
+
+  auto new_node_tp = new_lg->create_node(Ntype_op::Get_mask);
+  new_node_tp.setup_sink_pin("a").connect_driver(new_node_not.setup_driver_pin());  // mask -> tp
+  new_node_tp.setup_sink_pin("mask").connect_driver(new_lg->create_node_const(-1));
+
+
   for (auto old_dpin : old_node.out_connected_pins()) {
-    pinmap.insert_or_assign(old_dpin, new_node_not.setup_driver_pin());
+    pinmap.insert_or_assign(old_dpin, new_node_tp.setup_driver_pin());
   }
 }
 
 void Firmap::map_node_fir_eq(Node &old_node, Lgraph *new_lg, PinMap &pinmap) {
   auto new_node = new_lg->create_node(Ntype_op::EQ);
   for (auto old_spin : old_node.inp_connected_pins()) pinmap.insert_or_assign(old_spin, new_node.setup_sink_pin("A"));
+  
+  auto new_node_tp = new_lg->create_node(Ntype_op::Get_mask);
+  new_node_tp.setup_sink_pin("a").connect_driver(new_node.setup_driver_pin());  // mask -> tp
+  new_node_tp.setup_sink_pin("mask").connect_driver(new_lg->create_node_const(-1));
 
   for (auto old_dpin : old_node.out_connected_pins()) {
-    pinmap.insert_or_assign(old_dpin, new_node.setup_driver_pin());
+    pinmap.insert_or_assign(old_dpin, new_node_tp.setup_driver_pin());
   }
 }
 
@@ -685,8 +696,14 @@ void Firmap::map_node_fir_leq_geq(Node &old_node, Lgraph *new_lg, std::string_vi
 
   new_node_cmp.connect_driver(new_node_not.setup_sink_pin("a"));
 
+
+  auto new_node_tp = new_lg->create_node(Ntype_op::Get_mask);
+  new_node_tp.setup_sink_pin("a").connect_driver(new_node_not.setup_driver_pin());  // mask -> tp
+  new_node_tp.setup_sink_pin("mask").connect_driver(new_lg->create_node_const(-1));
+
+
   for (auto old_dpin : old_node.out_connected_pins()) {
-    pinmap.insert_or_assign(old_dpin, new_node_not.setup_driver_pin());
+    pinmap.insert_or_assign(old_dpin, new_node_tp.setup_driver_pin());
   }
 }
 
@@ -705,8 +722,15 @@ void Firmap::map_node_fir_lt_gt(Node &old_node, Lgraph *new_lg, std::string_view
     }
   }
 
+
+
+  auto new_node_tp = new_lg->create_node(Ntype_op::Get_mask);
+  new_node_tp.setup_sink_pin("a").connect_driver(new_node.setup_driver_pin());  // mask -> tp
+  new_node_tp.setup_sink_pin("mask").connect_driver(new_lg->create_node_const(-1));
+
+
   for (auto old_dpin : old_node.out_connected_pins()) {
-    pinmap.insert_or_assign(old_dpin, new_node.setup_driver_pin());
+    pinmap.insert_or_assign(old_dpin, new_node_tp.setup_driver_pin());
   }
 }
 
