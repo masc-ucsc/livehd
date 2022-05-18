e792ab5f970e98f6a75f9cb14b44db9d3a33ee35
sakshi garg <sgarg3@ucsc.edu>
2022-05-17 20:10:43 -0700

    adding the vector implementation to keep redundant nodes. To do: make hash of vectors after the flow stabilizes.

--- inou/paper_pass/traverse_lg.cpp
+++ inou/paper_pass/traverse_lg.cpp
@@ -10,11 +10,18 @@
 #include <bits/stdc++.h>
 #include <utility>
 #include <string>
+#include <algorithm>
+
+
+/*define only 1 of these:*/
+#define DEBUG //print everything
+#define KEEP_DUP //use vector
+//#define DE_DUP //use set 
 
 static Pass_plugin sample("traverse_lg", Traverse_lg::setup);
 
 void Traverse_lg::setup() {
-  Eprp_method m1("inou.traverse_lg", "Prints all nodes of LG", &Traverse_lg::travers);
+  Eprp_method m1("inou.traverse_lg", "Prints all nodes (and each of their IOs) of LG", &Traverse_lg::travers);
   //m1.add_label_optional("odir", "path to print the text to", ".");
   register_pass(m1);
 }
@@ -29,50 +36,105 @@ void Traverse_lg::travers(Eprp_var& var) {
   Traverse_lg p(var);
 
   for (const auto& l : var.lgs) {
-    p.do_travers(l, l->get_name());  // l->get_name gives the name of the top module (generally same as that of file name)
+    p.do_travers(l);  // l->get_name gives the name of the top module (generally same as that of file name)
   }
 }
 
-void Traverse_lg::do_travers(Lgraph* lg, std::string_view module_name) {
+void Traverse_lg::do_travers(Lgraph* lg) {
+#ifdef DEBUG
+  //std::string_view module_name = lg->get_name();
   std::ofstream ofs;
-  ofs.open(std::string(module_name), std::ofstream::out | std::ofstream::trunc);
+  ofs.open(std::string(lg->get_name()), std::ofstream::out | std::ofstream::trunc);
   if(!ofs.is_open()) {
-    std::cout<<"unable to open file "<<module_name<<".txt"<<std::endl;
+    fmt::print("unable to open file {}.txt\n",lg->get_name());
     return ;
   }
+  ofs<<std::endl<<std::endl<<lg->get_name()<<std::endl<<std::endl<<std::endl;
+#endif
 
-  ofs<<std::endl<<std::endl<<module_name<<std::endl<<std::endl<<std::endl;
-
-  for (const auto& node : lg->fast()) {
+  for (const auto& node : lg->forward()) {
+#ifdef DEBUG
     ofs<<"===============================\n";
-    ofs<<node.debug_name()<<std::endl;
+    ofs<<node.debug_name()<<std::endl; //this is the main node for which we will record IO
+#endif
+#ifdef KEEP_DUP
+    std::vector<std::string> in_vec;
+    std::vector<std::string> out_vec;
+    fmt::print("{}\n", node.debug_name());
+#endif
     if (! (node.has_inputs() || node.has_outputs())) {//no i/ps as well as no o/ps
+#ifdef DEBUG
       ofs<<"---------------\n";
+#endif
+      continue; //do not keep such nodes in nodeIOmap
     }
 
     
     if (node.has_inputs()){
+#ifdef DEBUG
     ofs<<"INPUTS:\n";
+#endif
       for (const auto& indr : node.inp_drivers()) {
         //auto inp_node = indr.get_node();
+#ifdef DEBUG
         get_input_node(indr, ofs);
+#endif
+#ifdef KEEP_DUP
+        get_input_node(indr, in_vec);
+#endif
       }
     }
 
     if (node.has_outputs()) {
+#ifdef DEBUG
     ofs<<"OUTPUTS:\n";
+#endif
       for (const auto& outdr : node.out_sinks() ) {//outdr is the pin of the output node.
         //auto out_node = outdr.get_node();
+#ifdef DEBUG
         get_output_node(outdr,ofs);
+#endif
+#ifdef KEEP_DUP
+        get_output_node(outdr, out_vec);
+#endif
       }
     }
-  }
 
+#ifdef KEEP_DUP
+    //print the vectors formed
+    fmt::print("INPUTS:\n");
+    for (const auto& i:in_vec) {
+      fmt::print("\t{}\n",i);
+    }
+    fmt::print("OUTPUTS:\n");
+    for (const auto& i:out_vec) {
+      fmt::print("\t{}\n",i);
+    }
+
+    if (in_vec.empty() && out_vec.empty()) {//no i/ps as well as no o/ps
+      continue;//do not keep such nodes in nodeIOmap
+    }
+    //insert in map
+    const auto& nodeid = node.get_nid().value;
+    nodeIOmap[nodeid]= std::make_pair(in_vec,out_vec);//FIXME: make hash of vectors and change datatype accordingly
+#endif
+  }//enf of for lg-> traversal
+
+#ifdef KEEP_DUP
+  //print the map
+  for(auto& [n, ioPair]: nodeIOmap) {
+    //fmt::print("{} has INPUTS: {} AND OUTPUTS: {}\n",node, ioPair.first, mapval.second.second );
+    fmt::print("{} has INPUTS:  \n",n);
+  }
+#endif
+
+#ifdef DEBUG
   if(ofs.is_open()) {
     ofs.close();
   }
+#endif
 }
-
+//FOR DEBUG:
 void Traverse_lg::get_input_node(const Node_pin &node_pin, std::ofstream& ofs) {
   auto node = node_pin.get_node();
   if(node.is_type_flop() || node.is_type_const() || node.is_graph_input() ) {
@@ -109,6 +171,71 @@ void Traverse_lg::get_output_node(const Node_pin &node_pin, std::ofstream& ofs) 
       for (const auto& outdr : node.out_sinks() ) {
         //auto out_node = outdr.get_node();
         get_output_node(outdr,ofs);
+      }
+  }
+}
+
+//FOR VECTOR:
+
+void Traverse_lg::get_input_node(const Node_pin &node_pin, std::vector<std::string>& in_vec) {
+  auto node = node_pin.get_node();
+  if(node.is_type_flop() || node.is_type_const() || node.is_graph_input() ) {
+    if(node.is_graph_io()) {
+      in_vec.emplace_back(node_pin.has_name()?node_pin.get_name():node_pin.get_pin_name());
+    } else {
+      std::string temp_str (node.get_type_name());
+      if(node.is_type_const()){ 
+        temp_str+=":"; 
+        temp_str+=node.get_type_const().to_pyrope();
+      }
+      else if(node.is_type_flop()){ 
+        temp_str+=":";
+        temp_str+=node_pin.get_pin_name();
+        temp_str+="->";
+        //temp_str+=node.get_driver_pin().get_pin_name();
+        //temp_str+="(";
+        temp_str+=(node.get_driver_pin().has_name()?node.get_driver_pin().get_name():"");
+        //temp_str+=")";
+      }
+      in_vec.emplace_back(temp_str);
+    }
+    return;
+  } else {
+      for (const auto& indr : node.inp_drivers()) {
+        //auto inp_node = indr.get_node();
+        get_input_node(indr, in_vec);
+      }
+  }
+}
+
+void Traverse_lg::get_output_node(const Node_pin &node_pin, std::vector<std::string>& out_vec) {
+  auto node = node_pin.get_node();
+  if(node.is_type_flop() || node.is_graph_output() ) {
+    if(node.is_graph_io()) {
+      //out_vec.emplace_back(node_pin.has_name()?node_pin.get_name():node_pin.get_pin_name());
+      out_vec.emplace_back(node_pin.get_pin_name());
+    } else {
+      std::string temp_str(node.get_type_name());
+      if(node.is_type_const()){ 
+        temp_str+=":"; 
+        temp_str+=node.get_type_const().to_pyrope();
+      }
+      else if(node.is_type_flop()){ 
+        temp_str+=":";
+        temp_str+=node_pin.get_pin_name();
+        temp_str+="->";
+        //temp_str+=node.get_driver_pin().get_pin_name();
+        //temp_str+="(";
+        temp_str+=(node.get_driver_pin().has_name()?node.get_driver_pin().get_name():"");
+        //temp_str+=")";
+      }
+      out_vec.emplace_back(temp_str);
+    }
+    return;
+  } else {
+      for (const auto& outdr : node.out_sinks() ) {
+        //auto out_node = outdr.get_node();
+        get_output_node(outdr, out_vec);
       }
   }
 }
--- inou/paper_pass/traverse_lg.hpp
+++ inou/paper_pass/traverse_lg.hpp
@@ -3,16 +3,26 @@
 #pragma once
 
 #include <string_view>
+#include <utility>
+#include <vector>
 
 #include "cell.hpp"
 #include "lgraph.hpp"
+#include "node.hpp"
 #include "pass.hpp"
+#include "absl/container/flat_hash_map.h"
 
 class Traverse_lg : public Pass {
+private:
+  absl::node_hash_map<unsigned int, std::pair<std::vector<std::string>, std::vector<std::string>>> nodeIOmap;
 protected:
-  void do_travers(Lgraph* g, std::string_view module_name);
+  void do_travers(Lgraph* g);
+  //FOR DEBUG:
   void get_input_node(const Node_pin &pin, std::ofstream& ofs);
   void get_output_node(const Node_pin &pin, std::ofstream& ofs);
+  //FOR VECT PART:
+  void get_input_node(const Node_pin &pin, std::vector<std::string>& in_vec);
+  void get_output_node(const Node_pin &pin, std::vector<std::string>& out_vec);
 
 public:
   static void travers(Eprp_var& var);
