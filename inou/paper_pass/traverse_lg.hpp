3bb1d3615ce676fbed7448918a54000ffaacf2e9
sakshi garg <sgarg3@ucsc.edu>
2022-06-03 21:22:06 -0700

    resolving issue due to absl and std

--- inou/paper_pass/traverse_lg.hpp
+++ inou/paper_pass/traverse_lg.hpp
@@ -29,7 +29,8 @@ public:
   typedef absl::node_hash_map<std::pair<std::set<std::string>, std::set<std::string>>, std::vector<Node::Compact_flat> > setMap_pairKey;
 private:
   absl::node_hash_map<Node::Compact_flat , std::vector<Node::Compact_flat> > matched_map;
-  absl::node_hash_map<Node::Compact_flat, std::pair<absl::btree_set<std::string>, absl::btree_set<std::string>>> unmatched_map;
+  //absl::node_hash_map<Node::Compact_flat, std::pair<absl::btree_set<std::string>, absl::btree_set<std::string>>> unmatched_map;
+  absl::node_hash_map<Node::Compact_flat, std::pair<std::set<std::string>, std::set<std::string>>> unmatched_map;
 /*
 #ifdef KEEP_DUP
   vecMap nodeIOmap;
