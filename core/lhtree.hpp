2f94661eda4d7a32a4d83c2cb3a8fe0396e510e8
Jose Renau <renau@ucsc.edu>
2022-09-18 01:51:13 -0700

    recover old doc

--- core/lhtree.hpp
+++ core/lhtree.hpp
@@ -1,6 +1,270 @@
 //  This file is distributed under the BSD 3-Clause License. See LICENSE for details.
 #pragma once
 
+// TODO: Decide order and fields. Best order:
+// 01: ?? | 1
+// 02: ?? -── 1.1
+// 03: ?? ├── 1.2
+// 04: ?? │   -── 1.2.1
+// 05: ?? │       -── 1.2.1.1
+// 06: ?? │       ├── 1.2.1.2
+// 07: ?? ├── 1.3
+// 08: ?? │   -── 1.3.1
+// 09: ?? │   ├── 1.3.2
+// 10: ?? │   ├── 1.3.3
+// 11: ?? │       -── 1.3.1.1
+// 12: ?? │       |── 1.3.1.2
+// 13: ?? │       |── 1.2.1.3
+// 14: ?? │           -── 1.2.1.3.1
+// 15: ?? │           -── 1.2.1.3.2
+// 16: ?? ├── 1.4
+// 17: ?? │   -── 1.4.1
+// 18: ?? │   ├── 1.4.2
+// 19: ?? │   │   -── 1.4.2.1
+// 20: ?? │   ├── 1.4.3
+// 21: ?? │   │   -── 1.4.3.1
+// 
+// Vector n-ary tree implementation:
+//
+// 2 arrays: main and overflow
+//
+// mmap: overflow (under 32K entries continuous) : main array
+//
+// main array:
+//
+//  * A int16_t per entry (<0 points to overflow), >0 is the tree level (0 deleted/unused)
+//  * Nodes in array using post-order, Entry is the level
+//
+// Capacity to print a bit like "tree" (but in post-order)
+// Index: NextSibling: Level : Tree
+// 00: +7, 2 -── 1.1
+// 01: +1, 4 │       -── 1.2.1.1
+// 02: +3, 4 │       ├── 1.2.1.2
+// 03: +1, 5 │       |   -── 1.2.1.3.1
+// 04: +0, 5 │       |   |── 1.2.1.3.2
+// 05: +0, 4 │       |── 1.2.1.3
+// 06: +0, 3 │   -── 1.2.1
+// 07: +6, 2 ├── 1.2
+// 08: +1, 4 │       -── 1.3.1.1
+// 09: +1, 4 │       |── 1.3.1.2
+// 0a: +1, 3 │   -── 1.3.1
+// 0b: +1, 3 │   ├── 1.3.2
+// 0c: +0, 3 │   ├── 1.3.3
+// 0d: +6, 2 ├── 1.3
+// 0e: +3, 3 │   -── 1.4.1
+// 0f: +1, 4 │   │   -── 1.4.2.1
+// 10: +0, 4 │   │   |── 1.4.2.2
+// 11: +1, 3 │   ├── 1.4.2
+// 12: +0, 3 │   ├── 1.4.3
+// 13: +0, 2 ├── 1.4
+// 14: +0, 1 | 1
+//
+// API: find_siblings
+//  while(l[pos]<self_level) {
+//    if (l[pos] == self_level)
+//      add_to_sibling list
+//    ++pos
+//  }
+//
+// API: find_parent
+//  while(l[pos]!=self_level+1) {
+//    ++pos
+//  }
+//  pos is parent (unless root which has no parent)
+//
+// API: find_children
+//  while(l[pos]<self_level) {
+//    if (l[pos] == self_level-1)
+//      add_to_children list
+//    --pos
+//  }
+//
+// ------------------------------------------------------
+// ALTERNATIVE ORDER: (children_first traversal) -- Better (more spatial locality, less search)
+// Visit tree node once children and niblings are visited
+// (post-order does visit tree node once the children are visited)
+// visit:
+//   visit children(...)
+//   print children()
+// Index: first Child, parent
+// 01: 00, 05 │           -── 1.2.1.3.1
+// 02: 00, 05 │           -── 1.2.1.3.2
+// 03: 00, 06 │       -── 1.2.1.1
+// 04: 00, 06 │       ├── 1.2.1.2
+// 05: 01, 06 │       |── 1.2.1.3
+// 06: 03, 12 │   -── 1.2.1
+// 07: 00, 09 │       -── 1.3.1.1
+// 08: 00, 09 │       |── 1.3.1.2
+// 09: 08, 13 │   -── 1.3.1
+// 0a: 00, 13 │   ├── 1.3.2
+// 0b: 00, 13 │   ├── 1.3.3
+// 0c: 00, 0e │   │   -── 1.4.2.1
+// 0d: 00, 0e │   │   -── 1.4.3.1
+// 0e: 00, 14 │   -── 1.4.1
+// 0f: 0c, 14 │   ├── 1.4.2
+// 10: 0d, 14 │   ├── 1.4.3
+// 11: 00, 15 -── 1.1
+// 12: 06, 15 ├── 1.2
+// 13: 09, 15 ├── 1.3
+// 14: 0e, 15 ├── 1.4
+// 15: 11, 00 | 1
+//
+// API: find_next_siblings
+//  while(parent[++pos]==parent[self]) {
+//    add_to_sibling list
+//  }
+//
+// API: find_prev_siblings
+//  while(parent[--pos]==parent[self]) {
+//    add_to_sibling list
+//  }
+//
+// API: find_parent
+//  parent[pos]
+//
+// API: find_children
+//  from = fc[pos]
+//  while(parent[from++] == pos) {
+//    add_to_children list
+//  }
+//
+//  API is_first_child()
+//    Check prev parent (if parent_id is diff, is first child)
+//
+//  API is_last_child()
+//    Check next parent (if parent_id is diff, is last child)
+//
+//  API: find_last_child()
+//    check older sibling that has children (fc[pos+1] if exist)
+//    sibling_fc-1 should be the last child
+//
+// API: insert_child_next_to
+//    insert after, keep same parent
+//
+// API: insert_first_child
+//    insert before fc[pos], update fc[pos]--
+//
+// API: insert_last_child
+//    find last_child, insert after
+//
+// API: add_child (first child or insert_last_child)
+//
+// ------------------------------------------------------
+// BEST potential locality on typical LNAST traversal
+//
+//   Get children & get parent calls
+// 02: 02 -── 1.1
+// 05: 04 │       -── 1.2.1.1
+// 06: 04 │       ├── 1.2.1.2
+// 14: 05 │           -── 1.2.1.3.1
+// 15: 05 │           -── 1.2.1.3.2
+// 13: 04 │       |── 1.2.1.3
+// 04: 03 │   -── 1.2.1
+// 03: 02 ├── 1.2
+// 11: 04 │       -── 1.3.1.1
+// 12: 04 │       |── 1.3.1.2
+// 08: 03 │   -── 1.3.1
+// 09: 03 │   ├── 1.3.2
+// 10: 03 │   ├── 1.3.3
+// 07: 02 ├── 1.3
+// 17: 03 │   -── 1.4.1
+// 19: 04 │   │   -── 1.4.2.1
+// 18: 03 │   ├── 1.4.2
+// 21: 04 │   │   -── 1.4.3.1
+// 20: 03 │   ├── 1.4.3
+// 16: 02 ├── 1.4
+// 01: 01 | 1
+//
+// API: find_next_siblings
+//  ++pos;
+//  while(level[pos]<=level[self]) {
+//    if(level[pos]==level[self]) {
+//      return pos
+//    }
+//    ++pos;
+//  }
+//
+// API: find_prev_siblings
+//   Same traverse --pos
+//
+// API: find_parent
+//   ++pos until level[pos] == level[self]+1
+//
+//  API is_first_child()
+//   go --pos while (level[pos]>level[self])
+//   true when level[pos] == level[self] || level[pos] == level[self-1]
+//
+//  API is_last_child()
+//   true when level[pos+1] == level[self] + 1
+//
+// ------------------------------------------------------
+// BEST Possible order for LNAST "typical" traversal is post-order (first-case)
+//   Get children & get parent calls
+//
+// Index: level (2bytes)
+//
+// 01: 01 | 1
+// 02: 02 -── 1.1
+// 03: 02 ├── 1.2
+// 04: 03 │   -── 1.2.1
+// 05: 04 │       -── 1.2.1.1
+// 06: 04 │       ├── 1.2.1.2
+// 07: 02 ├── 1.3
+// 08: 03 │   -── 1.3.1
+// 09: 03 │   ├── 1.3.2
+// 10: 03 │   ├── 1.3.3
+// 11: 04 │       -── 1.3.1.1
+// 12: 04 │       |── 1.3.1.2
+// 13: 04 │       |── 1.2.1.3
+// 14: 05 │           -── 1.2.1.3.1
+// 15: 05 │           -── 1.2.1.3.2
+// 16: 02 ├── 1.4
+// 17: 03 │   -── 1.4.1
+// 18: 03 │   ├── 1.4.2
+// 19: 04 │   │   -── 1.4.2.1
+// 20: 03 │   ├── 1.4.3
+// 21: 04 │   │   -── 1.4.3.1
+//
+// API: find_next_siblings
+//  ++pos;
+//  while(level[pos]<=level[self]) {
+//    if(level[pos]==level[self]) {
+//      return pos
+//    }
+//    ++pos;
+//  }
+//
+// API: find_prev_siblings
+//   Same traverse --pos
+//
+// API: find_parent
+//  Build to ID when traversing, but could be recomputed
+//  go back until level+1 is found
+//
+// API: find_children
+//  Similar find_next_siblings but continue adding
+//
+//  API is_first_child()
+//   return level[self-1] == level[self]-1
+//
+//  API is_last_child()
+//   return level[self+1] >= level[self]-1
+//
+//  API: find_last_child()
+//    check older sibling that has children (fc[pos+1] if exist)
+//    sibling_fc-1 should be the last child
+//
+// API: insert_child_next_to
+//    insert after, keep same parent
+//
+// API: insert_first_child
+//    insert before fc[pos], update fc[pos]--
+//
+// API: insert_last_child
+//    find last_child, insert after
+//
+// API: add_child (first child or insert_last_child)
+
 #include <sys/stat.h>
 #include <sys/types.h>
 
