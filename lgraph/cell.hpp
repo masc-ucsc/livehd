d198c13843679f3332af6f51aa348077a1a60ecd
sakshi garg <sgarg3@ucsc.edu>
2022-11-19 13:21:05 -0800

    loop_first should cover const as well as IO

--- lgraph/cell.hpp
+++ lgraph/cell.hpp
@@ -87,7 +87,7 @@ protected:
   static constexpr std::string_view get_sink_name_slow(Ntype_op op, int pid);
 
 public:
-  static inline constexpr bool is_loop_first(Ntype_op op) { return op == Ntype_op::Const; }
+  static inline constexpr bool is_loop_first(Ntype_op op) { return op == Ntype_op::Const || op == Ntype_op::IO; }
   static inline constexpr bool is_loop_last(Ntype_op op) {
     return static_cast<int>(op) >= static_cast<int>(Ntype_op::Memory) && static_cast<int>(op) <= static_cast<int>(Ntype_op::Sub);
   }
