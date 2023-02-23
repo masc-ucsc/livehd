9c4d25caf4c6d49bb09e46bfc8ae58b38aa5491b
Mark Zakharov <markzakharov1102@gmail.com>
2023-02-22 18:50:34 -0800

    fixed inou yosys tests syntax errors

--- inou/yosys/tests/simple_add.v
+++ inou/yosys/tests/simple_add.v
@@ -2,8 +2,8 @@ module simple_add(input [7:0] a, input [7:0] b,
   output signed [7:0] h
 );
 
-  signed wire [7:0] as = a;
-  signed wire [7:0] bs = b;
+  wire signed [7:0] as = a;
+  wire signed [7:0] bs = b;
 
   wire [7:0] f = as + bs;
   assign h = as + bs - as;
--- inou/yosys/tests/simple_weird.v
+++ inou/yosys/tests/simple_weird.v
@@ -2,8 +2,8 @@ module simple_weird(input [7:0] a, input [7:0] b,
   output signed [2:0] h
 );
 
-  signed wire [7:0] as = a;
-  signed wire [7:0] bs = b;
+  wire signed [7:0] as = a;
+  wire signed [7:0] bs = b;
 
   wire [7:0] f = as + bs;
   assign h[0] = 1 | {|{as + bs - as}};
