9c349c450405ba88eaccd6cab7558577564c1094
Jose Renau <renau@ucsc.edu>
2026-06-24 11:19:50 -0700

    cleanup test to run under 15secs for opt

--- inou/prp/tests/equiv/pipe_state_acc.v
+++ inou/prp/tests/equiv/pipe_state_acc.v
@@ -1,16 +1,16 @@
 module \pipe_state_acc.acc_mix (
   input             clock,
   input             reset,
-  input      [7:0]  a,
-  input      [7:0]  b,
-  output reg [20:0] x
+  input      [3:0]  a,
+  input      [3:0]  b,
+  output reg [8:0]  x
 );
 
-  reg [19:0] tmp;
+  reg [7:0] tmp;
 
   always @(posedge clock) begin
-    if (reset) tmp <= 20'd0;
-    else if (tmp < 20'd1000000) tmp <= tmp + a + b;
+    if (reset) tmp <= 8'd0;
+    else if (tmp < 8'd200) tmp <= tmp + a + b;
   end
 
   always @(posedge clock) begin
